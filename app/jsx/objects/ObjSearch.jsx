9075f0df77d713ea956baf42d10a94b1ed5c2839
JoelUCOP <joel.hagedorn@ucop.edu>
2016-08-17 14:01:56 -0700

    add search object close button; add absolute positioning

--- app/jsx/objects/ObjSearch.jsx
+++ app/jsx/objects/ObjSearch.jsx
@@ -3,11 +3,14 @@
 class ObjSearch extends React.Component {
   render() {
 		return (
-			<form action="" className="o-search">
-				<label className="o-search__label" htmlFor="global-search">Search eScholarship</label>
-				<input type="search" id="global-search" className="o-search__field" placeholder="Search eScholarship" />
-				<button type="submit" className="o-search__submit-button">Search</button>
-			</form>
+			<div className="o-search">
+				<form action="" className="o-search__form">
+					<label className="o-search__label" htmlFor="global-search">Search eScholarship</label>
+					<input type="search" id="global-search" className="o-search__field" placeholder="Search eScholarship" />
+					<button type="submit" className="o-search__submit-button">Search</button>
+					<button className="o-search__close-button" aria-label="close"></button>
+				</form>
+			</div>
 		)
 	}
 }
