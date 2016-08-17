7e71dff929132707613b98c53c3a15c49c4f86f7
JoelUCOP <joel.hagedorn@ucop.edu>
2016-08-17 14:34:00 -0700

    add search object radio buttons/labels

--- app/jsx/objects/ObjSearch.jsx
+++ app/jsx/objects/ObjSearch.jsx
@@ -3,14 +3,20 @@
 class ObjSearch extends React.Component {
   render() {
 		return (
-			<div className="o-search">
-				<form action="" className="o-search__form">
+			<form action="" className="o-search">
+				<div className="o-search__form">
 					<label className="o-search__label" htmlFor="global-search">Search eScholarship</label>
-					<input type="search" id="global-search" className="o-search__field" placeholder="Search eScholarship" />
+					<input type="search" id="global-search" className="o-search__field" placeholder="Search eScholarship"/>
 					<button type="submit" className="o-search__submit-button">Search</button>
 					<button className="o-search__close-button" aria-label="close"></button>
-				</form>
-			</div>
+				</div>
+				<div className="o-search__refine">
+					<input type="radio" name="search-refine" id="o-search__refine-campus"/>
+					<label htmlFor="o-search__refine-campus">This campus</label>
+					<input type="radio" name="search-refine" id="o-search__refine-eschol"/>
+					<label htmlFor="o-search__refine-eschol">eScholarship</label>
+				</div>
+			</form>
 		)
 	}
 }
