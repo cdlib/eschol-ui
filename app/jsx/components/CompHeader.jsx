e9db183271caaee51b87a9d0e74c122cb5c1a839
JoelUCOP <joel.hagedorn@ucop.edu>
2016-08-18 13:56:19 -0700

    add header search position/transform properties

--- app/jsx/components/CompHeader.jsx
+++ app/jsx/components/CompHeader.jsx
@@ -13,6 +13,7 @@ class CompHeader extends React.Component {
 					<div className="c-header__search">
 						<ObjSearch />
 					</div>
+					<button className="c-header__search-button">X</button>
 				</div>
 				<div className="c-header__deposit">
 					<button className="c-header__deposit-button">Deposit/Publish</button>
