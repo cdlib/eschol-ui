e90bc95a02b8e123b140ee7968203cfef21b93ed
JoelUCOP <joel.hagedorn@ucop.edu>
2016-08-23 10:52:49 -0700

    create initial globalnav component html/css

--- app/jsx/components/CompGlobalNav.jsx
+++ app/jsx/components/CompGlobalNav.jsx
@@ -5,9 +5,19 @@ import React from 'react'
 class CompGlobalNav extends React.Component {
   render() {
 		return (
-			<div className="c-globalnav">
-				[global navigation component to go here]
-			</div>
+			<nav className="c-globalnav">
+				<details>
+					<summary className="c-globalnav__button">Menu
+					</summary>
+					<div className="c-globalnav__items">
+						<a href="">About</a>
+						<a href="">Campuses</a>
+						<a href="">Open Access Policies</a>
+						<a href="">Journals</a>
+						<a href="">Get Started</a>
+					</div>
+				</details>
+			</nav>
 		)
 	}
 }
