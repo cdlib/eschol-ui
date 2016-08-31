7a8e86931b358131f5a398070cccbe1150ff9d83
JoelUCOP <joel.hagedorn@ucop.edu>
2016-08-31 16:55:59 -0700

    create initial styles for box object

--- app/jsx/objects/ObjBox.jsx
+++ app/jsx/objects/ObjBox.jsx
@@ -6,7 +6,9 @@ class ObjBox extends React.Component {
   render() {
 		return (
 			<div className="o-box">
-				[Box object will go here]
+				<h2 className="o-box__heading">About eScholarship</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias sed sapiente nulla debitis nobis asperiores hic est, fuga illo? Accusamus molestias pariatur nihil, libero possimus optio iusto nobis sapiente ad.
+				</p>
 			</div>
 		)
 	}
