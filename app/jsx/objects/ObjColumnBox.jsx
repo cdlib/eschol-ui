9066dab97f9fdbb209490841f84dffbc0ae62871
JoelUCOP <joel.hagedorn@ucop.edu>
2016-09-01 11:49:02 -0700

    add main and sidebar column box styles

--- app/jsx/objects/ObjColumnBox.jsx
+++ app/jsx/objects/ObjColumnBox.jsx
@@ -5,11 +5,21 @@ import React from 'react'
 class ObjBox extends React.Component {
   render() {
 		return (
-			<div className="o-columnbox">
-				<h2 className="o-columnbox__heading">About eScholarship</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias sed sapiente nulla debitis nobis asperiores hic est, fuga illo? Accusamus molestias pariatur nihil, libero possimus optio iusto nobis sapiente ad.
-				</p>
+			<div>
+				<h2>Main Column Box</h2>
+				<div className="o-columnbox-main">
+					<h2 className="o-columnbox-main__heading">About eScholarship</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias sed sapiente nulla debitis nobis asperiores hic est, fuga illo? Accusamus molestias pariatur nihil, libero possimus optio iusto nobis sapiente ad.
+					</p>
+				</div>
+				<h2>Sidebar Column Box</h2>
+				<div className="o-columnbox-sidebar">
+					<h2 className="o-columnbox-sidebar__heading">Placeholder Title</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias sed sapiente nulla debitis nobis asperiores hic est, fuga illo? Accusamus molestias pariatur nihil, libero possimus optio iusto nobis sapiente ad.
+					</p>
+				</div>
 			</div>
+			
 		)
 	}
 }
