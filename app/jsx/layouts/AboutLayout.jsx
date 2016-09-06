7bed20fc2f1dd7ab0d46d0512bab3f492a8d562b
JoelUCOP <joel.hagedorn@ucop.edu>
2016-09-06 11:56:19 -0700

    add sidebar with unstyled text to about layout

--- app/jsx/layouts/AboutLayout.jsx
+++ app/jsx/layouts/AboutLayout.jsx
@@ -13,23 +13,48 @@ class AboutLayout extends React.Component {
 				<HeaderComp />
 				<GlobalNavComp />
 				<BreadcrumbComp />
-				<div className="o-columnbox-main">
-					<h1 className="o-columnbox-main__heading">About eScholarship</h1>
-					<p>eScholarship provides a suite of open access, scholarly publishing services and research tools that enable departments, research units, publishing programs, and individual scholars associated with the University of California to have direct control over the creation and dissemination of the full range of their scholarship.
-					<p>With eScholarship, you can publish the following original scholarly works on a dynamic research platform available to scholars worldwide:</p>
-					</p>
-					<ul>
-						<li><a href="">Books</a></li>
-						<li><a href="">Journals</a></li>
-						<li><a href="">Working Papers</a></li>
-						<li><a href="">Previously Published Works</a></li>
-						<li><a href="">Conferences</a></li>
-					</ul>
-					<p>eScholarship also provides deposit and dissemination services for postprints, or previously published articles.</p>
-					<p>Publications benefit from manuscript and peer-review management systems, as well as a full range of persistent access and preservation services.</p>
-					<p>Learn more about what to expect from publishing with eScholarship.</p>
-					<p>eScholarship is a service of the Publishing Group of the California Digital Library.</p>
-					<p>Questions? <a href="">Contact us</a>.</p>
+				<div className="c-columns">
+					<aside>
+						<div className="o-columnbox-sidebar">
+							<h1 className="o-columnbox-sidebar__heading">About eScholarship</h1>
+							<ul>
+								<li><a href="">About eScholarship</a></li>
+								<li><a href="">About California Digital Library</a></li>
+								<li><a href="">eScholarship Staff</a></li>
+								<li><a href="">Campus Contacts</a></li>
+								<li><a href="">eScholarship Technology</a></li>
+								<li><a href="">Featured Announcements </a></li>
+								<li><a href="">In the Press</a></li>
+								<li><a href="">Press Releases</a></li>
+								<li><a href="">Testimonials</a></li>
+								<li><a href="">OA Policy Information</a></li>
+								<li><a href="">Privacy Policy</a></li>
+								<hr/>
+								<li><a href="">Recently Added Publications</a></li>
+								<li><a href="">Trending Publications</a></li>
+							</ul>
+						</div>
+					</aside>
+					<main>
+						<div className="o-columnbox-main">
+							<h1 className="o-columnbox-main__heading">About eScholarship</h1>
+							<p>eScholarship provides a suite of open access, scholarly publishing services and research tools that enable departments, research units, publishing programs, and individual scholars associated with the University of California to have direct control over the creation and dissemination of the full range of their scholarship.
+							<p>With eScholarship, you can publish the following original scholarly works on a dynamic research platform available to scholars worldwide:</p>
+							</p>
+							<ul>
+								<li><a href="">Books</a></li>
+								<li><a href="">Journals</a></li>
+								<li><a href="">Working Papers</a></li>
+								<li><a href="">Previously Published Works</a></li>
+								<li><a href="">Conferences</a></li>
+							</ul>
+							<p>eScholarship also provides deposit and dissemination services for postprints, or previously published articles.</p>
+							<p>Publications benefit from manuscript and peer-review management systems, as well as a full range of persistent access and preservation services.</p>
+							<p>Learn more about what to expect from publishing with eScholarship.</p>
+							<p>eScholarship is a service of the Publishing Group of the California Digital Library.</p>
+							<p>Questions? <a href="">Contact us</a>.</p>
+						</div>
+					</main>
 				</div>
 				<FooterComp />
 			</div>
