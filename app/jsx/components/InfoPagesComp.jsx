225f19e81b74739305e5c883c17371904de0fb28
JoelUCOP <joel.hagedorn@ucop.edu>
2016-09-07 16:56:09 -0700

    create initial infopages component styles

--- app/jsx/components/InfoPagesComp.jsx
+++ app/jsx/components/InfoPagesComp.jsx
@@ -6,7 +6,11 @@ class InfoPagesComp extends React.Component {
   render() {
 		return (
 			<div className="c-infopages">
-				[informational pages component to go here]
+				<div className="c-infopages__item">
+					<div className="c-infopages__heading">The Center for Environmental Design Research <span className="c-infopages__heading-title">In the Press</span></div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus iste officiis sequi maiores, numquam praesentium qui molestias perspiciatis accusamus. Commodi minima quaerat deleniti voluptate fugiat necessitatibus tenetur eum enim inventore.
+					</p>
+				</div>
 			</div>
 		)
 	}
