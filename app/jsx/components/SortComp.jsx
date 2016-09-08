e7a8f94451b10b132d84552fc0da7d3a0c2f9acc
JoelUCOP <joel.hagedorn@ucop.edu>
2016-09-08 13:56:49 -0700

    create initial sort component styles

--- app/jsx/components/SortComp.jsx
+++ app/jsx/components/SortComp.jsx
@@ -5,9 +5,31 @@ import React from 'react'
 class SortComp extends React.Component {
   render() {
 		return (
-			<div className="c-sort">
-			[sort by component will go here]
-			</div>
+			<form className="c-sort">
+				<div className="c-sort__dropgroup">
+					<label htmlFor="c-sort1">Sort By</label>
+					<select name="" id="c-sort1">
+						<option value="">Relevance</option>
+						<option value="">Most Popular</option>
+						<option value="">A-Z By Title</option>
+						<option value="">Z-A By Title</option>
+						<option value="">A-Z By Author</option>
+						<option value="">Z-A By Author</option>
+						<option value="">Date Ascending</option>
+						<option value="">Date Decending</option>
+					</select>
+				</div>
+				<div className="c-sort__dropgroup">
+					<label htmlFor="c-sort2">Per Page</label>
+					<select name="" id="c-sort2">
+						<option value="">10</option>
+						<option value="">20</option>
+						<option value="">30</option>
+						<option value="">50</option>
+						<option value="">100</option>
+					</select>
+				</div>
+			</form>
 		)
 	}
 }
