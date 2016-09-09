c15ccb6f5c9d3dd6cb42e2f7e46edaa5ebfe4654
JoelUCOP <joel.hagedorn@ucop.edu>
2016-09-09 16:42:34 -0700

    create initial pagination component html

--- app/jsx/components/PaginationComp.jsx
+++ app/jsx/components/PaginationComp.jsx
@@ -5,9 +5,17 @@ import React from 'react'
 class PaginationComp extends React.Component {
   render() {
 		return (
-			<div className="c-pagination">
-			[pagination component will go here]
-			</div>
+			<form className="c-pagination" action="">
+				<button className="c-pagination__prevnext">Previous</button>
+				<button className="c-pagination__page">1</button>
+				<button className="c-pagination__page">2</button>
+				<button className="c-pagination__page">3</button>
+				<button className="c-pagination__page">4</button>
+				<button className="c-pagination__page">5</button>
+				<span>&hellip;</span>
+				<button className="c-pagination__page">342</button>
+				<button className="c-pagination__prevnext">Next</button>
+			</form>
 		)
 	}
 }
