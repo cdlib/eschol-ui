6f9a9983432a955b283667047fcaaced209ffd4e
JoelUCOP <joel.hagedorn@ucop.edu>
2016-09-12 14:44:22 -0700

    set input-height to element-height; refactor pagination

--- app/jsx/components/PaginationComp.jsx
+++ app/jsx/components/PaginationComp.jsx
@@ -5,17 +5,17 @@ import React from 'react'
 class PaginationComp extends React.Component {
   render() {
 		return (
-			<form className="c-pagination" action="">
-				<button className="c-pagination__prevnext">Previous</button>
-				<button className="c-pagination__page">1</button>
-				<button className="c-pagination__page">2</button>
-				<button className="c-pagination__page">3</button>
-				<button className="c-pagination__page">4</button>
-				<button className="c-pagination__page">5</button>
-				<span>&hellip;</span>
-				<button className="c-pagination__page">342</button>
-				<button className="c-pagination__prevnext">Next</button>
-			</form>
+			<div className="c-pagination" action="">
+				<a className="c-pagination__prevnext" href="">Previous</a>
+				<a className="c-pagination__item--active" href="">1</a>
+				<a className="c-pagination__item" href="">2</a>
+				<a className="c-pagination__item" href="">3</a>
+				<a className="c-pagination__item" href="">4</a>
+				<a className="c-pagination__item" href="">5</a>
+				<span className="c-pagination__ellipses">&hellip;</span>
+				<a className="c-pagination__item" href="">342</a>
+				<a className="c-pagination__prevnext" href="">Next</a>
+			</div>
 		)
 	}
 }
