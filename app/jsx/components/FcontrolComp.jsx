818d6772de103da268e96f13ee96f8c43c79a2df
JoelUCOP <joel.hagedorn@ucop.edu>
2016-09-12 10:37:10 -0700

    create fcontrol droplist html & sass

--- app/jsx/components/FcontrolComp.jsx
+++ app/jsx/components/FcontrolComp.jsx
@@ -5,7 +5,18 @@ import React from 'react'
 class FcontrolComp extends React.Component {
   render() {
 		return (
-			<div className="c-fcontrol">[form controls will go here]
+			<div className="c-fcontrol">
+				<h2>Droplists</h2>
+				<div className="c-fcontrol__droplist">
+					<label htmlFor="c-fcontrol__droplist-label">Fruit:</label>
+					<select name="" id="c-fcontrol__droplist-label">
+						<option value="">Apples</option>
+						<option value="">Oranges</option>
+						<option value="">Pears</option>
+						<option value="">Grapes</option>
+						<option value="">Strawberries</option>
+					</select>
+				</div>
 			</div>
 		)
 	}
