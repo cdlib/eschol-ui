5b1155c5770004a02289c2f4ee844d4f7c1d2214
JoelUCOP <joel.hagedorn@ucop.edu>
2016-09-13 16:34:55 -0700

    create initial scholworks html and styles

--- app/jsx/components/ScholWorksComp.jsx
+++ app/jsx/components/ScholWorksComp.jsx
@@ -6,7 +6,20 @@ class ScholWorksComp extends React.Component {
   render() {
 		return (
 			<div className="c-scholworks">
-				[scholarly works component to go here]
+				<section className="c-scholworks__item">
+					<div className="c-scholworks__main-column">
+						<span className="c-scholworks__tab-article">Article</span>
+						<span className="c-scholworks__tab-peer">Peer Reviewed</span>
+						<header>
+							<a href="">From the New Heights: The City and Migrating Latinas in Real Woman Have Curves and Maria Full of Grace</a>
+						</header>
+						<p>Dahle, Kevin W: Pelfrey, Patricia A; Walker, Iain S; Kling, Rob; Huh, Tina <br/>
+						<a className="c-scholworks__institution-link" href="">Mester Journal, Volume 42, Issue 1</a> (2012)</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum esse ea laboriosam modi! Distinctio itaque rerum totam, architecto ad dolor et, repudiandae ipsum tempora voluptatem praesentium quaerat quod, nihil consectetur.
+						</p>
+					</div>
+					<img className="c-scholworks__article-preview" src="images/temp_article.png" alt="article"/>
+				</section>
 			</div>
 		)
 	}
