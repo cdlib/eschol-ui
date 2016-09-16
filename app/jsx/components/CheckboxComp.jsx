9debde05a1e1570c567677e325b15d28f4bf5cc5
JoelUCOP <joel.hagedorn@ucop.edu>
2016-09-16 10:57:05 -0700

    refactor checkbox html and styles

--- app/jsx/components/CheckboxComp.jsx
+++ app/jsx/components/CheckboxComp.jsx
@@ -6,26 +6,22 @@ class CheckboxComp extends React.Component {
   render() {
 		return (
 			<div className="c-checkbox">
-				<div className="c-checkbox__item">
-					<input id="c-checkbox__number1" type="checkbox" className="c-checkbox__input"/>
-					<label htmlFor="c-checkbox__number1" className="c-checkbox__label">Video</label>
-				</div>
-				<div className="c-checkbox__item">
-					<input id="c-checkbox__number2" type="checkbox" className="c-checkbox__input"/>
-					<label htmlFor="c-checkbox__number2" className="c-checkbox__label">Audio</label>
-				</div>
-				<div className="c-checkbox__item">
-					<input id="c-checkbox__number3" type="checkbox" className="c-checkbox__input"/>
-					<label htmlFor="c-checkbox__number3" className="c-checkbox__label">images</label>
-				</div>
-				<div className="c-checkbox__item">
-					<input id="c-checkbox__number4" type="checkbox" className="c-checkbox__input"/>
-					<label htmlFor="c-checkbox__number4" className="c-checkbox__label">PDF</label>
-				</div>
-				<div className="c-checkbox__item">
-					<input id="c-checkbox__number5" type="checkbox" className="c-checkbox__input"/>
-					<label htmlFor="c-checkbox__number5" className="c-checkbox__label">ZIP</label>
-				</div>
+
+				<input id="c-checkbox__number1" type="checkbox" className="c-checkbox__input"/>
+				<label htmlFor="c-checkbox__number1" className="c-checkbox__label">Video</label>
+
+				<input id="c-checkbox__number2" type="checkbox" className="c-checkbox__input"/>
+				<label htmlFor="c-checkbox__number2" className="c-checkbox__label">Audio</label>
+				
+				<input id="c-checkbox__number3" type="checkbox" className="c-checkbox__input"/>
+				<label htmlFor="c-checkbox__number3" className="c-checkbox__label">images</label>
+				
+				<input id="c-checkbox__number4" type="checkbox" className="c-checkbox__input"/>
+				<label htmlFor="c-checkbox__number4" className="c-checkbox__label">PDF</label>
+				
+				<input id="c-checkbox__number5" type="checkbox" className="c-checkbox__input"/>
+				<label htmlFor="c-checkbox__number5" className="c-checkbox__label">ZIP</label>
+
 			</div>
 		)
 	}
