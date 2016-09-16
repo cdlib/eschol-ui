4158f2654860eec050e664c8d6ea8e952acc4026
JoelUCOP <joel.hagedorn@ucop.edu>
2016-09-16 15:22:10 -0700

    add export, columns, columnbox, filter, and infopages comps to search layout

--- app/jsx/layouts/SearchLayout.jsx
+++ app/jsx/layouts/SearchLayout.jsx
@@ -4,6 +4,9 @@ import React from 'react'
 import HeaderComp from '../components/HeaderComp.jsx'
 import GlobalNavComp from '../components/GlobalNavComp.jsx'
 import ExportComp from '../components/ExportComp.jsx'
+import FilterComp from '../components/FilterComp.jsx'
+import InfoPagesComp from '../components/InfoPagesComp.jsx'
+import ScholWorksComp from '../components/ScholWorksComp.jsx'
 import FooterComp from '../components/FooterComp.jsx'
 
 class SearchLayout extends React.Component {
@@ -13,6 +16,25 @@ class SearchLayout extends React.Component {
 				<HeaderComp />
 				<GlobalNavComp />
 				<ExportComp />
+				<div className="c-columns">
+					<aside>
+					 	<FilterComp />
+					</aside>
+				  <main>
+				  	<section className="o-columnbox-main">
+							<header>
+								<h2 className="o-columnbox-main__heading">Informational Pages (12 results)</h2>
+							</header>
+							<InfoPagesComp />
+						</section>
+						<section className="o-columnbox-main">
+							<header>
+								<h2 className="o-columnbox-main__heading">Scholarly Works (12,023 results)</h2>
+							</header>
+							[ScholWorksComp to go here]
+						</section>
+				  </main>
+				</div>
 				<FooterComp />
 			</div>
 		)
