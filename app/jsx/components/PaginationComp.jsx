8be9087dbfea3ed38124a679c1b52dcdf9ec46fa
JoelUCOP <joel.hagedorn@ucop.edu>
2016-09-16 16:20:22 -0700

    remove 2 pagination component items

--- app/jsx/components/PaginationComp.jsx
+++ app/jsx/components/PaginationComp.jsx
@@ -10,8 +10,6 @@ class PaginationComp extends React.Component {
 				<a className="c-pagination__item--active" href="">1</a>
 				<a className="c-pagination__item" href="">2</a>
 				<a className="c-pagination__item" href="">3</a>
-				<a className="c-pagination__item" href="">4</a>
-				<a className="c-pagination__item" href="">5</a>
 				<span className="c-pagination__ellipses">&hellip;</span>
 				<a className="c-pagination__item" href="">342</a>
 				<a className="c-pagination__prevnext" href="">Next</a>
