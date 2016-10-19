c923f1f504fac4278aef11549901ece26cc9333a
JoelUCOP <joel.hagedorn@ucop.edu>
2016-10-19 16:34:07 -0700

    refactor search comp 2 to align elements

--- app/jsx/components/SearchComp2.jsx
+++ app/jsx/components/SearchComp2.jsx
@@ -6,17 +6,21 @@ class SearchComp2 extends React.Component {
   render() {
 		return (
 			<form action="" className="c-search">
-				<div className="c-search__form">
-					<label className="c-search__label" htmlFor="global-search">Search eScholarship</label>
-					<input type="search" id="global-search" className="c-search__field" placeholder="Search eScholarship"/>
+				<div className="c-search__thesearch">
+					<div className="c-search__form">
+						<label className="c-search__label" htmlFor="global-search">Search eScholarship</label>
+						<input type="search" id="global-search" className="c-search__field" placeholder="Search eScholarship"/>
+					</div>
+					<div className="c-search__refine">
+						<input type="radio" name="search-refine" id="c-search__refine-eschol"/>
+						<label htmlFor="c-search__refine-eschol">All of eScholarship</label>
+						<input type="radio" name="search-refine" id="c-search__refine-campus"/>
+						<label htmlFor="c-search__refine-campus">This campus</label>
+					</div>
+				</div>
+				<div className="c-search__buttons">
 					<button type="submit" className="c-search__submit-button" aria-label="search"></button>
 					<button className="c-search__search-close-button" aria-label="close search field" onClick = {()=>this.props.onClose()}></button>
-				</div>
-				<div className="c-search__refine">
-					<input type="radio" name="search-refine" id="c-search__refine-campus"/>
-					<label htmlFor="c-search__refine-campus">This campus</label>
-					<input type="radio" name="search-refine" id="c-search__refine-eschol"/>
-					<label htmlFor="c-search__refine-eschol">eScholarship</label>
 				</div>
 			</form>
 		)
