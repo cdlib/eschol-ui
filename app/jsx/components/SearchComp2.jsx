a97d2c06947320c5cea6d12d39680eb7f4ff2cb4
JoelUCOP <joel.hagedorn@ucop.edu>
2016-10-20 11:16:21 -0700

    refine search comp icon alignment; remove unused <div>

--- app/jsx/components/SearchComp2.jsx
+++ app/jsx/components/SearchComp2.jsx
@@ -6,7 +6,7 @@ class SearchComp2 extends React.Component {
   render() {
 		return (
 			<form action="" className="c-search">
-				<div className="c-search__thesearch">
+				<div className="c-search__inputs">
 					<div className="c-search__form">
 						<label className="c-search__label" htmlFor="global-search">Search eScholarship</label>
 						<input type="search" id="global-search" className="c-search__field" placeholder="Search eScholarship"/>
@@ -18,10 +18,8 @@ class SearchComp2 extends React.Component {
 						<label htmlFor="c-search__refine-campus">This campus</label>
 					</div>
 				</div>
-				<div className="c-search__buttons">
-					<button type="submit" className="c-search__submit-button" aria-label="search"></button>
-					<button className="c-search__search-close-button" aria-label="close search field" onClick = {()=>this.props.onClose()}></button>
-				</div>
+				<button type="submit" className="c-search__submit-button" aria-label="search"></button>
+				<button className="c-search__search-close-button" aria-label="close search field" onClick = {()=>this.props.onClose()}></button>
 			</form>
 		)
 	}
