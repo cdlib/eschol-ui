20b7e2530092e60da535382e83a14d03b5adf3e5
JoelUCOP <joel.hagedorn@ucop.edu>
2016-10-20 11:35:11 -0700

    duplicate search.scss, rename to versions 1 and 2

--- app/jsx/components/SearchComp1.jsx
+++ app/jsx/components/SearchComp1.jsx
@@ -5,14 +5,14 @@ import React from 'react'
 class SearchComp1 extends React.Component {
   render() {
 		return (
-			<form action="" className="c-search">
-				<div className="c-search__form">
-					<label className="c-search__label" htmlFor="global-search">Search eScholarship</label>
-					<input type="search" id="global-search" className="c-search__field" placeholder="Search eScholarship"/>
-					<button type="submit" className="c-search__submit-button" aria-label="search"></button>
-					<button className="c-search__search-close-button" aria-label="close search field" onClick = {()=>this.props.onClose()}></button>
+			<form action="" className="c-search1">
+				<div className="c-search1__form">
+					<label className="c-search1__label" htmlFor="global-search">Search eScholarship</label>
+					<input type="search" id="global-search" className="c-search1__field" placeholder="Search eScholarship"/>
+					<button type="submit" className="c-search1__submit-button" aria-label="search"></button>
+					<button className="c-search1__search-close-button" aria-label="close search field" onClick = {()=>this.props.onClose()}></button>
 				</div>
-				<div className="c-search__tagline">
+				<div className="c-search1__tagline">
 					Open Access publications from the University of California
 				</div>
 			</form>
--- app/jsx/components/SearchComp2.jsx
+++ app/jsx/components/SearchComp2.jsx
@@ -5,21 +5,21 @@ import React from 'react'
 class SearchComp2 extends React.Component {
   render() {
 		return (
-			<form action="" className="c-search">
-				<div className="c-search__inputs">
-					<div className="c-search__form">
-						<label className="c-search__label" htmlFor="global-search">Search eScholarship</label>
-						<input type="search" id="global-search" className="c-search__field" placeholder="Search eScholarship"/>
+			<form action="" className="c-search2">
+				<div className="c-search2__inputs">
+					<div className="c-search2__form">
+						<label className="c-search2__label" htmlFor="global-search">Search eScholarship</label>
+						<input type="search" id="global-search" className="c-search2__field" placeholder="Search eScholarship"/>
 					</div>
-					<div className="c-search__refine">
-						<input type="radio" name="search-refine" id="c-search__refine-eschol"/>
-						<label htmlFor="c-search__refine-eschol">All of eScholarship</label>
-						<input type="radio" name="search-refine" id="c-search__refine-campus"/>
-						<label htmlFor="c-search__refine-campus">This campus</label>
+					<div className="c-search2__refine">
+						<input type="radio" name="search-refine" id="c-search2__refine-eschol"/>
+						<label htmlFor="c-search2__refine-eschol">All of eScholarship</label>
+						<input type="radio" name="search-refine" id="c-search2__refine-campus"/>
+						<label htmlFor="c-search2__refine-campus">This campus</label>
 					</div>
 				</div>
-				<button type="submit" className="c-search__submit-button" aria-label="search"></button>
-				<button className="c-search__search-close-button" aria-label="close search field" onClick = {()=>this.props.onClose()}></button>
+				<button type="submit" className="c-search2__submit-button" aria-label="search"></button>
+				<button className="c-search2__search-close-button" aria-label="close search field" onClick = {()=>this.props.onClose()}></button>
 			</form>
 		)
 	}
