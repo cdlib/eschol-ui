1e89151b3a65bbdbfb0b7f3be7a0f321313ea8be
JoelUCOP <joel.hagedorn@ucop.edu>
2016-10-24 15:43:58 -0700

    add search2 animated refine show/hide styles and jsx

--- app/jsx/components/SearchComp2.jsx
+++ app/jsx/components/SearchComp2.jsx
@@ -3,18 +3,22 @@
 import React from 'react'
 
 class SearchComp2 extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {refineActive: false}
+  }
   render() {
 		return (
-			<form action="" className="c-search2">
+			<form action="" className="c-search2" onSubmit = {()=> this.setState({refineActive: false})}>
 				<div className="c-search2__inputs">
 					<div className="c-search2__form">
 						<label className="c-search2__label" htmlFor="global-search">Search eScholarship</label>
-						<input type="search" id="global-search" className="c-search2__field" placeholder="Search eScholarship"/>
+						<input type="search" id="global-search" className="c-search2__field" placeholder="Search eScholarship" onFocus = {()=> this.setState({refineActive: true})}/>
 					</div>
-					<div className="c-search2__refine">
-						<input type="radio" name="search-refine" id="c-search2__refine-eschol"/>
+					<div className={this.state.refineActive ? "c-search2__refine--active" : "c-search2__refine"}>
+						<input type="radio" name="search-refine" id="c-search2__refine-eschol" onClick = {()=> this.setState({refineActive: false})} defaultChecked={true}/>
 						<label htmlFor="c-search2__refine-eschol">All of eScholarship</label>
-						<input type="radio" name="search-refine" id="c-search2__refine-campus"/>
+						<input type="radio" name="search-refine" id="c-search2__refine-campus" onClick = {()=> this.setState({refineActive: false})}/>
 						<label htmlFor="c-search2__refine-campus">This campus</label>
 					</div>
 				</div>
