ded5125ff321c64e4927c2297de8c56e50e8c338
JoelUCOP <joel.hagedorn@ucop.edu>
2016-10-25 13:23:13 -0700

    begin subhead layout testing

--- app/jsx/pages/Subhead.jsx
+++ app/jsx/pages/Subhead.jsx
@@ -4,12 +4,23 @@ import React from 'react'
 
 class Subhead extends React.Component {
   render() {
-		return (
-			<div>
-	   		[Subhead page]
-	    </div>
-  	)
-	}
+    return (
+      <div className="c-subhead">
+        <div className="c-subhead__campus">
+          [campus]
+        </div>
+        <div className="c-subhead__menu">
+          [menu]
+        </div>
+        <div className="c-subhead__deposit">
+          [deposit]
+        </div>
+        <div className="c-subhead__banner">
+          [banner]
+        </div>
+      </div>
+    )
+  }
 }
 
 module.exports = Subhead;
