57309213eef583646fff11119d7a552fce87f603
JoelUCOP <joel.hagedorn@ucop.edu>
2016-10-25 16:58:50 -0700

    add initial campus comp html and styles

--- app/jsx/components/CampusComp.jsx
+++ app/jsx/components/CampusComp.jsx
@@ -1,12 +1,35 @@
 // ##### Campus Heading and Menu Component ##### //
 
 import React from 'react'
+import 'details-polyfill'
 
 class CampusComp extends React.Component {
   render() {
     return (
       <div className="c-campus">
-        [Campus heading and menu component will go here]
+        <h2 className="c-campus__heading">
+          UC Berkeley
+        </h2>
+        <div className="c-campus__menu">
+          <details open>
+            <summary>[arrow]</summary>
+            <div className="c-campus__menu-items">
+              <div>eScholarship at &hellip;</div>
+              <a href="">UC Berkeley</a>
+              <a href="">UC Davis</a>
+              <a href="">UC Irvine</a>
+              <a href="">UCLA</a>
+              <a href="">UC Merced</a>
+              <a href="">UC Riverside</a>
+              <a href="">UC San Diego</a>
+              <a href="">UC San Francisco</a>
+              <a href="">UC Santa Barbara</a>
+              <a href="">UC Santa Cruz</a>
+              <a href="">UC Office of the President</a>
+              <a href="">UC Press</a>
+            </div>
+          </details>
+        </div>
       </div>
     )
   }
