5533748db5a52a599da8d68e4f393e77fdd93d1c
JoelUCOP <joel.hagedorn@ucop.edu>
2016-10-26 11:21:28 -0700

    remove unused campus comp container <div>

--- app/jsx/components/CampusComp.jsx
+++ app/jsx/components/CampusComp.jsx
@@ -10,26 +10,24 @@ class CampusComp extends React.Component {
         <h2 className="c-campus__heading">
           UC Berkeley
         </h2>
-        <div className="c-campus__menu">
-          <details open>
-            <summary>[arrow]</summary>
-            <div className="c-campus__menu-items">
-              <div>eScholarship at &hellip;</div>
-              <a href="">UC Berkeley</a>
-              <a href="">UC Davis</a>
-              <a href="">UC Irvine</a>
-              <a href="">UCLA</a>
-              <a href="">UC Merced</a>
-              <a href="">UC Riverside</a>
-              <a href="">UC San Diego</a>
-              <a href="">UC San Francisco</a>
-              <a href="">UC Santa Barbara</a>
-              <a href="">UC Santa Cruz</a>
-              <a href="">UC Office of the President</a>
-              <a href="">UC Press</a>
-            </div>
-          </details>
-        </div>
+        <details open className="c-campus__menu">
+          <summary>[arrow]</summary>
+          <div className="c-campus__menu-items">
+            <div>eScholarship at &hellip;</div>
+            <a href="">UC Berkeley</a>
+            <a href="">UC Davis</a>
+            <a href="">UC Irvine</a>
+            <a href="">UCLA</a>
+            <a href="">UC Merced</a>
+            <a href="">UC Riverside</a>
+            <a href="">UC San Diego</a>
+            <a href="">UC San Francisco</a>
+            <a href="">UC Santa Barbara</a>
+            <a href="">UC Santa Cruz</a>
+            <a href="">UC Office of the President</a>
+            <a href="">UC Press</a>
+          </div>
+        </details>
       </div>
     )
   }
