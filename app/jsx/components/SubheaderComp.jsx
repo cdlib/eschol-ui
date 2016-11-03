fb8953c5380b5d13c1072f6c0f273c12e5195bfd
JoelUCOP <joel.hagedorn@ucop.edu>
2016-11-03 14:09:50 -0700

    import campus comp into subheader comp

--- app/jsx/components/SubheaderComp.jsx
+++ app/jsx/components/SubheaderComp.jsx
@@ -1,13 +1,14 @@
 // ##### Subheader Component ##### //
 
 import React from 'react'
+import CampusComp from '../components/CampusComp.jsx'
 
 class SubheaderComp extends React.Component {
   render() {
     return (
       <div className="c-subheader">
         <div className="c-subheader__campus">
-          [campus menu here]
+          <CampusComp />
         </div>
         <div className="c-subheader__nav">
           [global nav here]
@@ -16,7 +17,7 @@ class SubheaderComp extends React.Component {
           [deposit buttons here]
         </div>
         <div className="c-subheader__banner">
-          <img className="c-subheader__banner-image" src="http://placehold.it/500x100?text=Campus Logo Here" alt=""/>
+          <img className="c-subheader__banner-image" src="http://placehold.it/400x100?text=Campus Logo Here" alt=""/>
         </div>
       </div>
     )
