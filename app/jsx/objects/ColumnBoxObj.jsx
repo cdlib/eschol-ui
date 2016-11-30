12a26c0363936f322a3e8cf845ba9b993aa9d7cc
JoelUCOP <joel.hagedorn@ucop.edu>
2016-11-30 15:51:13 -0800

    create column boxes without section headers; refactor inset drop-shadow

--- app/jsx/objects/ColumnBoxObj.jsx
+++ app/jsx/objects/ColumnBoxObj.jsx
@@ -22,6 +22,16 @@ class ColumnBoxObj extends React.Component {
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias sed sapiente nulla debitis nobis asperiores hic est, fuga illo? Accusamus molestias pariatur nihil, libero possimus optio iusto nobis sapiente ad.
           </p>
         </section>
+        <h2>Main Column Box Without Section Header</h2>
+        <section className="o-columnbox-main">
+          <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod tempora reiciendis odit. Ducimus similique nulla aliquam praesentium. Pariatur, accusamus deserunt, tempore voluptates ducimus vitae quod, distinctio similique tempora ipsum repellat.
+          </div>
+        </section>
+        <h2>Sidebar Column Box Without Section Header</h2>
+        <section className="o-columnbox-sidebar">
+          <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt quasi molestiae praesentium at dignissimos inventore in, error! Temporibus, facere sed fuga sapiente expedita ab, quos ipsam obcaecati in cupiditate maxime?
+          </div>
+        </section>
       </div>
     )
   }
