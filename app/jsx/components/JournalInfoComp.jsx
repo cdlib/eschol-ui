cf2b419c038ad87902d29165be206bc4c6bf7ef0
JoelUCOP <joel.hagedorn@ucop.edu>
2016-12-07 13:39:53 -0800

    add journalinfo comp files

---
+++ app/jsx/components/JournalInfoComp.jsx
@@ -0,0 +1,15 @@
+// ##### Journal Information Component ##### //
+
+import React from 'react'
+
+class JournalInfoComp extends React.Component {
+  render() {
+    return (
+      <div className="c-journalinfo">
+      [journal information comp to go here]
+      </div>
+    )
+  }
+}
+
+module.exports = JournalInfoComp;
