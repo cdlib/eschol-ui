3e79a730ee2e6238dc280866e72f789c1da0eb4f
JoelUCOP <joel.hagedorn@ucop.edu>
2016-12-07 11:43:00 -0800

    add sample article content to journal layout columnbox

--- app/jsx/layouts/JournalLayout.jsx
+++ app/jsx/layouts/JournalLayout.jsx
@@ -44,8 +44,12 @@ class JournalLayout extends React.Component {
             <section className="o-columnbox2">
               <header>
                 <h2 className="o-columnbox2__heading">Featured Articles</h2>
-              </header>
-              [content to go here]
+                </header>
+                <p><a className="o-textlink__secondary" href="">Entre la ficción y el periodismo: Cambio social y la crónica mexicana contemporánea</a> <br/> Nadeau, Evelyn</p> 
+                <p><a className="o-textlink__secondary" href="">Journalism in Catalonia During Francoism</a> <br/> Reguant, Monserrat</p>
+                <p><a className="o-textlink__secondary" href="">En torno a un cuento olvidado de Clarín: "El oso mayor"</a> <br/> Gil, Angeles Ezama</p>
+                <p><a className="o-textlink__secondary" href="">Interview with Guillermo Cabrera Infante</a> <br/> Graham-Jones, Jean; Deosthale, Duleep</p>
+                <p><a className="o-textlink__secondary" href="">Lazlo Moussong. Castillos en la letra. Xalapa, México: Universidad Veracruzana, 1986.</a> <br/> Radchik, Laura</p>
             </section>
             <section className="o-columnbox2">
               <header>
