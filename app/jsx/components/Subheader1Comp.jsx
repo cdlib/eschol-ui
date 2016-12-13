d0d3cacd078cb75970e663b6e32aa4d13997b178
JoelUCOP <joel.hagedorn@ucop.edu>
2016-12-12 16:05:57 -0800

    remove c-subheader__button classes from subheader comps

--- app/jsx/components/Subheader1Comp.jsx
+++ app/jsx/components/Subheader1Comp.jsx
@@ -12,7 +12,7 @@ class Subheader1Comp extends React.Component {
           <CampusSelectorComp />
           <img className="c-subheader__banner" src="http://placehold.it/400x100?text=Campus Logo Here" alt=""/>
           <div className="c-subheader__buttons">
-            <button className="o-button__3 c-subheader__button">Deposit</button>
+            <button className="o-button__3">Deposit</button>
           </div>
         </div>
         <div className="c-subheader__row2">
--- app/jsx/components/Subheader2Comp.jsx
+++ app/jsx/components/Subheader2Comp.jsx
@@ -12,8 +12,8 @@ class Subheader2Comp extends React.Component {
           <CampusSelectorComp />
           <img className="c-subheader__banner" src="http://placehold.it/400x100?text=Campus Logo Here" alt=""/>
           <div className="c-subheader__buttons">
-            <button className="o-button__3 c-subheader__button">Submit</button>
-            <button className="o-button__3 c-subheader__button">Manage <span className="c-subheader__button-fragment">Submissions</span></button>
+            <button className="o-button__3">Submit</button>
+            <button className="o-button__3">Manage <span className="c-subheader__button-fragment">Submissions</span></button>
           </div>
         </div>
         <div className="c-subheader__row2">
