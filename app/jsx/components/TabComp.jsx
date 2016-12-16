98b5435af7926514512cd3cacb5d85c055014851
JoelUCOP <joel.hagedorn@ucop.edu>
2016-12-15 16:09:45 -0800

    begin 2nd tab comp refactor

--- app/jsx/components/TabComp.jsx
+++ app/jsx/components/TabComp.jsx
@@ -6,31 +6,36 @@ class TabComp extends React.Component {
   render() {
     return (
       <div className="c-tab">
-        <details open>
-          <summary>Main Content</summary>
-          <div className="c-tab__content">
-          Main content here. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea voluptas accusamus provident suscipit at voluptatum quod eum, facere iure ratione quaerat ex laudantium perspiciatis saepe porro et fugit quisquam doloremque!</div>
-        </details>
-        <details>
-          <summary>Supplemental material</summary>
-          <div className="c-tab__content">
-          Supplemental Material here. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere beatae quidem, eius accusantium officia in incidunt, hic totam eveniet vero, quam veniam nulla iure nesciunt non modi quaerat repellendus delectus.</div>
-        </details>
-        <details>
-          <summary>Metrics</summary>
-          <div className="c-tab__content">
-          Metrics here. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia architecto qui numquam, id, maxime molestiae autem officia deleniti magnam adipisci, suscipit a laborum quaerat cumque, vel minima! Sunt autem, illo.</div>
-        </details>
-        <details>
-          <summary>Author & Article Info</summary>
-          <div className="c-tab__content">
-          Author & Article Info here. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores voluptatibus, nihil maiores voluptates nulla reiciendis, nam vitae. Sit, adipisci voluptatibus totam nemo quod, repellat deserunt. Hic officia a incidunt eaque.</div>
-        </details>
-        <details>
-          <summary>Comments (2)</summary>
-          <div className="c-tab__content">
-          Contents here. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus quam, explicabo enim deserunt iure, in nesciunt libero facere architecto maxime pariatur, consequuntur at cumque quos praesentium, mollitia nostrum recusandae eaque.</div>
-        </details>
+        <div className="c-tab__list">
+          <button aria-selected="true">Main Content</button>
+          <button aria-selected="false">Supplemental material</button>
+          <details className="c-tab__details">
+            <summary>...</summary>
+            <button aria-selected="false">Metrics</button>
+            <button aria-selected="false">Author & Article Info</button>
+            <button aria-selected="false">Comments (2)</button>
+          </details>
+        </div>
+        <div className="c-tab__panel" aria-labelledby="">
+          <h2>Main Content</h2>
+          Main content here. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea voluptas accusamus provident suscipit at voluptatum quod eum, facere iure ratione quaerat ex laudantium perspiciatis saepe porro et fugit quisquam doloremque!
+        </div>
+        <div className="c-tab__panel" aria-labelledby="">
+          <h2>Supplemental material</h2>
+          Supplemental material here. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat doloremque id aliquid obcaecati recusandae magnam ratione illo aspernatur at itaque. Sapiente nihil in officiis, soluta incidunt ab iusto maiores sit!
+        </div>
+        <div className="c-tab__panel" aria-labelledby="">
+          <h2>Metrics</h2>
+          Metrics here. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum saepe reiciendis at nihil in modi esse ipsam rem veritatis quasi eum facere similique, impedit asperiores illo error. Delectus, temporibus expedita.
+        </div>
+        <div className="c-tab__panel" aria-labelledby="">
+          <h2>Author & Article Info</h2>
+          Author & Article Info here. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima labore illo expedita. Excepturi porro animi ipsam facilis dignissimos assumenda fugit quod ullam inventore, accusamus non beatae enim sunt eveniet eum.
+        </div>
+        <div className="c-tab__panel" aria-labelledby="">
+          <h2>Comments</h2>
+          Comments. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque provident, pariatur amet nobis facere deserunt, est suscipit adipisci dolorem. Esse laborum dolorem iusto, nostrum animi praesentium maxime minus temporibus blanditiis!
+        </div>
       </div>
     )
   }
