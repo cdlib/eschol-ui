779a1c093d5b91bd9d71e3cc7786ea4b331baa9e
JoelUCOP <joel.hagedorn@ucop.edu>
2016-12-19 16:24:45 -0800

    add headings to tab comps 1 - 5

--- app/jsx/components/Tab4Comp.jsx
+++ app/jsx/components/Tab4Comp.jsx
@@ -6,7 +6,7 @@ class Tab4Comp extends React.Component {
   render() {
     return (
       <div className="c-tab4">
-        Tab 4 content to go here. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti numquam at dolore eligendi impedit blanditiis odio ratione nihil, aspernatur saepe tempore sit, voluptatum minus similique magni expedita vitae reprehenderit tempora!
+        <h1 tabindex="-1">Tab 4 content to go here</h1> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti numquam at dolore eligendi impedit blanditiis odio ratione nihil, aspernatur saepe tempore sit, voluptatum minus similique magni expedita vitae reprehenderit tempora!
       </div>
     )
   }
