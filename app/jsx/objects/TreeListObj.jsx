de80339eaa883a42b19251e33952b28885e079d9
JoelUCOP <joel.hagedorn@ucop.edu>
2016-12-21 15:48:43 -0800

    create tree obj svgs, html, and styles

--- app/jsx/objects/TreeListObj.jsx
+++ app/jsx/objects/TreeListObj.jsx
@@ -6,7 +6,13 @@ class TreeListObj extends React.Component {
   render() {
     return (
       <div className="o-treelist">
-        Tree list object to go here.
+        <a id="o-treelist__label" className="o-textlink__secondary" href="">Article</a>
+        <ul className="o-treelist__list" aria-labelledby="o-treelist__label">
+          <li><a className="o-textlink__secondary" href="">Abstract</a></li>
+          <li><a className="o-textlink__secondary" href="">Main Content</a></li>
+          <li><a className="o-textlink__secondary" href="">References</a></li>
+          <li><a className="o-textlink__secondary" href="">Author Response</a></li>
+        </ul>
       </div>
     )
   }
