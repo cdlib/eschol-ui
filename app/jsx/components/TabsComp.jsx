cf791efb82ec72d2ab5f31f4dc61f949e8688357
JoelUCOP <joel.hagedorn@ucop.edu>
2016-12-21 10:43:20 -0800

    create tabs comp classes for each tab

--- app/jsx/components/TabsComp.jsx
+++ app/jsx/components/TabsComp.jsx
@@ -27,14 +27,14 @@ class TabsComp extends React.Component {
     return (
       <div className="c-tabs">
         <div className="c-tabs__main-list">
-          <button onClick = {()=> this.setState({currentTab: 1})}>Main Content</button>
-          <button onClick = {()=> this.setState({currentTab: 2})}>Supplemental material</button>
+          <button className="c-tabs__button" onClick = {()=> this.setState({currentTab: 1})}>Main Content</button>
+          <button className="c-tabs__button" onClick = {()=> this.setState({currentTab: 2})}>Supplemental material</button>
           <details open={this.state.isOpen ? "open" : ""} className="c-tabs__sub-list">
             <summary>...</summary>
             <div className="c-tabs__details-buttons">
-              <button onClick = {()=> this.setState({currentTab: 3})}>Metrics</button>
-              <button onClick = {()=> this.setState({currentTab: 4})}>Author & Article Info</button>
-              <button onClick = {()=> this.setState({currentTab: 5})}>Comments (2)</button>
+              <button className="c-tabs__button" onClick = {()=> this.setState({currentTab: 3})}>Metrics</button>
+              <button className="c-tabs__button" onClick = {()=> this.setState({currentTab: 4})}>Author & Article Info</button>
+              <button className="c-tabs__button" onClick = {()=> this.setState({currentTab: 5})}>Comments (2)</button>
             </div>
           </details>
         </div>
