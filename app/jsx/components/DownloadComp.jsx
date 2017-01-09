1303e4593a3654dd16e8f56b1f57cd43c7f118f1
JoelUCOP <joel.hagedorn@ucop.edu>
2017-01-09 15:39:53 -0800

    shorten and rename download comp classes

--- app/jsx/components/DownloadComp.jsx
+++ app/jsx/components/DownloadComp.jsx
@@ -10,27 +10,27 @@ class DownloadComp extends React.Component {
           Download PDF
         </summary>
         <div className="c-download__menu">
-          <div className="c-download__menu-heading" id="c-download__menu1">
+          <div className="c-download__heading" id="c-download__list1">
             Main
           </div>
-          <ul className="c-download__menu-list" aria-labelledby="c-download__menu1">
+          <ul className="c-download__list" aria-labelledby="c-download__list1">
             <li><a href="">PDF</a></li>
             <li><a href="">ePub</a></li>
             <li><a href="">HTML</a></li>
           </ul>
-          <div className="c-download__menu-heading" id="c-download__menu2">
+          <div className="c-download__heading" id="c-download__list2">
             Citation
           </div>
-          <ul className="c-download__menu-list" aria-labelledby="c-download__menu2">
+          <ul className="c-download__list" aria-labelledby="c-download__list2">
             <li><a href="">RIS</a></li>
             <li><a href="">BibText</a></li>
             <li><a href="">EndNote</a></li>
             <li><a href="">RefWorks</a></li>
           </ul>
-          <div className="c-download__menu-heading" id="c-download__menu3">
+          <div className="c-download__heading" id="c-download__list3">
             Supplemental Material
           </div>
-          <ul className="c-download__menu-list" aria-labelledby="c-download__menu3">
+          <ul className="c-download__list" aria-labelledby="c-download__list3">
             <li><a href="">Image</a></li>
             <li><a href="">Audio</a></li>
             <li><a href="">Video</a></li>
