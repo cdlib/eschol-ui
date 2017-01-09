f44a90aba865ade2f4af1b4747715b5e7b397063
JoelUCOP <joel.hagedorn@ucop.edu>
2017-01-09 11:52:18 -0800

    remove parent <div> from download comp

--- app/jsx/components/DownloadComp.jsx
+++ app/jsx/components/DownloadComp.jsx
@@ -5,19 +5,17 @@ import React from 'react'
 class DownloadComp extends React.Component {
   render() {
     return (
-      <div className="c-download">
-        <select name="" id="">
-          <optgroup label="Main Content">
-            <option value="">Download PDF</option>
-            <option value="">One</option>
-          </optgroup>
-          <optgroup label="Citation">
-            <option value="">Two</option>
-            <option value="">Three</option>
-            <option value="">Four</option>
-          </optgroup>
-        </select>
-      </div>
+      <select className="c-download" name="" id="">
+        <optgroup label="Main Content">
+          <option value="">Download PDF</option>
+          <option value="">One</option>
+        </optgroup>
+        <optgroup label="Citation">
+          <option value="">Two</option>
+          <option value="">Three</option>
+          <option value="">Four</option>
+        </optgroup>
+      </select>
     )
   }
 }
