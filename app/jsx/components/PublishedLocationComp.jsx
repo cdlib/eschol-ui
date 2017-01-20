3942838fa2eeb99a3b9523bdc10e5d5275101e6d
JoelUCOP <joel.hagedorn@ucop.edu>
2017-01-19 16:33:31 -0800

    create published location comp html & styles

--- app/jsx/components/PublishedLocationComp.jsx
+++ app/jsx/components/PublishedLocationComp.jsx
@@ -6,7 +6,13 @@ class PublishedLocationComp extends React.Component {
   render() {
     return (
       <div className="c-publishedlocation">
-      Published Location component to go here]
+        <div className="c-publishedlocation__location">
+          <p><a className="o-textlink__secondary" href="">Published Web Location</a></p>
+          <p>No data is associated with this publication.</p>
+        </div>
+        <a href="" className="c-publishedlocation__license">
+          <img src="../images/icon_cc-by-nc.svg" alt=""/>
+        </a>
       </div>
     )
   }
