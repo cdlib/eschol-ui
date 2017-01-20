e2e757b3d3a95ae0e1e4875ded88b0b569d68d13
JoelUCOP <joel.hagedorn@ucop.edu>
2017-01-20 12:29:53 -0800

    create toggle content comp html & styles

--- app/jsx/components/ToggleContentComp.jsx
+++ app/jsx/components/ToggleContentComp.jsx
@@ -5,9 +5,10 @@ import React from 'react'
 class ToggleContentComp extends React.Component {
   render() {
     return (
-      <div className="togglecontent">
-      [toggle content component to go here]
-      </div>
+      <details className="c-togglecontent">
+        <summary><h2>Main Content</h2></summary>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum nihil quidem nisi sapiente vel obcaecati esse vero placeat! Dicta fugiat quam, nemo accusamus cum, non voluptas sit eveniet. Voluptates odio consectetur eum dicta vel at rerum accusamus inventore officiis, voluptate deleniti et reiciendis aliquam illo eaque laborum neque excepturi pariatur unde obcaecati molestiae dolores voluptas non earum minima. Libero dolores rerum nesciunt deserunt incidunt, aspernatur similique fugit beatae quis impedit corrupti, voluptate, unde facilis. Voluptatibus labore sunt maxime, accusantium animi mollitia ducimus. Vel ipsum reiciendis, a voluptates delectus. Itaque perferendis debitis odit sed. Earum vero aut laborum numquam accusantium dolores!
+      </details>
     )
   }
 }
