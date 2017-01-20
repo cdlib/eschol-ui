0566c2eb18646a23cb38b96a713a436ce64681dd
JoelUCOP <joel.hagedorn@ucop.edu>
2017-01-19 16:42:14 -0800

    add published location comp base & screen1 styles

--- app/jsx/components/PublishedLocationComp.jsx
+++ app/jsx/components/PublishedLocationComp.jsx
@@ -8,7 +8,7 @@ class PublishedLocationComp extends React.Component {
       <div className="c-publishedlocation">
         <div className="c-publishedlocation__location">
           <p><a className="o-textlink__secondary" href="">Published Web Location</a></p>
-          <p>No data is associated with this publication.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus rem ad sequi quia aut nisi, facilis harum ducimus eos itaque consequuntur! Ad esse aliquam voluptatibus ut eveniet optio, mollitia voluptas?</p>
         </div>
         <a href="" className="c-publishedlocation__license">
           <img src="../images/icon_cc-by-nc.svg" alt=""/>
