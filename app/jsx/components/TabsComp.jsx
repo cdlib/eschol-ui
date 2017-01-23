ef53e810b61523360569e1626ae6c127772471da
JoelUCOP <joel.hagedorn@ucop.edu>
2017-01-23 15:26:29 -0800

    begin refactoring tabs comp using grid

--- app/jsx/components/TabsComp.jsx
+++ app/jsx/components/TabsComp.jsx
@@ -29,14 +29,10 @@ class TabsComp extends React.Component {
         <div className="c-tabs__main-list">
           <button className={this.state.currentTab === 1 ? "c-tabs__button--active" : "c-tabs__button"} onClick = {()=> this.setState({currentTab: 1})}>Main Content</button>
           <button className={this.state.currentTab === 2 ? "c-tabs__button--active" : "c-tabs__button"} onClick = {()=> this.setState({currentTab: 2})}>Supplemental material</button>
-          <details open={this.state.isOpen ? "open" : ""} className="c-tabs__sub-list">
-            <summary>...</summary>
-            <div className="c-tabs__details-buttons">
-              <button className={this.state.currentTab === 3 ? "c-tabs__button--active" : "c-tabs__button"} onClick = {()=> this.setState({currentTab: 3})}>Metrics</button>
-              <button className={this.state.currentTab === 4 ? "c-tabs__button--active" : "c-tabs__button"} onClick = {()=> this.setState({currentTab: 4})}>Author & Article Info</button>
-              <button className={this.state.currentTab === 5 ? "c-tabs__button--active" : "c-tabs__button"} onClick = {()=> this.setState({currentTab: 5})}>Comments (2)</button>
-            </div>
-          </details>
+          <button className={this.state.currentTab === 3 ? "c-tabs__button--active" : "c-tabs__button"} onClick = {()=> this.setState({currentTab: 3})}>Metrics</button>
+          <button className={this.state.currentTab === 4 ? "c-tabs__button--active" : "c-tabs__button"} onClick = {()=> this.setState({currentTab: 4})}>Author & Article Info</button>
+          <button className={this.state.currentTab === 5 ? "c-tabs__button--active" : "c-tabs__button"} onClick = {()=> this.setState({currentTab: 5})}>Comments (2)</button>
+          <button className="c-tabs__button-more">...</button>
         </div>
         <div className="c-tabs__panel">
           {this.state.currentTab === 1 ? <Tab1Comp /> : null}
