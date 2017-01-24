1020fe1f88aee9a4d588b3d747959d8c194ea36f
JoelUCOP <joel.hagedorn@ucop.edu>
2017-01-24 10:46:34 -0800

    add nth-child button styles and tabs set.State jsx

--- app/jsx/components/TabsComp.jsx
+++ app/jsx/components/TabsComp.jsx
@@ -1,7 +1,6 @@
 // ##### Tabs Component ##### //
 
 import React from 'react'
-import Breakpoints from '../../js/breakpoints.json'
 import Tab1Comp from '../components/Tab1Comp.jsx'
 import Tab2Comp from '../components/Tab2Comp.jsx'
 import Tab3Comp from '../components/Tab3Comp.jsx'
@@ -11,28 +10,18 @@ import Tab5Comp from '../components/Tab5Comp.jsx'
 class TabsComp extends React.Component {
   constructor(props){
     super(props)
-    this.state = {currentTab: 1}
-  }
-  componentWillMount() {
-    if (matchMedia) {
-      this.mq = matchMedia("(min-width:"+Breakpoints.screen3+")")
-      this.mq.addListener(this.widthChange)
-      this.widthChange()
-    }
-  }
-  widthChange = ()=> {
-    this.setState({isOpen: this.mq.matches})
+    this.state = {currentTab: 1, moreTabs: false}
   }
   render() {
     return (
       <div className="c-tabs">
-        <div className="c-tabs__main-list">
+        <div className={this.state.moreTabs ? "c-tabs__tabs--show-all" : "c-tabs__tabs"}>
+          <button className="c-tabs__button-more" onClick = {()=> this.setState({moreTabs: true})} aria-label="Show all tabs">...</button>
           <button className={this.state.currentTab === 1 ? "c-tabs__button--active" : "c-tabs__button"} onClick = {()=> this.setState({currentTab: 1})}>Main Content</button>
           <button className={this.state.currentTab === 2 ? "c-tabs__button--active" : "c-tabs__button"} onClick = {()=> this.setState({currentTab: 2})}>Supplemental material</button>
           <button className={this.state.currentTab === 3 ? "c-tabs__button--active" : "c-tabs__button"} onClick = {()=> this.setState({currentTab: 3})}>Metrics</button>
           <button className={this.state.currentTab === 4 ? "c-tabs__button--active" : "c-tabs__button"} onClick = {()=> this.setState({currentTab: 4})}>Author & Article Info</button>
           <button className={this.state.currentTab === 5 ? "c-tabs__button--active" : "c-tabs__button"} onClick = {()=> this.setState({currentTab: 5})}>Comments (2)</button>
-          <button className="c-tabs__button-more">...</button>
         </div>
         <div className="c-tabs__panel">
           {this.state.currentTab === 1 ? <Tab1Comp /> : null}
