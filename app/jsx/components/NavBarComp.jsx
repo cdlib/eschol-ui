293e8a2c2b4f3359225d61cc2f06e08f72619847
JoelCDL <joel.hagedorn@ucop.edu>
2017-01-25 15:25:46 -0800

    import nav, social icons and language comps into nav bar

--- app/jsx/components/NavBarComp.jsx
+++ app/jsx/components/NavBarComp.jsx
@@ -1,12 +1,17 @@
 // ##### Nav Bar Component ##### //
 
 import React from 'react'
+import LanguageComp from '../components/LanguageComp.jsx'
+import NavComp from '../components/NavComp.jsx'
+import SocialIconsComp from '../components/SocialIconsComp.jsx'
 
 class NavBarComp extends React.Component {
   render() {
     return (
       <div className="c-navbar">
-        [Nav Bar Component to go here]
+        <NavComp />
+        <SocialIconsComp />
+        <LanguageComp />
       </div>
     )
   }
