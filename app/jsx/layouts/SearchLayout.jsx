6b8a4d42ac8f3317c4db1e62d2842ab97813fd8d
JoelUCOP <joel.hagedorn@ucop.edu>
2017-01-27 11:57:10 -0800

    add customized nav comp to 'search' layout

--- app/jsx/layouts/SearchLayout.jsx
+++ app/jsx/layouts/SearchLayout.jsx
@@ -2,7 +2,9 @@
 
 import React from 'react'
 import Header1Comp from '../components/Header1Comp.jsx'
-import Nav1Comp from '../components/Nav1Comp.jsx'
+import NavComp from '../components/NavComp.jsx'
+import NavSubComp from '../components/NavSubComp.jsx'
+import Breakpoints from '../../js/breakpoints.json'
 import ExportComp from '../components/ExportComp.jsx'
 import FilterComp from '../components/FilterComp.jsx'
 import CheckboxComp from '../components/CheckboxComp.jsx'
@@ -13,11 +15,50 @@ import ScholWorksComp from '../components/ScholWorksComp.jsx'
 import FooterComp from '../components/FooterComp.jsx'
 
 class SearchLayout extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {submenuActive: null}
+  }
+  componentWillMount() {
+    if (matchMedia) {
+      this.mq = matchMedia("(min-width:"+Breakpoints.screen3+")")
+      this.mq.addListener(this.widthChange)
+      this.widthChange()
+    }
+  }
+  widthChange = ()=> {
+    this.setState({isOpen: this.mq.matches})
+  }
   render() {
     return (
       <div className="l-search">
         <Header1Comp />
-        <Nav1Comp />
+        <div className="c-navbar">
+          <nav className="c-nav">
+            <details open={this.state.isOpen ? "open" : ""} className="c-nav__main">
+              <summary className="c-nav__main-button">Menu
+              </summary>
+              <div className={this.state.submenuActive ? "c-nav__main-items--submenu-active" : "c-nav__main-items"} role="list">
+                <NavSubComp name="Campus Sites" open={this.state.submenuActive == 1} onSubmenuChanged={(flag)=> this.setState({submenuActive:flag ? 1 : null})}>
+                  <a href="" role="listitem">UC Berkeley</a>
+                  <a href="" role="listitem">UC Davis</a>
+                  <a href="" role="listitem">UC Irvine</a>
+                  <a href="" role="listitem">UCLA</a>
+                  <a href="" role="listitem">UC Merced</a>
+                  <a href="" role="listitem">UC Riverside</a>
+                  <a href="" role="listitem">UC San Diego</a>
+                  <a href="" role="listitem">UC San Francisco</a>
+                  <a href="" role="listitem">UC Santa Barbara</a>
+                  <a href="" role="listitem">UC Santa Cruz</a>
+                  <a href="" role="listitem">UC Office of the President</a>
+                  <a href="" role="listitem">UC Press</a>
+                </NavSubComp>
+                <a href="" role="listitem" className="c-nav__item--active">UC Open Access Policies</a>
+                <a href="" role="listitem">eScholarship Publishing</a>
+              </div>
+            </details>
+          </nav>
+        </div>
         <ExportComp />
         <div className="c-columns">
           <aside>
