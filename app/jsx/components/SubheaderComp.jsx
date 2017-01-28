fe1742b995cb6ae981cc7976da57302c804cc8b9
JoelUCOP <joel.hagedorn@ucop.edu>
2017-01-27 17:01:44 -0800

    add sample subheader banner and styles

--- app/jsx/components/SubheaderComp.jsx
+++ app/jsx/components/SubheaderComp.jsx
@@ -8,7 +8,7 @@ class SubheaderComp extends React.Component {
     return (
       <div className="c-subheader">
         <CampusSelectorComp />
-        <img className="c-subheader__banner" src="http://placehold.it/400x100?text=Campus Logo Here" alt=""/>
+        <img className="c-subheader__banner" src="images/temp_journal-banner.png" alt=""/>
         <div className="c-subheader__buttons">
           <button className="o-button__3">Submit</button>
           <button className="o-button__3">Manage <span className="c-subheader__button-fragment">Submissions</span></button>
