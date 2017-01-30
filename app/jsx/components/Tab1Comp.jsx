53bfec3093fcbdad2786062e68b5d9ca8bdc8046
JoelUCOP <joel.hagedorn@ucop.edu>
2017-01-30 14:24:44 -0800

    import components into tab1 comp

--- app/jsx/components/Tab1Comp.jsx
+++ app/jsx/components/Tab1Comp.jsx
@@ -1,12 +1,20 @@
 // ##### Tab 1 Component ##### //
 
 import React from 'react'
+import ItemActionsComp from '../components/ItemActionsComp.jsx'
+import AuthorListComp from '../components/AuthorListComp.jsx'
+import PublishedLocationComp from '../components/PublishedLocationComp.jsx'
+import ToggleContentComp from '../components/ToggleContentComp.jsx'
 
 class Tab1Comp extends React.Component {
   render() {
     return (
       <div className="c-tab1">
-        <h1 tabindex="-1">Tab 1 content to go here</h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto nobis maxime officiis aliquid delectus libero neque ullam voluptatibus distinctio quod, illo ab, repudiandae voluptate incidunt dolor consequatur debitis fugiat nostrum.
+        <ItemActionsComp />
+        <h1>From the New Heights: The City and Migrating Latinas in Real Women Have Curves and María Full of Grace</h1>
+          <AuthorListComp />
+          <PublishedLocationComp />
+          <ToggleContentComp />
       </div>
     )
   }
