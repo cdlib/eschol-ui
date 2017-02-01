ec879580c36717e8ae4ce76dc8baefca9d2cce53
JoelUCOP <joel.hagedorn@ucop.edu>
2017-01-31 16:35:11 -0800

    add sample content to related items in journal item layout

--- app/jsx/layouts/JournalItemLayout.jsx
+++ app/jsx/layouts/JournalItemLayout.jsx
@@ -35,8 +35,10 @@ class JournalItemLayout extends React.Component {
             <section className="o-columnbox2">
               <header>
                 <h2 className="o-columnbox2__heading">Related Items</h2>
-                </header>
-                [content to go here]
+              </header>
+              <p><a className="o-textlink__secondary" href="">Collaborative Film Authorship: Writing Latinas Into the Picture</a><br/>CSW update</p>
+              <p><a className="o-textlink__secondary" href="">Sporting Bodies, Displaying History: Black Embodiment</a><br/>UCLA Electronic Theses and Dissertations</p>
+              <p><a className="o-textlink__secondary" href="">United States-Based Latina Producers of Feature Films</a><br/>UCLA Electronic Theses and Dissertations</p>
             </section>
           </aside>
         </div>
