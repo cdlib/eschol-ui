20273ea5263f0a3e0549ebf8d8fd40afb090fc0f
JoelCDL <joel.hagedorn@ucop.edu>
2017-02-01 18:24:29 -0800

    create initial table comp html & styles

--- app/jsx/components/TableComp.jsx
+++ app/jsx/components/TableComp.jsx
@@ -5,8 +5,35 @@ import React from 'react'
 class TableComp extends React.Component {
   render() {
     return (
-      <table>
-        [table component to go here]
+      <table className="c-table">
+        <thead>
+          <tr>
+            <th scope="col">Quantity</th>
+            <th scope="col">Apples</th>
+            <th scope="col">Oranges</th>
+            <th scope="col">Pears</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">One</th>
+            <td>Odio</td>
+            <td>Lorem</td>
+            <td>Optio</td>
+          </tr>
+          <tr>
+            <th scope="row">Two</th>
+            <td>Iusto</td>
+            <td>Architecto</td>
+            <td>Lorem</td>
+          </tr>
+          <tr>
+            <th scope="row">Three</th>
+            <td>Lorem</td>
+            <td>Assumenda</td>
+            <td>Quos</td>
+          </tr>
+        </tbody>
       </table>
     )
   }
