e13fbdc5db20c23968e508b610465d059d960db8
JoelUCOP <joel.hagedorn@ucop.edu>
2017-02-03 16:37:40 -0800

    replace nav comp list roles, <div>s with <nav>

--- app/jsx/components/NavComp.jsx
+++ app/jsx/components/NavComp.jsx
@@ -21,37 +21,37 @@ class NavComp extends React.Component {
   }
   render() {
     return (
-      <nav className="c-nav">
+      <div className="c-nav">
         <details open={this.state.isOpen ? "open" : ""} className="c-nav__main">
           <summary className="c-nav__main-button">Menu
           </summary>
-          <div className={this.state.submenuActive ? "c-nav__main-items--submenu-active" : "c-nav__main-items"} role="list">
-            <a href="" role="listitem">Journal Home</a>
+          <nav className={this.state.submenuActive ? "c-nav__main-items--submenu-active" : "c-nav__main-items"}>
+            <a href="">Journal Home</a>
             <NavSubComp name="Issues" open={this.state.submenuActive == 1} onSubmenuChanged={(flag)=> this.setState({submenuActive:flag ? 1 : null})}>
-              <a href="" role="listitem">Volume 41, Issue 1</a>
-              <a href="" role="listitem">Volume 40, Issue 1</a>
-              <a href="" role="listitem">Volume 39, Issue 1</a>
-              <a href="" role="listitem">Volume 38, Issue 1</a>
-              <a href="" role="listitem">Volume 37, Issue 1</a>
-              <a href="" role="listitem">Volume 36, Issue 1</a>
-              <a href="" role="listitem">Volume 35, Issue 1</a>
-              <a href="" role="listitem">Volume 34, Issue 1</a>
-              <a href="" role="listitem">Volume 33, Issue 1</a>
-              <a href="" role="listitem">Volume 32, Issue 1</a>
-              <a href="" role="listitem">Volume 31, Issue 1</a>
-              <a href="" role="listitem">Volume 30, Issue 1</a>
+              <a href="">Volume 41, Issue 1</a>
+              <a href="">Volume 40, Issue 1</a>
+              <a href="">Volume 39, Issue 1</a>
+              <a href="">Volume 38, Issue 1</a>
+              <a href="">Volume 37, Issue 1</a>
+              <a href="">Volume 36, Issue 1</a>
+              <a href="">Volume 35, Issue 1</a>
+              <a href="">Volume 34, Issue 1</a>
+              <a href="">Volume 33, Issue 1</a>
+              <a href="">Volume 32, Issue 1</a>
+              <a href="">Volume 31, Issue 1</a>
+              <a href="">Volume 30, Issue 1</a>
             </NavSubComp>
             <NavSubComp name="About" open={this.state.submenuActive == 2} onSubmenuChanged={(flag)=> this.setState({submenuActive:flag ? 2 : null})}>
-              <a href="" role="listitem">About Us</a>
-              <a href="" role="listitem">Aims &amp; Scope</a>
-              <a href="" role="listitem">Editorial Board</a>
+              <a href="">About Us</a>
+              <a href="">Aims &amp; Scope</a>
+              <a href="">Editorial Board</a>
             </NavSubComp>
-            <a href="" role="listitem" className="c-nav__item--active">Policies</a>
-            <a href="" role="listitem">Submission Guidelines</a>
-            <a href="" role="listitem">Contact</a>
-          </div>
+            <a href=""  className="c-nav__item--active">Policies</a>
+            <a href="">Submission Guidelines</a>
+            <a href="">Contact</a>
+          </nav>
         </details>
-      </nav>
+      </div>
     )
   }
 }
