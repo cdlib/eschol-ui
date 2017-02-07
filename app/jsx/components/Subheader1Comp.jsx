f68f1b40112578a1437395d16971b4903c43b8cd
JoelUCOP <joel.hagedorn@ucop.edu>
2017-02-07 13:56:09 -0800

    refactor subheader1 with imported header2 comp

--- app/jsx/components/Subheader1Comp.jsx
+++ app/jsx/components/Subheader1Comp.jsx
@@ -3,21 +3,14 @@
 import React from 'react'
 import Breakpoints from '../../js/breakpoints.json'
 import NavComp from '../components/NavComp.jsx'
+import Header2Comp from '../components/Header2Comp.jsx'
 
 class Subheader1Comp extends React.Component {
   render() {
     return (
       <div className="c-subheader1">
-        <div className="c-subheader1__logo">
-          <a href="">
-            <picture>
-              <source srcSet="images/logo_escholarship.svg" media={"(min-width: "+Breakpoints.screen3+")"}/>
-              <img src="images/logo_temp-eschol-mobile.png" alt="escholarship"/>
-            </picture>
-          </a>
-        </div>
-        <div className="c-subheader1__search">
-          Search goes here
+        <div className="c-subheader1__header">
+          <Header2Comp />
         </div>
         <div className="c-subheader1__nav">
           <NavComp />
