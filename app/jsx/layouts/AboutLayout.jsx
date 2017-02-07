dc054ba8973475a80cfa437aaa3b84d6e36fd2ab
JoelUCOP <joel.hagedorn@ucop.edu>
2017-02-07 14:53:19 -0800

    add subheader1 comp to about layout

--- app/jsx/layouts/AboutLayout.jsx
+++ app/jsx/layouts/AboutLayout.jsx
@@ -1,7 +1,7 @@
 // ##### About Layout ##### //
 
 import React from 'react'
-import Header1Comp from '../components/Header1Comp.jsx'
+import Header2Comp from '../components/Header2Comp.jsx'
 import NavComp from '../components/NavComp.jsx'
 import NavSubComp from '../components/NavSubComp.jsx'
 import Breakpoints from '../../js/breakpoints.json'
@@ -27,32 +27,39 @@ class AboutLayout extends React.Component {
   render() {
     return (
       <div className="l-about">
-        <Header1Comp />
-        <div className="c-navbar">
-          <nav className="c-nav">
-            <details open={this.state.isOpen ? "open" : ""} className="c-nav__main">
-              <summary className="c-nav__main-button">Menu
-              </summary>
-              <div className={this.state.submenuActive ? "c-nav__main-items--submenu-active" : "c-nav__main-items"} role="list">
-                <NavSubComp name="Campus Sites" open={this.state.submenuActive == 1} onSubmenuChanged={(flag)=> this.setState({submenuActive:flag ? 1 : null})}>
-                  <a href="" role="listitem">UC Berkeley</a>
-                  <a href="" role="listitem">UC Davis</a>
-                  <a href="" role="listitem">UC Irvine</a>
-                  <a href="" role="listitem">UCLA</a>
-                  <a href="" role="listitem">UC Merced</a>
-                  <a href="" role="listitem">UC Riverside</a>
-                  <a href="" role="listitem">UC San Diego</a>
-                  <a href="" role="listitem">UC San Francisco</a>
-                  <a href="" role="listitem">UC Santa Barbara</a>
-                  <a href="" role="listitem">UC Santa Cruz</a>
-                  <a href="" role="listitem">UC Office of the President</a>
-                  <a href="" role="listitem">UC Press</a>
-                </NavSubComp>
-                <a href="" role="listitem" className="c-nav__item--active">UC Open Access Policies</a>
-                <a href="" role="listitem">eScholarship Publishing</a>
-              </div>
-            </details>
-          </nav>
+        <div className="c-subheader1">
+          <div className="c-subheader1__header">
+            <Header2Comp />
+          </div>
+          <div className="c-subheader1__nav">
+            <nav className="c-nav">
+              <details open={this.state.isOpen ? "open" : ""} className="c-nav__main">
+                <summary className="c-nav__main-button">Menu
+                </summary>
+                <div className={this.state.submenuActive ? "c-nav__main-items--submenu-active" : "c-nav__main-items"} role="list">
+                  <NavSubComp name="Campus Sites" open={this.state.submenuActive == 1} onSubmenuChanged={(flag)=> this.setState({submenuActive:flag ? 1 : null})}>
+                    <a href="" role="listitem">UC Berkeley</a>
+                    <a href="" role="listitem">UC Davis</a>
+                    <a href="" role="listitem">UC Irvine</a>
+                    <a href="" role="listitem">UCLA</a>
+                    <a href="" role="listitem">UC Merced</a>
+                    <a href="" role="listitem">UC Riverside</a>
+                    <a href="" role="listitem">UC San Diego</a>
+                    <a href="" role="listitem">UC San Francisco</a>
+                    <a href="" role="listitem">UC Santa Barbara</a>
+                    <a href="" role="listitem">UC Santa Cruz</a>
+                    <a href="" role="listitem">UC Office of the President</a>
+                    <a href="" role="listitem">UC Press</a>
+                  </NavSubComp>
+                  <a href="" role="listitem" className="c-nav__item--active">UC Open Access Policies</a>
+                  <a href="" role="listitem">eScholarship Publishing</a>
+                </div>
+              </details>
+            </nav>
+          </div>
+          <div className="c-subheader1__button">
+            <button className="o-button__3">Get Started</button>
+          </div>
         </div>
         <BreadcrumbComp />
         <div className="c-columns">
