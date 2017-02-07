f4857de1c35671c61b1a2849d4682fdd7821ae74
JoelUCOP <joel.hagedorn@ucop.edu>
2017-02-06 16:54:15 -0800

    create initial subheader1 comp html & styles

--- app/jsx/components/Subheader1Comp.jsx
+++ app/jsx/components/Subheader1Comp.jsx
@@ -1,12 +1,30 @@
 // ##### Subheader 1 Component ##### //
 
 import React from 'react'
+import Breakpoints from '../../js/breakpoints.json'
+import NavComp from '../components/NavComp.jsx'
 
 class Subheader1Comp extends React.Component {
   render() {
     return (
       <div className="c-subheader1">
-        [subheader 1 component to go here]
+        <div className="c-subheader1__logo">
+          <a href="">
+            <picture>
+              <source srcSet="images/logo_escholarship.svg" media={"(min-width: "+Breakpoints.screen3+")"}/>
+              <img src="images/logo_temp-eschol-mobile.png" alt="escholarship"/>
+            </picture>
+          </a>
+        </div>
+        <div className="c-subheader1__search">
+          Search goes here
+        </div>
+        <div className="c-subheader1__nav">
+          <NavComp />
+        </div>
+        <div className="c-subheader1__button">
+          <button className="o-button__3">Get Started</button>
+        </div>
       </div>
     )
   }
