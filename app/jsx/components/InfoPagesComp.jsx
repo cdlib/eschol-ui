41880f36c089f11fcf4506a6ea41231c25892e62
JoelCDL <joel.hagedorn@ucop.edu>
2017-02-08 15:31:41 -0800

    separate infopages item heading into 2 links; update link font sizes

--- app/jsx/components/InfoPagesComp.jsx
+++ app/jsx/components/InfoPagesComp.jsx
@@ -13,24 +13,24 @@ class InfoPagesComp extends React.Component {
       <div className="c-infopages">
         <div className="c-infopages__item">
           <h2>
-            <a href="">The Center for Environmental Design Research <span className="c-infopages__heading-title">In the Press</span>
-            </a>
+            <a href="">The Center for Environmental Design Research</a>
+            <a href="" className="c-infopages__heading-title">In the Press</a>
           </h2>
           <div className="c-infopages__text">The University of California awards contract to Symplectic for the implementation of a publication harvesting system to support UC’s Open Access Policy" March, 2014
           </div>
         </div>
         <div className="c-infopages__item">
           <h2>
-            <a href="">eScholarship <span className="c-infopages__heading-title">Content on this Site: Frequently Asked Questions</span>
-            </a>
+            <a href="">eScholarship</a>
+            <a href="" className="c-infopages__heading-title">Content on this Site: Frequently Asked Questions</a>
           </h2>
           <div className="c-infopages__text">eScholarship provides open access, scholarly publishing services to the University of California and delivers a dynamic research platform to scholars worldwide. Learn more about the digital publishing services provided by&hellip;
           </div>
         </div>
         <div className="c-infopages__item">
           <h2>
-            <a href="">UC Berkeley <span className="c-infopages__heading-title">Content on this Site: Frequently Asked Questions</span>
-            </a>
+            <a href="">UC Berkeley</a>
+            <a href="" className="c-infopages__heading-title">Content on this Site: Frequently Asked Questions</a>
           </h2>
           <div className="c-infopages__text">eScholarship provides open access, scholarly publishing services to the University of California and delivers a dynamic research platform to scholars worldwide. Learn more about the digital publishing services provided by eScholarship&hellip;
           </div>
