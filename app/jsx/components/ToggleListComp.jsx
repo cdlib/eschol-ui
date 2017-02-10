f3062f9e9d24b66a8852fa3ddd74b7dbe1038ed2
JoelUCOP <joel.hagedorn@ucop.edu>
2017-02-10 15:25:19 -0800

    add togglelist buttons

--- app/jsx/components/ToggleListComp.jsx
+++ app/jsx/components/ToggleListComp.jsx
@@ -5,33 +5,39 @@ import React from 'react'
 class ToggleListComp extends React.Component {
   render() {
     return (
-      <ul className="c-togglelist">
-        <li><a href="">Bourns College of Engineering</a></li>
-        <li><a href="">Center for Environmental Design Research</a></li>
-        <li><a href="">California Academic Partnership Program</a></li>
-        <li>
-          <details className="c-togglelist__sublist">
-            <summary>California Center for Population Research</summary>
-            <ul>
-              <li><a href="">Safe Transportation Research & Education Center</a></li>
-              <li><a href="">UC Berkeley Center for Future Urban Transport: A Volvo Center of Excellence</a></li>
-              <li>
-                <details className="c-togglelist__sublist">
-                  <summary>UC Berkeley Transportation Sustainability Research Center</summary>
-                  <ul>
-                    <li><a href="">Center for Latino Policy Research</a></li>
-                    <li><a href="">Center for Research on Native American Issues</a></li>
-                    <li><a href="">Center for Right-Wing Studies</a></li>
-                  </ul>
-                </details>
-              </li>
-            </ul>
-          </details>
-        </li>
-        <li><a href="">California Community College Collaborative (C4)</a></li>
-        <li><a href="">California Digital Library</a></li>
-        <li><a href="">California Health Benefits Review Program</a></li>
-      </ul>
+      <div className="c-togglelist">
+        <div className="c-togglelist__buttons">
+          <button className="o-button__2">Expand All</button>
+          <button className="o-button__2">Collapse All</button>
+        </div>
+        <ul className="c-togglelist__mainlist">
+          <li><a href="">Bourns College of Engineering</a></li>
+          <li><a href="">Center for Environmental Design Research</a></li>
+          <li><a href="">California Academic Partnership Program</a></li>
+          <li>
+            <details className="c-togglelist__sublist">
+              <summary>California Center for Population Research</summary>
+              <ul>
+                <li><a href="">Safe Transportation Research & Education Center</a></li>
+                <li><a href="">UC Berkeley Center for Future Urban Transport: A Volvo Center of Excellence</a></li>
+                <li>
+                  <details className="c-togglelist__sublist">
+                    <summary>UC Berkeley Transportation Sustainability Research Center</summary>
+                    <ul>
+                      <li><a href="">Center for Latino Policy Research</a></li>
+                      <li><a href="">Center for Research on Native American Issues</a></li>
+                      <li><a href="">Center for Right-Wing Studies</a></li>
+                    </ul>
+                  </details>
+                </li>
+              </ul>
+            </details>
+          </li>
+          <li><a href="">California Community College Collaborative (C4)</a></li>
+          <li><a href="">California Digital Library</a></li>
+          <li><a href="">California Health Benefits Review Program</a></li>
+        </ul>
+      </div>
     )
   }
 }
