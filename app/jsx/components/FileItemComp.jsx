011e90a5b8cd5119685caa25234242eea79424aa
JoelCDL <joel.hagedorn@ucop.edu>
2017-02-22 12:23:46 -0800

    create initial fileitem comp html & styles

--- app/jsx/components/FileItemComp.jsx
+++ app/jsx/components/FileItemComp.jsx
@@ -6,7 +6,12 @@ class FileItemComp extends React.Component {
   render() {
     return (
       <div className="c-fileitem">
-        [file item comp to go here]
+        <h2 className="c-fileitem__heading">New Horizons Full Trajectory Blitzkrieg Bonanza</h2>
+        <img className="c-fileitem__image" src="http://placehold.it/300x200?text=Image" alt="" />
+        <button className="c-fileitem__button o-button__7">Download</button>
+        <div className="c-fileitem__description">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit saepe, quia sequi ipsum excepturi officiis perferendis, nihil omnis, voluptatem facilis explicabo maxime, fugiat repudiandae rerum? Adipisci id necessitatibus dolorem. Alias!
+        </div>
       </div>
     )
   }
