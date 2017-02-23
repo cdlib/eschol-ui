ddfb282c8b912e53713136a21d3fcc2772001d16
JoelCDL <joel.hagedorn@ucop.edu>
2017-02-22 16:54:41 -0800

    update media comp heading icon styles

--- app/jsx/components/MediaFileComp.jsx
+++ app/jsx/components/MediaFileComp.jsx
@@ -6,7 +6,7 @@ class MediaFileComp extends React.Component {
   render() {
     return (
       <div className="c-mediafile">
-        <h2 className="c-mediafile__heading">New Horizons Full Trajectory Blitzkrieg Bonanza</h2>
+        <h2 className="c-mediafile__heading--picture">New Horizons Full Trajectory Blitzkrieg Bonanza</h2>
         <img className="c-mediafile__image" src="http://placehold.it/300x200?text=Image" alt="" />
         <button className="c-mediafile__button o-button__7">Download</button>
         <div className="c-mediafile__description">
