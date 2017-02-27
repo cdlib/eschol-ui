473a3b36678dbc7a87b396f9ba57e12b4555168a
JoelUCOP <joel.hagedorn@ucop.edu>
2017-02-27 14:44:13 -0800

    add mediafilegrid component files

---
+++ app/jsx/components/MediaFileGridComp.jsx
@@ -0,0 +1,15 @@
+// ##### Media File Grid Component ##### //
+
+import React from 'react'
+
+class MediaFileGridComp extends React.Component {
+  render() {
+    return (
+      <div className="c-mediafilegrid">
+      [media file grid component to go here]
+      </div>
+    )
+  }
+}
+
+module.exports = MediaFileGridComp;
---
+++ app/jsx/display/MediaFileGridDisp.jsx
@@ -0,0 +1,16 @@
+// ##### Media File Display ##### //
+
+import React from 'react'
+import MediaFileGridComp from '../components/MediaFileGridComp.jsx'
+
+class MediaFileGridDisp extends React.Component {
+  render() {
+    return (
+      <div>
+        <MediaFileGridComp />
+      </div>
+    )
+  }
+}
+
+module.exports = MediaFileGridDisp;
