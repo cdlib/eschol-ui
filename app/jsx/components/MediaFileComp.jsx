d1b7aedde531492a19ebc88a975eb10a275692f6
JoelUCOP <joel.hagedorn@ucop.edu>
2017-02-27 13:38:27 -0800

    update mediafile grid styles and sample image size

--- app/jsx/components/MediaFileComp.jsx
+++ app/jsx/components/MediaFileComp.jsx
@@ -7,7 +7,7 @@ class MediaFileComp extends React.Component {
     return (
       <div className="c-mediafile">
         <h2 className="c-mediafile__heading--picture">New Horizons Full Trajectory Blitzkrieg Bonanza</h2>
-        <img className="c-mediafile__image" src="http://placehold.it/300x200?text=Image" alt="" />
+        <img className="c-mediafile__image" src="http://placehold.it/300x300?text=Image" alt="" />
         <button className="c-mediafile__button o-button__7">Download</button>
         <div className="c-mediafile__description">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit saepe, quia sequi ipsum excepturi officiis perferendis, nihil omnis!
