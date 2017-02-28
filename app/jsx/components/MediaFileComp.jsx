ce8aaf2dba5810db3b0434cb89b28edf17750dc6
JoelUCOP <joel.hagedorn@ucop.edu>
2017-02-28 13:55:45 -0800

    add sample image to mediafile comp

--- app/jsx/components/MediaFileComp.jsx
+++ app/jsx/components/MediaFileComp.jsx
@@ -12,7 +12,7 @@ class MediaFileComp extends React.Component {
     return (
       <div className="c-mediafile">
         <h2 className="c-mediafile__heading--picture">New Horizons Full Trajectory Blitzkrieg Bonanza</h2>
-        <img className="c-mediafile__image" src="http://placehold.it/300x300?text=Image" alt="" />
+        <img className="c-mediafile__image" src="images/temp_new-horizons-data.png" alt="New horizons data" />
         <button className="c-mediafile__button o-button__7">Download</button>
         <div className="c-mediafile__description">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, in quo libero recusandae debitis, repudiandae rem hic, ut quisquam maxime dolores? Harum expedita fugiat molestiae explicabo, qui, perspiciatis architecto numquam.
