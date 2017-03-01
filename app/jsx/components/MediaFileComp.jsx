de28065891e29091b935175f8840c696517bf396
JoelCDL <joel.hagedorn@ucop.edu>
2017-03-01 10:53:02 -0800

    wrap mediafile image in link; move icon overlay styles to link

--- app/jsx/components/MediaFileComp.jsx
+++ app/jsx/components/MediaFileComp.jsx
@@ -10,9 +10,11 @@ class MediaFileComp extends React.Component {
   }
   render() {
     return (
-      <div className="c-mediafile--eye">
+      <div className="c-mediafile">
         <h2 className="c-mediafile__heading--image">New Horizons Full Trajectory - Side View</h2>
-        <img className="c-mediafile__image" src="images/temp_new-horizons-data.png" alt="New horizons data" />
+        <a className="c-mediafile__link" href="">
+          <img className="c-mediafile__image" src="images/temp_new-horizons-data.png" alt="New horizons data" />
+        </a>
         <button className="c-mediafile__button o-button__7">Download</button>
         <div className="c-mediafile__description">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, in quo libero recusandae debitis, repudiandae rem hic, ut quisquam maxime dolores? Harum expedita fugiat molestiae explicabo, qui, perspiciatis architecto numquam.
