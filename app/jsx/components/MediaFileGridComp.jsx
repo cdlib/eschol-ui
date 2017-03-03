dada09367d74296dcf0a43d6660ebc76fcef3177
JoelUCOP <joel.hagedorn@ucop.edu>
2017-03-03 11:52:35 -0800

    replace old mediafile comps with updated objects in mediafilegrid

--- app/jsx/components/MediaFileGridComp.jsx
+++ app/jsx/components/MediaFileGridComp.jsx
@@ -5,55 +5,51 @@ import $ from 'jquery'
 
 class MediaFileGridComp extends React.Component {
   componentDidMount() {
-    $('[class^="c-mediafile__heading"], .c-mediafile__description').dotdotdot({watch:
-      'window'});
+    $('.o-mediafile__heading, .o-mediafile__description').dotdotdot({watch: 'window'});
   }
   render() {
     return (
       <div className="c-mediafilegrid">
         {/* Media File Component #1 */}
-        <div className="c-mediafile--selected">
-          <h2 className="c-mediafile__heading--video">New Horizons Kuiper Belt Fly-through</h2>
-          <img className="c-mediafile__image" src="images/sample_video-poster.jpg" alt="New horizons data" />
-          <button className="c-mediafile__button o-button__7">Download</button>
-          <div className="c-mediafile__description">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, in quo libero recusandae debitis, repudiandae rem hic, ut quisquam maxime dolores? Harum expedita fugiat molestiae explicabo, qui, perspiciatis architecto numquam.
+        <div className="o-mediafile--audio">
+          <h2 className="o-mediafile__heading">Outer Space Music</h2>
+          <a className="o-mediafile__link" href="images/sample_audio.mp3" aria-label="play audio file">
+            {/* image with o-media__image doesn't extist */}
+          </a>
+          <button className="o-mediafile__button o-button__7">Download</button>
+          <div className="o-mediafile__description">
+            Architecto quo praesentium, suscipit, qui est maxime ut repellendus earum odio ab, consequuntur saepe voluptatem commodi dolorum eos adipisci, fugiat tempore ipsa. Reprehenderit corrupti quae ea, veritatis iste, perspiciatis. Ad, reiciendis praesentium!
           </div>
         </div>
         {/* Media File Component #2 */}
-        <div className="c-mediafile">
-          <h2 className="c-mediafile__heading--audio">Outer Space Music</h2>
-          <img className="c-mediafile__image" src="http://placehold.it/300x300?text=Audio" alt="" />
-          <button className="c-mediafile__button o-button__7">Download</button>
-          <div className="c-mediafile__description">
-            Corrupti delectus aut fugit labore fugiat totam odit recusandae optio dolore ipsa qui, dolores sed tempore ut molestias, numquam maxime culpa alias perspiciatis tenetur neque iusto a impedit! Assumenda praesentium iure ut!
+        <div className="o-mediafile--file">
+          <h2 className="o-mediafile__heading">New Horizons File Archive</h2>
+          {/* link and image with o-mediafile__link and o-media__image don't extist */}
+          <button className="o-mediafile__button o-button__7">Download</button>
+          <div className="o-mediafile__description">
+            Eaque, qui rerum fugiat sed. Error aperiam quo atque debitis doloremque. Adipisci, ab placeat! Atque vitae blanditiis, fuga necessitatibus harum libero, repudiandae quasi vero repellendus fugiat asperiores placeat tempore qui vel iusto!
           </div>
         </div>
         {/* Media File Component #3 */}
-        <div className="c-mediafile">
-          <h2 className="c-mediafile__heading--image">New Horizons Full Trajectory - Overhead View</h2>
-          <img className="c-mediafile__image" src="images/sample_overhead-view.png" alt="" />
-          <button className="c-mediafile__button o-button__7">Download</button>
-          <div className="c-mediafile__description">
-            Nemo tempore sunt nihil maiores sint eveniet sequi non, unde in delectus, molestiae harum, voluptate voluptates nostrum vitae inventore veritatis. Nisi, sapiente! Impedit a, amet minima sunt nemo, consectetur accusantium dolores totam.
+        <div className="o-mediafile--image--selected">
+          <h2 className="o-mediafile__heading">New Horizons Full Trajectory - Side View</h2>
+          <a className="o-mediafile__link" href="images/temp_new-horizons-data.png">
+            <img className="o-mediafile__image" src="images/temp_new-horizons-data.png" alt="New horizons data" />
+          </a>
+          <button className="o-mediafile__button o-button__7">Download</button>
+          <div className="o-mediafile__description">
+            Esse nulla ad veritatis dolorum, vitae maxime tenetur nemo. Quod eius corrupti provident nisi ullam, repellendus molestias, aliquam neque nulla dolorem, magnam commodi ratione enim ex, suscipit labore veniam deserunt nam exercitationem!
           </div>
         </div>
         {/* Media File Component #4 */}
-        <div className="c-mediafile">
-          <h2 className="c-mediafile__heading--image">New Horizons Full Trajectory - Side View</h2>
-          <img className="c-mediafile__image" src="images/temp_new-horizons-data.png" alt="New horizons data" />
-          <button className="c-mediafile__button o-button__7">Download</button>
-          <div className="c-mediafile__description">
-            Accusantium corrupti doloribus quibusdam, qui eveniet tempore nobis aspernatur placeat delectus. Ad sapiente veritatis doloremque officiis, beatae in minima dolores commodi cumque, laboriosam repellendus, tempore odio accusantium. Nobis inventore, ipsam eius vel.
-          </div>
-        </div>
-        {/* Media File Component #5 */}
-        <div className="c-mediafile">
-          <h2 className="c-mediafile__heading--file">New Horizons Full Trajectory File Archive</h2>
-          <img className="c-mediafile__image" src="http://placehold.it/300x300?text=File" alt="" />
-          <button className="c-mediafile__button o-button__7">Download</button>
-          <div className="c-mediafile__description">
-            Nostrum est odio iusto perspiciatis, inventore magnam. Doloremque quidem, beatae nobis explicabo, numquam ex molestias, saepe aliquam sit reiciendis eligendi quod! Modi incidunt, repudiandae quam enim ipsa tempora pariatur ducimus at debitis.
+        <div className="o-mediafile--video">
+          <h2 className="o-mediafile__heading">New Horizons Kuiper Belt Fly-through</h2>
+          <a className="o-mediafile__link" href="images/sample_video.mp4" aria-label="play video file">
+            <img className="o-mediafile__image" src="images/sample_video-poster.jpg" alt="New horizons data" />
+          </a>
+          <button className="o-mediafile__button o-button__7">Download</button>
+          <div className="o-mediafile__description">
+            Veritatis veniam quidem voluptatem laborum necessitatibus iure facilis laudantium possimus sequi libero aliquid, adipisci explicabo, itaque odit ut delectus nemo quas, quia neque. Error cum porro pariatur vel ducimus aliquid quo adipisci.
           </div>
         </div>
       </div>
