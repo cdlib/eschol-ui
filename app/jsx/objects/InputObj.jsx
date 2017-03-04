a3a9153cfa6c4f9b926584800caa9cd5d31822d2
JoelUCOP <joel.hagedorn@ucop.edu>
2017-03-03 16:10:45 -0800

    create droplist2 html & styles from droplist1

--- app/jsx/objects/InputObj.jsx
+++ app/jsx/objects/InputObj.jsx
@@ -6,7 +6,9 @@ class InputObj extends React.Component {
   render() {
     return (
       <div>
-        <h2>Drop-down Lists</h2>
+
+        <h2>Drop-down List 1</h2>
+
         <div className="o-input__droplist1">
           <label htmlFor="o-input__droplist-label">Fruit:</label>
           <select name="" id="o-input__droplist-label">
@@ -15,6 +17,19 @@ class InputObj extends React.Component {
             <option value="">Pears</option>
             <option value="">Grapes</option>
             <option value="">Strawberries</option>
+          </select>
+        </div>
+
+        <h2>Drop-down List 2</h2>
+
+        <div className="o-input__droplist2">
+          <label htmlFor="o-input__droplist-label">Refine media type by:</label>
+          <select name="" id="o-input__droplist-label">
+            <option value="">All</option>
+            <option value="">Documents</option>
+            <option value="">Images</option>
+            <option value="">Videos</option>
+            <option value="">Others</option>
           </select>
         </div>
       </div>
