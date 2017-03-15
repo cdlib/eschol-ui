780471d9e01d8c215d2aa48b9b60bc73f54b285f
JoelUCOP <joel.hagedorn@ucop.edu>
2017-03-15 10:13:50 -0700

    update search2 focus/blur jsx

--- app/jsx/components/Search2Comp.jsx
+++ app/jsx/components/Search2Comp.jsx
@@ -9,22 +9,22 @@ class SearchComp2 extends React.Component {
   }
   render() {
     return (
-      <form action="" className="c-search2" onSubmit = {()=> this.setState({refineActive: false})}>
+      <div className="c-search2">
         <div className="c-search2__inputs">
           <div className="c-search2__form">
             <label className="c-search2__label" htmlFor="global-search">Search eScholarship</label>
-            <input type="search" id="global-search" className="c-search2__field" placeholder="Search eScholarship" onFocus = {()=> this.setState({refineActive: true})}/>
+            <input type="search" id="global-search" className="c-search2__field" placeholder="Search" onFocus = {()=> this.setState({refineActive: true})} onBlur = {()=> this.setState({refineActive: false})}/>
           </div>
           <div className={this.state.refineActive ? "c-search2__refine--active" : "c-search2__refine"}>
-            <input type="radio" name="search-refine" id="c-search2__refine-eschol" onClick = {()=> this.setState({refineActive: false})} defaultChecked={true}/>
+            <input type="radio" name="search-refine" id="c-search2__refine-eschol" defaultChecked={true}/>
             <label htmlFor="c-search2__refine-eschol">All of eScholarship</label>
-            <input type="radio" name="search-refine" id="c-search2__refine-campus" onClick = {()=> this.setState({refineActive: false})}/>
+            <input type="radio" name="search-refine" id="c-search2__refine-campus"/>
             <label htmlFor="c-search2__refine-campus">This campus</label>
           </div>
         </div>
-        <button type="submit" className="c-search2__submit-button" aria-label="search"></button>
+        <button className="c-search2__submit-button" aria-label="search"></button>
         <button className="c-search2__search-close-button" aria-label="close search field" onClick = {()=>this.props.onClose()}></button>
-      </form>
+      </div>
     )
   }
 }
