5a2da7b39516b317b558763b5c2fc04d18598e0d
JoelUCOP <joel.hagedorn@ucop.edu>
2017-03-15 09:45:19 -0700

    add tabcontent heading class to tab headings

--- app/jsx/components/TabAuthorComp.jsx
+++ app/jsx/components/TabAuthorComp.jsx
@@ -6,7 +6,7 @@ class TabAuthorComp extends React.Component {
   render() {
     return (
       <div className="c-tabcontent">
-        <h1>Author & Article content to go here</h1> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti numquam at dolore eligendi impedit blanditiis odio ratione nihil, aspernatur saepe tempore sit, voluptatum minus similique magni expedita vitae reprehenderit tempora!
+        <h1 className="c-tabcontent__heading">Author & Article content to go here</h1> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti numquam at dolore eligendi impedit blanditiis odio ratione nihil, aspernatur saepe tempore sit, voluptatum minus similique magni expedita vitae reprehenderit tempora!
       </div>
     )
   }
--- app/jsx/components/TabMetricsComp.jsx
+++ app/jsx/components/TabMetricsComp.jsx
@@ -6,7 +6,7 @@ class TabMetricsComp extends React.Component {
   render() {
     return (
       <div className="c-tabcontent">
-        <h1>Metrics content to go here</h1> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione quam, tempora repellendus! Doloremque porro eveniet nemo explicabo quo et consequatur deleniti quis cum saepe, quod quos, molestiae dolor aliquam animi.
+        <h1 className="c-tabcontent__heading">Metrics content to go here</h1> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione quam, tempora repellendus! Doloremque porro eveniet nemo explicabo quo et consequatur deleniti quis cum saepe, quod quos, molestiae dolor aliquam animi.
       </div>
     )
   }
