3add1167db1c80c026190177c497da9d5d7dbc58
JoelUCOP <joel.hagedorn@ucop.edu>
2017-03-15 14:51:47 -0700

    remove search1 comp width style

--- app/jsx/components/Search1Comp.jsx
+++ app/jsx/components/Search1Comp.jsx
@@ -5,17 +5,17 @@ import React from 'react'
 class SearchComp1 extends React.Component {
   render() {
     return (
-      <form action="" className="c-search1">
+      <div className="c-search1">
         <div className="c-search1__form">
           <label className="c-search1__label" htmlFor="global-search">Search eScholarship</label>
-          <input type="search" id="global-search" className="c-search1__field" placeholder="Search eScholarship"/>
+          <input type="search" id="global-search" className="c-search1__field" placeholder="Search"/>
           <button type="submit" className="c-search1__submit-button" aria-label="search"></button>
           <button className="c-search1__search-close-button" aria-label="close search field" onClick = {()=>this.props.onClose()}></button>
         </div>
         <div className="c-search1__tagline">
           Open Access publications from the University of California
         </div>
-      </form>
+      </div>
     )
   }
 }
