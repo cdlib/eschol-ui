0820eafaaee3fcd96ceab542feee4439afe0f47e
JoelUCOP <joel.hagedorn@ucop.edu>
2017-03-24 12:16:03 -0700

    create initial mediafeature object html & styles

--- app/jsx/objects/MediaFeatureObj.jsx
+++ app/jsx/objects/MediaFeatureObj.jsx
@@ -6,7 +6,16 @@ class MediaFeatureObj extends React.Component {
   render() {
     return (
       <div className="o-mediafeature">
-        [media feature objects will go here]
+        <div className="o-mediafeature__item">
+          [media item to go here]
+        </div>
+        <div className="o-mediafeature__title-bar">
+          <h2>New Horizons Kuiper Belt Fly-Through</h2>
+          <button>Download</button>
+        </div>
+        <div className="o-mediafeature__description">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis optio numquam aut facere odio et accusantium eius quo harum, quis ab molestias illum totam earum nemo voluptatum eligendi nobis. Molestiae reprehenderit et neque nisi nam corporis totam exercitationem, numquam fuga. Sint minima quisquam, velit quod ratione. Vero, libero, accusantium. Rem.
+        </div>
       </div>
     )
   }
