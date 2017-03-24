8a306f95c736bfb2009b343f3ce38d287df458e1
JoelUCOP <joel.hagedorn@ucop.edu>
2017-03-24 15:00:19 -0700

    simplify search2 focus/blur jsx

--- app/jsx/components/Search2Comp.jsx
+++ app/jsx/components/Search2Comp.jsx
@@ -3,27 +3,26 @@
 import React from 'react'
 
 class SearchComp2 extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {refineActive: false}
-  }
+  state={refineActive: false}
+  makeActive = ()=> this.setState({refineActive: true})
+  makeInactive = ()=> this.setState({refineActive: false})
   render() {
     return (
       <div className="c-search2">
         <div className="c-search2__inputs">
           <div className="c-search2__form">
             <label className="c-search2__label" htmlFor="global-search">Search eScholarship</label>
-            <input type="search" id="global-search" className="c-search2__field" placeholder="Search" onFocus = {()=> this.setState({refineActive: true})} onBlur = {()=> this.setState({refineActive: false})}/>
+            <input type="search" id="global-search" className="c-search2__field" placeholder="Search" onFocus={this.makeActive} onBlur={this.makeInactive}/>
           </div>
           <div className={this.state.refineActive ? "c-search2__refine--active" : "c-search2__refine"}>
-            <input type="radio" name="search-refine" id="c-search2__refine-eschol" defaultChecked={true} onFocus = {()=> this.setState({refineActive: true})} onBlur = {()=> this.setState({refineActive: false})}/>
+            <input type="radio" name="search-refine" id="c-search2__refine-eschol" defaultChecked={true} onFocus={this.makeActive} onBlur={this.makeInactive}/>
             <label htmlFor="c-search2__refine-eschol">All of eScholarship</label>
-            <input type="radio" name="search-refine" id="c-search2__refine-campus" onFocus = {()=> this.setState({refineActive: true})} onBlur = {()=> this.setState({refineActive: false})}/>
+            <input type="radio" name="search-refine" id="c-search2__refine-campus" onFocus={this.makeActive} onBlur={this.makeInactive}/>
             <label htmlFor="c-search2__refine-campus">This campus</label>
           </div>
         </div>
         <button className="c-search2__submit-button" aria-label="search"></button>
-        <button className="c-search2__search-close-button" aria-label="close search field" onClick = {()=>this.props.onClose()}></button>
+        <button className="c-search2__search-close-button" aria-label="close search field" onClick={()=>this.props.onClose()}></button>
       </div>
     )
   }
