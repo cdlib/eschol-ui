198de1d91fda578697de7d26e4d1f4f5250a6dbc
JoelUCOP <joel.hagedorn@ucop.edu>
2017-03-27 12:26:38 -0700

    add mediafeature picture sample html and styles

--- app/jsx/objects/MediaFeatureObj.jsx
+++ app/jsx/objects/MediaFeatureObj.jsx
@@ -21,6 +21,20 @@ class MediaFeatureObj extends React.Component {
           </div>
         </div>
 
+        <h2>Picture Example</h2>
+        <div className="o-mediafeature">
+          <div className="o-mediafeature__item--picture">
+            <img src="images/sample_overhead-view.png" alt=""/>
+          </div>
+          <div className="o-mediafeature__title-bar">
+            <h2>New Horizons Full Trajectory - Overhead View</h2>
+            <button>Download</button>
+          </div>
+          <div className="o-mediafeature__description">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quidem mollitia error neque debitis voluptas similique quibusdam aliquid voluptatum, numquam consectetur expedita ipsa est obcaecati omnis tenetur ea doloribus enim vero excepturi dolore earum. Nisi provident aliquid minus aperiam magnam et mollitia quidem, fugiat possimus illo quisquam aut dolorem sequi!
+          </div>
+        </div>
+
         <h2>Video Example</h2>
         <div className="o-mediafeature">
           <div className="o-mediafeature__item--video">
