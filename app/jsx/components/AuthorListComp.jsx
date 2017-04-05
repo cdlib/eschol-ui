5d0d2f0c4ffd22f1f58410bcd9860fc5864a8216
JoelCDL <joel.hagedorn@ucop.edu>
2017-04-05 15:27:33 -0700

    link authorlist list items

--- app/jsx/components/AuthorListComp.jsx
+++ app/jsx/components/AuthorListComp.jsx
@@ -13,24 +13,25 @@ class AuthorListComp extends React.Component {
     return (
       <div className="c-authorlist">
         <time className="c-authorlist__year">2012</time>
-        <ul className="c-authorlist__list">      
-          <li>Leung, Wilson</li>
-          <li>Shaffer, Christopher D</li>
-          <li>Reed, Laura K</li>
-          <li>Smith, Sheryl T</li>
-          <li>Barshop, William</li>
-          <li>Dirkes, William</li>
-          <li>Dothager, Matthew</li>
-          <li>Lee, Paul</li>
-          <li>Wong, Jeannette</li>
-          <li>Xiong, David</li>
-          <li>Yuan, Han</li>
-          <li>Bedard James E J</li>
-          <li>Machone, Joshua F</li>
-          <li>Patterson, Seantay D</li>
-          <li>Price, Amber L</li>
-          <li>Turner, Bryce A</li>
-          <li>Robic, Srebrenka</li>
+        <ul className="c-authorlist__list">
+        {/* links can be left out of the list if no links are desired: */}
+          <li><a href="">Leung, Wilson</a></li>
+          <li><a href="">Shaffer, Christopher D</a></li>
+          <li><a href="">Reed, Laura K</a></li>
+          <li><a href="">Smith, Sheryl T</a></li>
+          <li><a href="">Barshop, William</a></li>
+          <li><a href="">Dirkes, William</a></li>
+          <li><a href="">Dothager, Matthew</a></li>
+          <li><a href="">Lee, Paul</a></li>
+          <li><a href="">Wong, Jeannette</a></li>
+          <li><a href="">Xiong, David</a></li>
+          <li><a href="">Yuan, Han</a></li>
+          <li><a href="">Bedard James E J</a></li>
+          <li><a href="">Machone, Joshua F</a></li>
+          <li><a href="">Patterson, Seantay D</a></li>
+          <li><a href="">Price, Amber L</a></li>
+          <li><a href="">Turner, Bryce A</a></li>
+          <li><a href="">Robic, Srebrenka</a></li>
           <li><a href="" className="c-authorlist__list-more-link">et al.</a></li>
         </ul>
         
