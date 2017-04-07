ada34aaee251f65672af328f587129a68ef04d2f
JoelUCOP <joel.hagedorn@ucop.edu>
2017-04-07 11:02:21 -0700

    replace journal simple layout frontmatter classes with pub classes

--- app/jsx/layouts/JournalSimpleLayout.jsx
+++ app/jsx/layouts/JournalSimpleLayout.jsx
@@ -35,19 +35,14 @@ class JournalSimpleLayout extends React.Component {
             </section>
             <section className="o-columnbox1">
               <ItemActionsComp />
-              <div className="c-frontmatter">
-                <div className="c-frontmatter__text">
-                  <h2 className="c-frontmatter__heading">
-                    <a href="">Volume 41, Issue 1, 2012</a>
-                  </h2>
-                  <div className="c-frontmatter__subheading">Focus: Caribbean Studies and Literatures</div>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur fuga laborum, qui debitis vitae quaerat quas ab officia, dolor dignissimos ipsum nam ratione unde animi? Officiis rerum unde eveniet natus.
-                    Laboriosam tenetur vel, rem culpa maiores non, tempora voluptatibus quasi quos provident exercitationem itaque dolorum quam sequi dolor odio hic accusamus, repellendus ut dignissimos. Labore modi consectetur ullam, iste accusamus!
-                    Dolore quod illum praesentium sint. Consectetur illum voluptas reiciendis possimus ullam nesciunt, laboriosam nisi nihil hic. Veritatis porro doloribus iste eos, assumenda fugiat dicta nesciunt. Autem sed recusandae at quod!
-                    Ducimus adipisci provident nam voluptatem officia. Quos maiores molestias atque alias deserunt ullam laborum similique, quae cum excepturi. Rem harum facilis perspiciatis a illum, eum, officiis. Ducimus officia, veniam ea.
-                  </p>
-                </div>
+              <div className="c-pub">
+                <h2 className="c-pub__heading">
+                  <a href="">Volume 41, Issue 1, 2012</a>
+                </h2>
+                <div className="c-pub__subheading">Focus: Caribbean Studies and Literatures</div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur fuga laborum, qui debitis vitae quaerat quas ab officia, dolor dignissimos ipsum nam ratione unde animi? Officiis rerum unde eveniet natus. Laboriosam tenetur vel, rem culpa maiores non, tempora voluptatibus quasi quos provident exercitationem itaque dolorum quam sequi dolor odio hic accusamus, repellendus ut dignissimos. Labore modi consectetur ullam, iste accusamus!
+                </p>
               </div>
               <h3>Front Matter</h3>
               <PubPreviewComp />
