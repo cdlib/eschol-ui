3538d03c8959fa7af2e6f5b6af63016ba6e6a445
JoelUCOP <joel.hagedorn@ucop.edu>
2017-04-10 16:46:56 -0700

    begin refactoring tabs comp with new tabs interface

--- app/jsx/components/TabsComp.jsx
+++ app/jsx/components/TabsComp.jsx
@@ -15,27 +15,43 @@ class TabsComp extends React.Component {
   }
   tabFocus(tabNumber) {
     // For keyboard users, jump to heading within the tab.
-    setTimeout(()=>$(".c-tabcontent__heading").focus(), 0)
+    // setTimeout(()=>$(".c-tabcontent__heading").focus(), 0)
     this.setState({currentTab: tabNumber})
   }
   render() {
     return (
-      <div className="c-tabs">
-        <div className={this.state.moreTabs ? "c-tabs__tabs--show-all" : "c-tabs__tabs"}>
-          <button className="c-tabs__button-more" onClick = {()=> this.setState({moreTabs: !this.state.moreTabs})} aria-label="Show all tabs">...</button>
-          <button className={this.state.currentTab === 1 ? "c-tabs__button--active" : "c-tabs__button"} onClick = {()=> this.tabFocus(1)}>Main Content</button>
-          <button className={this.state.currentTab === 2 ? "c-tabs__button--active" : "c-tabs__button"} onClick = {()=> this.tabFocus(2)}>Supplemental material</button>
-          <button className={this.state.currentTab === 3 ? "c-tabs__button--active" : "c-tabs__button"} onClick = {()=> this.tabFocus(3)}>Metrics</button>
-          <button className={this.state.currentTab === 4 ? "c-tabs__button--active" : "c-tabs__button"} onClick = {()=> this.tabFocus(4)}>Author & Article Info</button>
-          <button className={this.state.currentTab === 5 ? "c-tabs__button--active" : "c-tabs__button"} onClick = {()=> this.tabFocus(5)}>Comments (2)</button>
-        </div>
-        <div className="c-tabs__content">
-          {this.state.currentTab === 1 ? <TabMainComp /> : null}
-          {this.state.currentTab === 2 ? <TabSupplementalComp /> : null}
-          {this.state.currentTab === 3 ? <TabMetricsComp /> : null}
-          {this.state.currentTab === 4 ? <TabAuthorComp /> : null}
-          {this.state.currentTab === 5 ? <TabCommentsComp /> : null}
-        </div>
+      <div className={this.state.moreTabs ? "c-tabs--show-all" : "c-tabs"}>
+        <details className="c-tabs__details">
+          <summary className={this.state.currentTab === 1 ? "c-tabs__summary--active" : "c-tabs__summary"} onClick = {()=> this.tabFocus(1)}>Main Content</summary>
+          <div className="c-tabs__tabpanel">
+            {this.state.currentTab === 1 ? <TabMainComp /> : null}
+          </div>
+        </details>
+        <details className="c-tabs__details">
+          <summary className={this.state.currentTab === 2 ? "c-tabs__summary--active" : "c-tabs__summary"} onClick = {()=> this.tabFocus(2)}>Supplemental material</summary>
+          <div className="c-tabs__tabpanel">
+            {this.state.currentTab === 2 ? <TabSupplementalComp /> : null}
+          </div>
+        </details>
+        <details className="c-tabs__details">
+          <summary className={this.state.currentTab === 3 ? "c-tabs__summary--active" : "c-tabs__summary"} onClick = {()=> this.tabFocus(3)}>Metrics</summary>
+          <div className="c-tabs__tabpanel">
+            {this.state.currentTab === 3 ? <TabMetricsComp /> : null}
+          </div>
+        </details>
+        <details className="c-tabs__details">
+          <summary className={this.state.currentTab === 4 ? "c-tabs__summary--active" : "c-tabs__summary"} onClick = {()=> this.tabFocus(4)}>Author & Article Info</summary>
+          <div className="c-tabs__tabpanel">
+            {this.state.currentTab === 4 ? <TabAuthorComp /> : null}
+          </div>
+        </details>
+        <details className="c-tabs__details">
+          <summary className={this.state.currentTab === 5 ? "c-tabs__summary--active" : "c-tabs__summary"} onClick = {()=> this.tabFocus(5)}>Comments</summary>
+          <div className="c-tabs__tabpanel">
+            {this.state.currentTab === 5 ? <TabCommentsComp /> : null}
+          </div>
+        </details>
+        <button className="c-tabs__button" onClick = {()=> this.setState({moreTabs: !this.state.moreTabs})} aria-label="Show all tabs">...</button>
       </div>
     )
   }
