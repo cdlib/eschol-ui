39ec030c7b0bee05714ad7d06b23c477b403d1a0
JoelUCOP <joel.hagedorn@ucop.edu>
2017-04-10 15:43:19 -0700

    complete testing new tab interface

--- app/jsx/display/Test.jsx
+++ app/jsx/display/Test.jsx
@@ -7,31 +7,31 @@ class Test extends React.Component {
     return (
       <div className="c-test">
         <details className="c-test__details">
-          <summary className="c-test__summary">Tab Number One Here</summary>
+          <summary className="c-test__summary">Main Content</summary>
           <div className="c-test__tabpanel">
           Cumque itaque numquam atque at nam in, cum nostrum sint dolorum quis assumenda. Voluptatem eaque sit neque eveniet aliquid aperiam voluptatum quia. <a href="">Link</a>
           </div>
         </details>
         <details className="c-test__details">
-          <summary className="c-test__summary">Tab Number Two Here</summary>
+          <summary className="c-test__summary">Supplemental material</summary>
           <div className="c-test__tabpanel">
           Quidem nemo accusantium nisi, rem cupiditate et doloribus itaque voluptatum quae officia excepturi vero veritatis eligendi consequuntur, voluptatibus deleniti corporis voluptatem dolores facere quaerat repellendus reprehenderit alias dolor possimus. Non quos corrupti totam placeat, deserunt quidem magnam laborum qui dignissimos repellat nam. <a href="">Link</a>
           </div>
         </details>
         <details className="c-test__details">
-          <summary className="c-test__summary">Tab Number Three Here</summary>
+          <summary className="c-test__summary">Metrics</summary>
           <div className="c-test__tabpanel">
           Fugit dignissimos debitis inventore maxime quasi sit est et! Consequuntur error aut repellat praesentium ipsum obcaecati quia harum provident, odio, ad vitae? <a href="">Link</a>
           </div>
         </details>
         <details className="c-test__details">
-          <summary className="c-test__summary">Tab Number Four Is Right Here</summary>
+          <summary className="c-test__summary">Author & Article Info</summary>
           <div className="c-test__tabpanel">
           Numquam modi incidunt, magnam quae voluptates voluptate, sit repellendus, corrupti commodi provident expedita, quaerat ipsam. Laboriosam quia non ipsa, animi, ea quod! <a href="">Link</a>
           </div>
         </details>
         <details className="c-test__details">
-          <summary className="c-test__summary">Tab Number Five Here</summary>
+          <summary className="c-test__summary">Comments (2)</summary>
           <div className="c-test__tabpanel">
           Similique temporibus recusandae commodi, magni adipisci. Delectus, repudiandae. Doloremque placeat odio, nesciunt minima possimus laudantium tempore culpa, illum quibusdam porro, similique. Fugiat. <a href="">Link</a>
           </div>
