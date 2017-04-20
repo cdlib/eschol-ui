dcc8e16170cd763ca40be8aed91f545055a432dd
JoelUCOP <joel.hagedorn@ucop.edu>
2017-04-20 16:01:50 -0700

    add campuscarousel sample html; style sections, headings, and cells

--- app/jsx/components/CampusCarouselComp.jsx
+++ app/jsx/components/CampusCarouselComp.jsx
@@ -11,7 +11,8 @@ class CampusCarouselComp extends React.Component {
       cellAlign: 'left',
       contain: true,
       initialIndex: 0,
-      imagesLoaded: true
+      imagesLoaded: true,
+      pageDots: false
     }
     this.flkty = new Flickity(carousel, options);
   }
@@ -23,38 +24,44 @@ class CampusCarouselComp extends React.Component {
   render() {
     return (
       <div className="c-campuscarousel">
-        <div className="c-campuscarousel__cell">
+        <div className="c-campuscarousel__section">
           <h2 className="c-campuscarousel__cell-heading">
             UC Berkeley
           </h2>
-          <div className="c-campuscarousel__cell-item">
-            Item 1
+          <div className="c-campuscarousel__cell--item">
+            <strong>24,844</strong> Items
           </div>
-          <div className="c-campuscarousel__cell-item">
-            Item 2
+          <div className="c-campuscarousel__cell--view">
+            <strong>380,941</strong> Views
           </div>
-          <div className="c-campuscarousel__cell-item">
-            Item 3
+          <div className="c-campuscarousel__cell--passed">
+            <strong>6,532</strong> Items since UC OA Policy passed
           </div>
-          <div className="c-campuscarousel__cell-item">
-            Item 4
+          <div className="c-campuscarousel__cell--journals">
+            <strong>31</strong> eScholarship Journals
+          </div>
+          <div className="c-campuscarousel__cell--units">
+            <strong>119</strong> Research Units
           </div>
         </div>
-        <div className="c-campuscarousel__cell">
+        <div className="c-campuscarousel__section">
           <h2 className="c-campuscarousel__cell-heading">
             All eScholarship
           </h2>
-          <div className="c-campuscarousel__cell-item">
-            Item 1
+          <div className="c-campuscarousel__cell--item">
+            <strong>127,057</strong> Items
           </div>
-          <div className="c-campuscarousel__cell-item">
-            Item 2
+          <div className="c-campuscarousel__cell--view">
+            <strong>35,489,231</strong> Views
           </div>
-          <div className="c-campuscarousel__cell-item">
-            Item 3
+          <div className="c-campuscarousel__cell--passed">
+            <strong>31,750</strong> Items since UC OA Policy passed
           </div>
-          <div className="c-campuscarousel__cell-item">
-            Item 4
+          <div className="c-campuscarousel__cell--journals">
+            <strong>91</strong> eScholarship Journals
+          </div>
+          <div className="c-campuscarousel__cell--units">
+            <strong>500</strong> Research Units
           </div>
         </div>
       </div>
