db39f09f044da7e2a6b5ac0a45f57e85c42f237e
JoelCDL <joel.hagedorn@ucop.edu>
2017-04-26 15:57:19 -0700

    create heading object sample html & styles

--- app/jsx/objects/HeadingObj.jsx
+++ app/jsx/objects/HeadingObj.jsx
@@ -1,5 +1,7 @@
 // ##### Heading Objects ##### //
 
+// Heading element numbers (<h1>, <h2>, <h3>, etc.) need to be organized logically in the DOM, per accessibility requirements. Therefore, the 'o-heading' className numbers below may not match the heading element numbers, per design requirements. for example, <h3 className="o-heading2"> would be fine.
+
 import React from 'react'
 
 class HeadingObj extends React.Component {
@@ -7,7 +9,14 @@ class HeadingObj extends React.Component {
     return (
       <div>
 
-        <h1 className="o-heading1">[heading objects to go here]</h1>
+        <h2>Heading Style 1</h2>
+        <h4 className="o-heading1">From the New Heights: The City and Migrating Latinas in Real Women Have Curves and María Full of Grace</h4>
+
+        <h2>Heading Style 2</h2>
+        <h1 className="o-heading2">From the New Heights: The City and Migrating Latinas in Real Women Have Curves and María Full of Grace</h1>
+
+        <h2>Heading Style 3</h2>
+        <h3 className="o-heading3">From the New Heights: The City and Migrating Latinas in Real Women Have Curves and María Full of Grace</h3>
 
       </div>
     )
