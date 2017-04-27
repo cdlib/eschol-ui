3f4fbaa4388fdd3ece9afac5e16a501813447d2d
JoelUCOP <joel.hagedorn@ucop.edu>
2017-04-27 11:56:12 -0700

    add tabcontent divide container/child html & styles

--- app/jsx/components/TabMetricsComp.jsx
+++ app/jsx/components/TabMetricsComp.jsx
@@ -6,75 +6,80 @@ class TabMetricsComp extends React.Component {
   render() {
     return (
       <div className="c-tabcontent">
-        <h1 id="c-tabcontent__main-heading" className="o-heading1" tabIndex="-1">Metrics content to go here</h1> 
-        <div className="o-dividecontent2x">
-          <table className="c-datatable">
-            <caption>Total Usage</caption>
-            <thead>
-              <tr>
-                <th scope="col">Actions</th>
-                <th scope="col">Total</th>
-                <th scope="col">Monthly Average</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">Hits</th>
-                <td>Odio</td>
-                <td>Lorem</td>
-              </tr>
-              <tr>
-                <th scope="row">Downloads</th>
-                <td>Iusto</td>
-                <td>Architecto</td>
-              </tr>
-            </tbody>
-          </table>
-          <h2 className="o-heading3">By Month</h2>
-          <div className="c-itemactions">
-            <div className="o-input__droplist2">
-              <label htmlFor="o-input__droplist-label2">From:</label>
-              <select name="" id="o-input__droplist-label2">
-                <option value="">January, 2017</option>
-                <option value="">February, 2017</option>
-                <option value="">March, 2017</option>
-              </select>
+        <h1 id="c-tabcontent__main-heading" className="o-heading1" tabIndex="-1">Metrics content to go here</h1>
+        <div className="c-tabcontent__divide2x">
+          <div className="c-tabcontent__divide2x-child">
+            <table className="c-datatable">
+              <caption>Total Usage</caption>
+              <thead>
+                <tr>
+                  <th scope="col">Actions</th>
+                  <th scope="col">Total</th>
+                  <th scope="col">Monthly Average</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">Hits</th>
+                  <td>Odio</td>
+                  <td>Lorem</td>
+                </tr>
+                <tr>
+                  <th scope="row">Downloads</th>
+                  <td>Iusto</td>
+                  <td>Architecto</td>
+                </tr>
+              </tbody>
+            </table>
+            <h2 className="o-heading3">By Month</h2>
+            <div className="c-itemactions">
+              <div className="o-input__droplist2">
+                <label htmlFor="o-input__droplist-label2">From:</label>
+                <select name="" id="o-input__droplist-label2">
+                  <option value="">January, 2017</option>
+                  <option value="">February, 2017</option>
+                  <option value="">March, 2017</option>
+                </select>
+              </div>
+              <div className="o-input__droplist2">
+                <label htmlFor="o-input__droplist-label2">To:</label>
+                <select name="" id="o-input__droplist-label2">
+                  <option value="">January, 2017</option>
+                  <option value="">February, 2017</option>
+                  <option value="">March, 2017</option>
+                </select>
+              </div>
             </div>
-            <div className="o-input__droplist2">
-              <label htmlFor="o-input__droplist-label2">To:</label>
-              <select name="" id="o-input__droplist-label2">
-                <option value="">January, 2017</option>
-                <option value="">February, 2017</option>
-                <option value="">March, 2017</option>
-              </select>
-            </div>
+            <table className="c-datatable">
+              <thead>
+                <tr>
+                  <th scope="col">Monthly</th>
+                  <th scope="col">Hits</th>
+                  <th scope="col">Downloads</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">2016-03</th>
+                  <td>1,268</td>
+                  <td>271</td>
+                </tr>
+                <tr>
+                  <th scope="row">2016-02</th>
+                  <td>1,269</td>
+                  <td>250</td>
+                </tr>
+                <tr>
+                  <th scope="row">2016-01</th>
+                  <td>1,110</td>
+                  <td>233</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
-          <table className="c-datatable">
-            <thead>
-              <tr>
-                <th scope="col">Monthly</th>
-                <th scope="col">Hits</th>
-                <th scope="col">Downloads</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">2016-03</th>
-                <td>1,268</td>
-                <td>271</td>
-              </tr>
-              <tr>
-                <th scope="row">2016-02</th>
-                <td>1,269</td>
-                <td>250</td>
-              </tr>
-              <tr>
-                <th scope="row">2016-01</th>
-                <td>1,110</td>
-                <td>233</td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="c-tabcontent__divide2x-child">
+            [right division]
+          </div>
         </div>
       </div>
     )
