c8d9c1a0e6ad8ad0ed7bac3e5a9eefb7caa85b3f
JoelUCOP <joel.hagedorn@ucop.edu>
2017-05-08 10:30:30 -0700

    add share comp <span> to <summary> with inline-flex, interactive mixin

--- app/jsx/components/ShareComp.jsx
+++ app/jsx/components/ShareComp.jsx
@@ -6,7 +6,7 @@ class ShareComp extends React.Component {
   render() {
     return (
       <details className="c-share">
-        <summary>Share</summary>
+        <summary><span>Share</span></summary>
         <div className="c-share__list">
           <a className="c-share__email" href="">Email</a>
           <a className="c-share__facebook" href="">Facebook</a>
