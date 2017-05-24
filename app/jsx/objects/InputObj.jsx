b4d7dce65e8c57df512c9aaada470313c65b24bc
JoelCDL <joel.hagedorn@ucop.edu>
2017-05-24 13:18:42 -0700

    create inline input object sample html & styles

--- app/jsx/objects/InputObj.jsx
+++ app/jsx/objects/InputObj.jsx
@@ -7,7 +7,7 @@ class InputObj extends React.Component {
     return (
       <div>
 
-        <h2>Drop-down List 1</h2>
+        <h2>Drop-down List with Inline Label</h2>
 
         <div className="o-input__droplist1">
           <label htmlFor="o-input__droplist1-label">Fruit:</label>
@@ -20,7 +20,7 @@ class InputObj extends React.Component {
           </select>
         </div>
 
-        <h2>Drop-down List 2</h2>
+        <h2>Drop-down List with Block Label</h2>
 
         <div className="o-input__droplist2">
           <label htmlFor="o-input__droplist-label2">Refine media type by:</label>
@@ -32,6 +32,33 @@ class InputObj extends React.Component {
             <option value="">Others</option>
           </select>
         </div>
+
+        <h2>Inline Inputs</h2>
+
+        {/* Any input examples above can be used inline */}
+        <div className="o-input__inline">
+          <div className="o-input__droplist1">
+            <label htmlFor="o-input__droplist3-label">Fruit:</label>
+            <select name="" id="o-input__droplist3-label">
+              <option value="">Apples</option>
+              <option value="">Oranges</option>
+              <option value="">Pears</option>
+              <option value="">Grapes</option>
+              <option value="">Strawberries</option>
+            </select>
+          </div>
+          <div className="o-input__droplist1">
+            <label htmlFor="o-input__droplist4-label">Fruit:</label>
+            <select name="" id="o-input__droplist4-label">
+              <option value="">Apples</option>
+              <option value="">Oranges</option>
+              <option value="">Pears</option>
+              <option value="">Grapes</option>
+              <option value="">Strawberries</option>
+            </select>
+          </div>
+        </div>
+
       </div>
     )
   }
