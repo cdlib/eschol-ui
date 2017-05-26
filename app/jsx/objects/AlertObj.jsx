777fc57be84e1bb746a099f9eb06d65db73864f5
JoelUCOP <joel.hagedorn@ucop.edu>
2017-05-26 12:26:21 -0700

    add alert2 html & styles

--- app/jsx/objects/AlertObj.jsx
+++ app/jsx/objects/AlertObj.jsx
@@ -7,10 +7,19 @@ class AlertObj extends React.Component {
     return (
       <div>
 
-        <h1>Colorless Inline Alert</h1>
+        <h2>Black Inline Alert</h2>
 
         <div className="o-alert1" role="alert">
           This item is <strong>not available</strong> for download from eScholarship.
+        </div>
+
+        <h2>Full-width with Close Alert</h2>
+
+        <div className="o-alert2" role="alert">
+          <div className="o-alert2__text">
+            New Website In Progress
+          </div>
+          <button className="o-alert2__button">Got it!</button>
         </div>
 
       </div>
