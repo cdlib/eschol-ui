a71186f7d2082e5dbb937ef723f88ee208b9d66d
JoelUCOP <joel.hagedorn@ucop.edu>
2017-05-30 14:54:20 -0700

    rename testmessage jsx state; add 'hidden' attribute

--- app/jsx/components/TestMessageComp.jsx
+++ app/jsx/components/TestMessageComp.jsx
@@ -5,16 +5,16 @@ import React from 'react'
 class TestMessageComp extends React.Component {
   constructor(){
     super()
-    this.state = {alertClose: false}
+    this.state = {messageClose: false}
   }
   render() {
     return (
-      <div className={this.state.alertClose ? "c-testmessage--close" : "c-testmessage"} role="alert">
+      <div className={this.state.messageClose ? "c-testmessage--close" : "c-testmessage"} role="alert" hidden={this.state.messageClose ? true : false}>
         <strong className="c-testmessage__main-text">New Website In Progress</strong>
         <div className="c-testmessage__more-info">
           {/* optional informational text would go here */}
         </div>
-        <button className="c-testmessage__button" onClick = {()=> this.setState({alertClose: true})}>Got it!</button>
+        <button className="c-testmessage__button" onClick = {()=> this.setState({messageClose: true})}>Got it!</button>
       </div>
     )
   }
