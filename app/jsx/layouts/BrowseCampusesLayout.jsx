ae34d1920e5ac0a634f8353d0118a7d75383b7a7
JoelCDL <joel.hagedorn@ucop.edu>
2017-06-07 15:29:40 -0700

    replace subheader1 with navbar and nav comps in about, browse campuses, browse journals, and search layouts

--- app/jsx/layouts/BrowseCampusesLayout.jsx
+++ app/jsx/layouts/BrowseCampusesLayout.jsx
@@ -1,7 +1,9 @@
 // ##### Browse Campuses Layout ##### //
 
 import React from 'react'
-import Header2Comp from '../components/Header2Comp.jsx'
+import Header1Comp from '../components/Header1Comp.jsx'
+import NavComp from '../components/NavComp.jsx'
+import NavSubComp from '../components/NavSubComp.jsx'
 import CampusSelectorComp from '../components/CampusSelectorComp.jsx'
 import Breakpoints from '../../js/breakpoints.json'
 import WellComp from '../components/WellComp.jsx'
@@ -27,25 +29,38 @@ class BrowseCampusesLayout extends React.Component {
     return (
       <div>
         <a href="#maincontent" className="c-skipnav">Skip to main content</a>
-        <Header2Comp />
-        <div className="c-subheader2">
-          <CampusSelectorComp />
-          <img className="c-subheader2__banner" src="images/temp_journal-banner.png" alt=""/>
-          <div className="c-subheader2__sidebar">
-            <button className="o-button__3">Deposit</button>
-            <div className="c-subheader2__sidebar-text">University of California, Berkeley <br/>Publications in eScholarship</div>
-            <div className="c-subheader2__sidebar-number">16,464</div>
-          </div>
-        </div>
+        <Header1Comp />
         <div className="c-navbar">
           <div className="c-nav">
             <details open={this.state.isOpen ? "open" : ""} className="c-nav__main">
               <summary className="c-nav__main-button"><span>Menu</span>
               </summary>
               <nav className={this.state.submenuActive ? "c-nav__main-items--submenu-active" : "c-nav__main-items"}>
-                <a href="" className="c-nav__item--active">Open Access Policies</a>
-                <a href="">Journals</a>
-                <a href="">Academic Units</a>
+                <NavSubComp name="About" open={this.state.submenuActive == 1} onSubmenuChanged={(flag)=> this.setState({submenuActive:flag ? 1 : null})}>
+                  <a href="">About Us</a>
+                  <a href="">Aims &amp; Scope</a>
+                  <a href="">Editorial Board</a>
+                </NavSubComp>
+                <NavSubComp name="Campus Sites" open={this.state.submenuActive == 2} onSubmenuChanged={(flag)=> this.setState({submenuActive:flag ? 2 : null})}>
+                  <a href="">UC Berkeley</a>
+                  <a href="">UC Davis</a>
+                  <a href="">UC Irvine</a>
+                  <a href="">UCLA</a>
+                  <a href="">UC Merced</a>
+                  <a href="">UC Riverside</a>
+                  <a href="">UC San Diego</a>
+                  <a href="">UC San Francisco</a>
+                  <a href="">UC Santa Barbara</a>
+                  <a href="">UC Santa Cruz</a>
+                  <a href="">UC Office of the President</a>
+                  <a href="">UC Press</a>
+                </NavSubComp>
+                <NavSubComp name="UC Open Access" open={this.state.submenuActive == 3} onSubmenuChanged={(flag)=> this.setState({submenuActive:flag ? 3 : null})}>
+                  <a href="">To Be Determined 1</a>
+                  <a href="">To Be Determined 2</a>
+                  <a href="">To Be Determined 3</a>
+                </NavSubComp>
+                <a href="">eScholarship Publishing</a>
               </nav>
             </details>
           </div>
--- app/jsx/layouts/BrowseJournalsLayout.jsx
+++ app/jsx/layouts/BrowseJournalsLayout.jsx
@@ -28,38 +28,40 @@ class BrowseJournalsLayout extends React.Component {
     return (
       <div>
         <a href="#maincontent" className="c-skipnav">Skip to main content</a>
-        <div className="c-subheader1">
-          <div className="c-subheader1__header">
-            <Header1Comp />
-          </div>
-          <div className="c-subheader1__nav">
-            <div className="c-nav">
-              <details open={this.state.isOpen ? "open" : ""} className="c-nav__main">
-                <summary className="c-nav__main-button"><span>Menu</span>
-                </summary>
-                <nav className={this.state.submenuActive ? "c-nav__main-items--submenu-active" : "c-nav__main-items"}>
-                  <NavSubComp name="Campus Sites" open={this.state.submenuActive == 1} onSubmenuChanged={(flag)=> this.setState({submenuActive:flag ? 1 : null})}>
-                    <a href="">UC Berkeley</a>
-                    <a href="">UC Davis</a>
-                    <a href="">UC Irvine</a>
-                    <a href="">UCLA</a>
-                    <a href="">UC Merced</a>
-                    <a href="">UC Riverside</a>
-                    <a href="">UC San Diego</a>
-                    <a href="">UC San Francisco</a>
-                    <a href="">UC Santa Barbara</a>
-                    <a href="">UC Santa Cruz</a>
-                    <a href="">UC Office of the President</a>
-                    <a href="">UC Press</a>
-                  </NavSubComp>
-                  <a href="" className="c-nav__item--active">UC Open Access Policies</a>
-                  <a href="">eScholarship Publishing</a>
-                </nav>
-              </details>
-            </div>
-          </div>
-          <div className="c-subheader1__button">
-            <button className="o-button__3">Get Started</button>
+        <Header1Comp />
+        <div className="c-navbar">
+          <div className="c-nav">
+            <details open={this.state.isOpen ? "open" : ""} className="c-nav__main">
+              <summary className="c-nav__main-button"><span>Menu</span>
+              </summary>
+              <nav className={this.state.submenuActive ? "c-nav__main-items--submenu-active" : "c-nav__main-items"}>
+                <NavSubComp name="About" open={this.state.submenuActive == 1} onSubmenuChanged={(flag)=> this.setState({submenuActive:flag ? 1 : null})}>
+                  <a href="">About Us</a>
+                  <a href="">Aims &amp; Scope</a>
+                  <a href="">Editorial Board</a>
+                </NavSubComp>
+                <NavSubComp name="Campus Sites" open={this.state.submenuActive == 2} onSubmenuChanged={(flag)=> this.setState({submenuActive:flag ? 2 : null})}>
+                  <a href="">UC Berkeley</a>
+                  <a href="">UC Davis</a>
+                  <a href="">UC Irvine</a>
+                  <a href="">UCLA</a>
+                  <a href="">UC Merced</a>
+                  <a href="">UC Riverside</a>
+                  <a href="">UC San Diego</a>
+                  <a href="">UC San Francisco</a>
+                  <a href="">UC Santa Barbara</a>
+                  <a href="">UC Santa Cruz</a>
+                  <a href="">UC Office of the President</a>
+                  <a href="">UC Press</a>
+                </NavSubComp>
+                <NavSubComp name="UC Open Access" open={this.state.submenuActive == 3} onSubmenuChanged={(flag)=> this.setState({submenuActive:flag ? 3 : null})}>
+                  <a href="">To Be Determined 1</a>
+                  <a href="">To Be Determined 2</a>
+                  <a href="">To Be Determined 3</a>
+                </NavSubComp>
+                <a href="">eScholarship Publishing</a>
+              </nav>
+            </details>
           </div>
         </div>
         <nav className="c-breadcrumb">
