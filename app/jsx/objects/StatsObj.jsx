f8bc6926a01f6dd8702e10821729775654db4561
JoelUCOP <joel.hagedorn@ucop.edu>
2017-06-09 15:23:33 -0700

    create stats html & styles

--- app/jsx/objects/StatsObj.jsx
+++ app/jsx/objects/StatsObj.jsx
@@ -9,8 +9,20 @@ class StatsObj extends React.Component {
 
         <h2>Statistics Objects Used within Campus Landing Page Carousels</h2>
 
-        <div className="o-stats">
-          [stats to go here]
+        <div className="o-stats--item">
+          <b>24,844</b> Items
+        </div>
+        <div className="o-stats--view">
+          <b>380,941</b> Views
+        </div>
+        <div className="o-stats--passed">
+          <b>6,532</b> Items since UC OA Policy passed
+        </div>
+        <div className="o-stats--journals">
+          <b>31</b> eScholarship Journals
+        </div>
+        <div className="o-stats--units">
+          <b>119</b> Research Units
         </div>
 
       </div>
