8febf8e0df8e5be4a63533a2e3b426f2123c6d56
JoelUCOP <joel.hagedorn@ucop.edu>
2017-06-09 15:28:44 -0700

    rename stats styles to stat

--- app/jsx/objects/StatsObj.jsx
+++ app/jsx/objects/StatsObj.jsx
@@ -9,19 +9,19 @@ class StatsObj extends React.Component {
 
         <h2>Statistics Objects Used within Campus Landing Page Carousels</h2>
 
-        <div className="o-stats--item">
+        <div className="o-stat--item">
           <b>24,844</b> Items
         </div>
-        <div className="o-stats--view">
+        <div className="o-stat--view">
           <b>380,941</b> Views
         </div>
-        <div className="o-stats--passed">
+        <div className="o-stat--passed">
           <b>6,532</b> Items since UC OA Policy passed
         </div>
-        <div className="o-stats--journals">
+        <div className="o-stat--journals">
           <b>31</b> eScholarship Journals
         </div>
-        <div className="o-stats--units">
+        <div className="o-stat--units">
           <b>119</b> Research Units
         </div>
 
