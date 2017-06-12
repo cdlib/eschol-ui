f27266563a15bb5840601834c3c4d299bb6e2357
JoelUCOP <joel.hagedorn@ucop.edu>
2017-06-12 16:12:05 -0700

    create horizontal unititem object; truncate to 2 lines

--- app/jsx/objects/UnitItemObj.jsx
+++ app/jsx/objects/UnitItemObj.jsx
@@ -17,8 +17,23 @@ class UnitItemObj extends React.Component {
 
         <h2>Unit Item Objects</h2>
 
-        <a href="" className="o-unititem__vertical">
+        <a href="" className="o-unititem--vert">
           <div className="o-unititem__title">Corporate Social Responsibility and Financial Performance</div>
+          <ul className="o-unititem__author">
+            <li>Fung, Joe</li>
+            <li>Wu, Abe</li>
+            <li>Reed, Laura K</li>
+            <li>Smith, Sheryl T</li>
+            <li>Barshop, William</li>
+            <li>Wong, Jeannette</li>
+            <li>Dothager, Matthew</li>
+            <li>Lee, Paul</li>
+            <li>Wong, Jeannette</li>
+          </ul>
+        </a>
+
+        <a href="" className="o-unititem--horz">
+          <div className="o-unititem__title">Internalized Stigma of Mental Illness: Psychometric Properties of a New Measure</div>
           <ul className="o-unititem__author">
             <li>Fung, Joe</li>
             <li>Wu, Abe</li>
