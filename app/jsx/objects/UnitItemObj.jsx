7f632bcd005c85d2eb276ca6fb8ec13bc70d2813
JoelUCOP <joel.hagedorn@ucop.edu>
2017-06-22 09:57:28 -0700

    convert unititem type to heading; reorder html & styles

--- app/jsx/objects/UnitItemObj.jsx
+++ app/jsx/objects/UnitItemObj.jsx
@@ -18,6 +18,7 @@ class UnitItemObj extends React.Component {
         <h2>Unit Item Objects</h2>
 
         <a href="" className="o-unititem--vert">
+          <h3 className="o-unititem__type--article">Article</h3>
           <div className="o-unititem__title">Libero doloremque suscipit perferendis amet nostrum! Nostrum quisquam, tempore voluptatum ea dolor, hic esse adipisci reprehenderit ullam minima distinctio. Vero, molestias non.</div>
           <ul className="o-unititem__author">
             <li>Fung, Joe</li>
@@ -30,10 +31,10 @@ class UnitItemObj extends React.Component {
             <li>Lee, Paul</li>
             <li>Wong, Jeannette</li>
           </ul>
-          <div className="o-unititem__type--article" aria-label="Item type:">Article</div>
         </a>
 
         <a href="" className="o-unititem--horz">
+          <h3 className="o-unititem__type--multimedia">Multimedia</h3>
           <div className="o-unititem__title">Sapiente pariatur voluptatibus, quisquam quam libero aspernatur esse dolorem, nisi voluptate accusantium consectetur temporibus maiores quis tempore. Non, architecto eveniet a laudantium.</div>
           <ul className="o-unititem__author">
             <li>Fung, Joe</li>
@@ -46,7 +47,6 @@ class UnitItemObj extends React.Component {
             <li>Lee, Paul</li>
             <li>Wong, Jeannette</li>
           </ul>
-          <div className="o-unititem__type--multimedia" aria-label="Item type:">Multimedia</div>
         </a>
 
       </div>
