8c9e2b08c1a08840b56cfc2171cee8f6ae93903a
JoelUCOP <joel.hagedorn@ucop.edu>
2017-06-23 16:03:17 -0700

    add scholworks book, thesis, and multimedia tag icons

--- app/jsx/layouts/SearchLayout.jsx
+++ app/jsx/layouts/SearchLayout.jsx
@@ -108,7 +108,8 @@ class SearchLayout extends React.Component {
                 <h2>Scholarly Works (12,023 results)</h2>
               </header>
               <SortPaginationComp />
-              {/* ScholWorksComp example #1 below */}
+
+              {/* ScholWorksComp article example below */}
               <section className="c-scholworks">
                 <div className="c-scholworks__main-column">
                   <ul className="c-scholworks__tag-list">
@@ -148,11 +149,11 @@ class SearchLayout extends React.Component {
                 <img className="c-scholworks__article-preview" src="images/temp_article.png" alt="article"/>
               </section>
 
-              {/* ScholWorksComp example #1 below */}
+              {/* ScholWorksComp book example below */}
               <section className="c-scholworks">
                 <div className="c-scholworks__main-column">
                   <ul className="c-scholworks__tag-list">
-                    <li className="c-scholworks__tag-article">Article</li>
+                    <li className="c-scholworks__tag-book">Book</li>
                     <li className="c-scholworks__tag-peer">Peer Reviewed</li>
                   </ul>
                   <heading>
@@ -188,11 +189,11 @@ class SearchLayout extends React.Component {
                 <img className="c-scholworks__article-preview" src="images/temp_article.png" alt="article"/>
               </section>
 
-              {/* ScholWorksComp example #2 below */}
+              {/* ScholWorksComp thesis example below */}
               <section className="c-scholworks">
                 <div className="c-scholworks__main-column">
                   <ul className="c-scholworks__tag-list">
-                    <li className="c-scholworks__tag-article">Article</li>
+                    <li className="c-scholworks__tag-thesis">Thesis</li>
                     <li className="c-scholworks__tag-peer">Peer Reviewed</li>
                   </ul>
                   <heading>
@@ -228,91 +229,11 @@ class SearchLayout extends React.Component {
                 <img className="c-scholworks__article-preview" src="images/temp_article.png" alt="article"/>
               </section>
 
-              {/* ScholWorksComp example #3 below */}
+              {/* ScholWorksComp multimedia example below */}
               <section className="c-scholworks">
                 <div className="c-scholworks__main-column">
                   <ul className="c-scholworks__tag-list">
-                    <li className="c-scholworks__tag-article">Article</li>
-                    <li className="c-scholworks__tag-peer">Peer Reviewed</li>
-                  </ul>
-                  <heading>
-                    <h2 className="c-scholworks__heading">
-                      <a href="">From the New Heights: The City and Migrating Latinas in Real Woman Have Curves and Maria Full of Grace</a>
-                    </h2 >
-                  </heading>
-                  <div className="c-authorlist">
-                    {/* <time> element intentionally omitted */}
-                    <ul className="c-authorlist__list">
-                    {/* links can be left out of the list if no links are desired: */}
-                      <li><a href="">Leung, Wilson</a></li>
-                      <li><a href="">Shaffer, Christopher D</a></li>
-                      <li><a href="">Reed, Laura K</a></li>
-                      <li><a href="">Smith, Sheryl T</a></li>
-                      <li><a href="">Barshop, William</a></li>
-                      <li><a href="">Dirkes, William</a></li>
-                      <li><a href="" className="c-authorlist__list-more-link">et al.</a></li>
-                    </ul>
-                  </div>
-                  <div className="c-scholworks__publication">
-                    <a href="">Mester Journal, Volume 42, Issue 1</a> (2012)
-                  </div>
-                  <div className="c-scholworks__abstract">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, dolor obcaecati magni quas quos ab dignissimos neque beatae, provident unde architecto ut corrupti! Ratione aperiam, recusandae quidem nisi inventore asperiores nam sed eaque totam, quam minima reprehenderit, quisquam cupiditate reiciendis. Vero earum magni asperiores quisquam impedit voluptates eveniet aspernatur assumenda.
-                    </p>
-                  </div>
-                  <div className="c-scholworks__media">
-                    <MediaListComp />
-                    <img className="c-scholworks__license" src="images/cc-by-small.svg" alt="creative commons attribution 4.0 international public license"/>
-                  </div>
-                </div>
-                <img className="c-scholworks__article-preview" src="images/temp_article.png" alt="article"/>
-              </section>
-
-              {/* ScholWorksComp example #4 below */}
-              <section className="c-scholworks">
-                <div className="c-scholworks__main-column">
-                  <ul className="c-scholworks__tag-list">
-                    <li className="c-scholworks__tag-article">Article</li>
-                    <li className="c-scholworks__tag-peer">Peer Reviewed</li>
-                  </ul>
-                  <heading>
-                    <h2 className="c-scholworks__heading">
-                      <a href="">From the New Heights: The City and Migrating Latinas in Real Woman Have Curves and Maria Full of Grace</a>
-                    </h2 >
-                  </heading>
-                  <div className="c-authorlist">
-                    {/* <time> element intentionally omitted */}
-                    <ul className="c-authorlist__list">
-                    {/* links can be left out of the list if no links are desired: */}
-                      <li><a href="">Leung, Wilson</a></li>
-                      <li><a href="">Shaffer, Christopher D</a></li>
-                      <li><a href="">Reed, Laura K</a></li>
-                      <li><a href="">Smith, Sheryl T</a></li>
-                      <li><a href="">Barshop, William</a></li>
-                      <li><a href="">Dirkes, William</a></li>
-                      <li><a href="" className="c-authorlist__list-more-link">et al.</a></li>
-                    </ul>
-                  </div>
-                  <div className="c-scholworks__publication">
-                    <a href="">Mester Journal, Volume 42, Issue 1</a> (2012)
-                  </div>
-                  <div className="c-scholworks__abstract">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, dolor obcaecati magni quas quos ab dignissimos neque beatae, provident unde architecto ut corrupti! Ratione aperiam, recusandae quidem nisi inventore asperiores nam sed eaque totam, quam minima reprehenderit, quisquam cupiditate reiciendis. Vero earum magni asperiores quisquam impedit voluptates eveniet aspernatur assumenda.
-                    </p>
-                  </div>
-                  <div className="c-scholworks__media">
-                    <MediaListComp />
-                    <img className="c-scholworks__license" src="images/cc-by-small.svg" alt="creative commons attribution 4.0 international public license"/>
-                  </div>
-                </div>
-                <img className="c-scholworks__article-preview" src="images/temp_article.png" alt="article"/>
-              </section>
-
-              {/* ScholWorksComp example #5 below */}
-              <section className="c-scholworks">
-                <div className="c-scholworks__main-column">
-                  <ul className="c-scholworks__tag-list">
-                    <li className="c-scholworks__tag-article">Article</li>
+                    <li className="c-scholworks__tag-multimedia">Multimedia</li>
                     <li className="c-scholworks__tag-peer">Peer Reviewed</li>
                   </ul>
                   <heading>
