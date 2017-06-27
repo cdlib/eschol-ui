24829ddcad2b06ac678b07d4409f26a108c03b31
JoelUCOP <joel.hagedorn@ucop.edu>
2017-06-27 12:36:39 -0700

    add medialist comp 'other' and 'zip' supplemental material types and icons

--- app/jsx/components/MediaListComp.jsx
+++ app/jsx/components/MediaListComp.jsx
@@ -6,10 +6,12 @@ class MediaListComp extends React.Component {
   render() {
     return (
       <ul className="c-medialist">
-        <li className="c-medialist__video">Contains 5 videos</li>
         <li className="c-medialist__image">Contains 2 images</li>
-        <li className="c-medialist__pdf">Contains 2 additional PDFs</li>
+        <li className="c-medialist__pdf">Contains 2 PDFs</li>
         <li className="c-medialist__audio">Contains 5 audio files</li>
+        <li className="c-medialist__video">Contains 5 videos</li>
+        <li className="c-medialist__zip">Contains 4 <abbr title="zip">ZIP</abbr> files</li>
+        <li className="c-medialist__other">Contains 3 additional files</li>
       </ul>
     )
   }
