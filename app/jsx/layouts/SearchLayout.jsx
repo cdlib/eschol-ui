deeaab5445f99e2f985c43b967544d1757da6091
JoelUCOP <joel.hagedorn@ucop.edu>
2017-06-27 14:18:14 -0700

    replace search layout book item content with short content example

--- app/jsx/layouts/SearchLayout.jsx
+++ app/jsx/layouts/SearchLayout.jsx
@@ -175,27 +175,22 @@ class SearchLayout extends React.Component {
                   </ul>
                   <heading>
                     <h2 className="c-scholworks__heading">
-                      <a href="">From the New Heights: The City and Migrating Latinas in Real Woman Have Curves and Maria Full of Grace</a>
+                      <a href="">A Fairly Short Title</a>
                     </h2 >
                   </heading>
                   <div className="c-authorlist">
                     {/* <time> element intentionally omitted */}
                     <ul className="c-authorlist__list">
                     {/* links can be left out of the list if no links are desired: */}
-                      <li><a href="">Leung, Wilson</a></li>
-                      <li><a href="">Shaffer, Christopher D</a></li>
-                      <li><a href="">Reed, Laura K</a></li>
-                      <li><a href="">Smith, Sheryl T</a></li>
-                      <li><a href="">Barshop, William</a></li>
-                      <li><a href="">Dirkes, William</a></li>
-                      <li><a href="" className="c-authorlist__list-more-link">et al.</a></li>
+                      <li><a href="">Baker, James</a></li>
+                      <li><a href="">Smith, William B</a></li>
                     </ul>
                   </div>
                   <div className="c-scholworks__publication">
                     <a href="">Mester Journal, Volume 42, Issue 1</a> (2012)
                   </div>
                   <div className="c-scholworks__abstract">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, dolor obcaecati magni quas quos ab dignissimos neque beatae, provident unde architecto ut corrupti! Ratione aperiam, recusandae quidem nisi inventore asperiores nam sed eaque totam, quam minima reprehenderit, quisquam cupiditate reiciendis. Vero earum magni asperiores quisquam impedit voluptates eveniet aspernatur assumenda.
+                    <p>Just a little bit of copy here; not too much, though.
                     </p>
                   </div>
                   <div className="c-scholworks__media">
