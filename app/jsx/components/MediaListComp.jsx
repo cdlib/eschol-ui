6cde8a4294ab864bfbc8e22400ddf0e820977eb8
JoelUCOP <joel.hagedorn@ucop.edu>
2017-06-27 15:08:23 -0700

    update medialist labels with consistent text

--- app/jsx/components/MediaListComp.jsx
+++ app/jsx/components/MediaListComp.jsx
@@ -6,12 +6,12 @@ class MediaListComp extends React.Component {
   render() {
     return (
       <ul className="c-medialist">
-        <li className="c-medialist__image">Contains 2 images</li>
-        <li className="c-medialist__pdf">Contains 2 PDFs</li>
-        <li className="c-medialist__audio">Contains 5 audio files</li>
-        <li className="c-medialist__video">Contains 5 videos</li>
-        <li className="c-medialist__zip">Contains 4 <abbr title="zip">ZIP</abbr> files</li>
-        <li className="c-medialist__other">Contains 3 additional files</li>
+        <li className="c-medialist__image">Contains 2 supplemental images</li>
+        <li className="c-medialist__pdf">Contains 2 supplemental PDFs</li>
+        <li className="c-medialist__audio">Contains 5 supplemental audio</li>
+        <li className="c-medialist__video">Contains 5 supplemental videos</li>
+        <li className="c-medialist__zip">Contains 4 supplemental <abbr title="zip">ZIP</abbr></li>
+        <li className="c-medialist__other">Contains 3 supplemental files</li>
       </ul>
     )
   }
