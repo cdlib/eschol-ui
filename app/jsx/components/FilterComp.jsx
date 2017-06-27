09e8c51dbe115988967bcb338a69b56f830a8f66
JoelUCOP <joel.hagedorn@ucop.edu>
2017-06-27 16:51:06 -0700

    move filter comp buttons into <details>

--- app/jsx/components/FilterComp.jsx
+++ app/jsx/components/FilterComp.jsx
@@ -8,16 +8,19 @@ class FilterComp extends React.Component {
       <form action="" className="c-filter">
         <h2 className="c-filter__heading">Your Search: “Open Access”</h2>
         <div className="c-filter__results">Results: 12 pages, 12,023 works</div>
+      {/*}
         <div className="c-filter__inactive-note">No filters applied</div>
-        <div className="c-filter__active-header">
-          <span id="c-filter__active-title">Active filters:</span>
-          <button>clear all</button>
-        </div>
-        <div role="group" aria-labelledby="c-filter__active-title" className="c-filter__active">
-          <button>Type of work (Articles)</button>
-          <button>Publication Year (All)</button>
-          <button>Campus (UC Berkeley)</button>
-        </div>
+      {*/}
+        <details open className="c-filter__active">
+          <summary><span><strong>3</strong> filters applied</span></summary>
+          <button className="c-filter__clear-all">clear all</button>
+          <ul className="c-filter__active-list">
+            <li><button>Type of work (Articles)</button></li>
+            <li><button>Publication Year (All)</button></li>
+            <li><button>Campus (UC Berkeley)</button></li>
+          </ul>
+        </details>
+
         <a href="" className="c-filter__tips">search tips</a>
       </form>
     )
