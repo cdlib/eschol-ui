1e3154e361682d9c0320416c186f31ead554f744
JoelCDL <joel.hagedorn@ucop.edu>
2017-06-28 16:09:26 -0700

    convert facetbox 'show more' link to button

--- app/jsx/components/FacetBoxComp.jsx
+++ app/jsx/components/FacetBoxComp.jsx
@@ -5,10 +5,10 @@ import React from 'react'
 class FacetBoxComp extends React.Component {
   render() {
     return (
-      <details className="c-facetbox">
+      <details open className="c-facetbox">
         <summary className="c-facetbox__summary"><span>Refine By</span></summary>
         <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias error sit possimus ducimus pariatur, repellat facere quod beatae eaque saepe eos tenetur nulla excepturi culpa aspernatur, quisquam adipisci dolore officia.</div>
-        <a href="" className="c-facetbox__show-more-link">Show more</a>
+        <button className="c-facetbox__show-more">Show more</button>
       </details>
     )
   }
