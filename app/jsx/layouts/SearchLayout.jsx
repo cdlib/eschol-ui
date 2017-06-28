f820791367a1e908895e6de1558c2e3314034d3a
JoelCDL <joel.hagedorn@ucop.edu>
2017-06-28 15:05:04 -0700

    update search layout checkbox 'department' sample with nested list

--- app/jsx/layouts/SearchLayout.jsx
+++ app/jsx/layouts/SearchLayout.jsx
@@ -78,12 +78,40 @@ class SearchLayout extends React.Component {
           <aside>
             <FilterComp />
             <details className="c-facetbox">
-              <summary className="c-facetbox__summary"><span>Refine By</span></summary>
-              <div className="c-checkbox">
-                {/* 'id' and 'htmlFor' values must contain a unique number per pair below for accessibility */}
-                <input id="c-checkbox__number6" type="checkbox" className="c-checkbox__input"/>
-                <label htmlFor="c-checkbox__number6" className="c-checkbox__label">Peer-Reviewed only (##)</label>
-              </div>
+              <summary className="c-facetbox__summary"><span>Department</span></summary>
+              <ul className="c-checkbox">
+                {/* 'id' and 'htmlFor' values must contain a unique number per each pair below for accessibility */}
+                <li>
+                  <input id="c-checkbox__number1" type="checkbox" className="c-checkbox__input"/>
+                  <label htmlFor="c-checkbox__number1" className="c-checkbox__label">Agricultural History Center (##)</label>
+                </li>
+                <li>
+                  <input id="c-checkbox__number2" type="checkbox" className="c-checkbox__input"/>
+                  <label htmlFor="c-checkbox__number2" className="c-checkbox__label">Agriculture and Natural Resources Research and Extension Centers (##)</label>
+                  <ul>
+                    <li>
+                      <input id="c-checkbox__number3" type="checkbox" className="c-checkbox__input"/>
+                      <label htmlFor="c-checkbox__number3" className="c-checkbox__label">Hopland Research and Extension Center (##)</label>
+                    </li>
+                    <li>
+                      <input id="c-checkbox__number4" type="checkbox" className="c-checkbox__input"/>
+                      <label htmlFor="c-checkbox__number4" className="c-checkbox__label">Sierra Foothill Research and Extension Center (##)</label>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <input id="c-checkbox__number5" type="checkbox" className="c-checkbox__input"/>
+                  <label htmlFor="c-checkbox__number5" className="c-checkbox__label">Western Journal of Emergency Medicine: Integrating Emergency Care with Population Health</label>
+                </li>
+                <li>
+                  <input id="c-checkbox__number6" type="checkbox" className="c-checkbox__input"/>
+                  <label htmlFor="c-checkbox__number6" className="c-checkbox__label">PDF</label>
+                </li>
+                <li>
+                  <input id="c-checkbox__number7" type="checkbox" className="c-checkbox__input"/>
+                  <label htmlFor="c-checkbox__number7" className="c-checkbox__label">ZIP</label>
+                </li>
+              </ul>
             </details>
             <details className="c-facetbox">
               <summary className="c-facetbox__summary"><span>Research</span></summary>
