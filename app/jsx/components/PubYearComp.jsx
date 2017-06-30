ccd51f83ce09b42b6534a268141cee7001cb470f
JoelUCOP <joel.hagedorn@ucop.edu>
2017-06-30 12:17:10 -0700

    create initial pubyear html & styles

--- app/jsx/components/PubYearComp.jsx
+++ app/jsx/components/PubYearComp.jsx
@@ -6,7 +6,11 @@ class PubYearComp extends React.Component {
   render() {
     return (
       <div className="c-pubyear">
-        [publication year facet component to go here]
+        {/* placeholder attributes below should be set to the current year via scripting */}
+        <label htmlFor="c-pubyear__textfield1">From:</label>
+        <input id="c-pubyear__textfield1" type="text" maxLength="4" placeholder="2017"/>
+        <label htmlFor="c-pubyear__textfield2">To:</label>
+        <input id="c-pubyear__textfield2" type="text" maxLength="4" placeholder="2017"/>
       </div>
     )
   }
