004b8b25bf8a1ac77255f490732c2ae7abef6a5a
JoelUCOP <joel.hagedorn@ucop.edu>
2017-06-30 16:06:21 -0700

    replace pubyear input placeholder values '2017' with 'YYYY'

--- app/jsx/components/PubYearComp.jsx
+++ app/jsx/components/PubYearComp.jsx
@@ -8,9 +8,9 @@ class PubYearComp extends React.Component {
       <div className="c-pubyear">
         {/* placeholder attributes below should be set to the current year via scripting */}
         <label htmlFor="c-pubyear__textfield1">From:</label>
-        <input id="c-pubyear__textfield1" type="text" maxLength="4" placeholder="2017"/>
+        <input id="c-pubyear__textfield1" type="text" maxLength="4" placeholder="YYYY"/>
         <label htmlFor="c-pubyear__textfield2">To:</label>
-        <input id="c-pubyear__textfield2" type="text" maxLength="4" placeholder="2017"/>
+        <input id="c-pubyear__textfield2" type="text" maxLength="4" placeholder="YYYY"/>
       </div>
     )
   }
