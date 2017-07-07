789592de69d38fa7446923c2689394c21a4eb3ba
JoelUCOP <joel.hagedorn@ucop.edu>
2017-07-07 15:09:49 -0700

    add url layout subheader ucla logo; add custom campusselectors to embargoed and url layouts set to their campuses

--- app/jsx/layouts/SeriesItemUrlLayout.jsx
+++ app/jsx/layouts/SeriesItemUrlLayout.jsx
@@ -18,8 +18,32 @@ class SeriesItemUrlLayout extends React.Component {
         <a href="#maincontent" className="c-skipnav">Skip to main content</a>
         <Header2Comp />
         <div className="c-subheader">
-          <CampusSelectorComp />
-          <img className="c-subheader__banner" src="images/temp_journal-banner.png" alt=""/>
+          <div className="c-campusselector">
+            <h2 className="c-campusselector__heading">
+              <a href="">UC Riverside</a>
+            </h2>
+            <details className="c-campusselector__selector">
+              <summary aria-label="select campus"></summary>
+              <div className="c-campusselector__menu">
+                <div className="c-campusselector__sub-heading" id="c-campusselector__sub-heading">eScholarship at &hellip;</div>
+                <div className="c-campusselector__items" aria-labelledby="c-campusselector__sub-heading" role="list">
+                  <a href="" role="listitem">UC Berkeley</a>
+                  <a href="" role="listitem">UC Davis</a>
+                  <a href="" role="listitem">UC Irvine</a>
+                  <a href="" role="listitem">UCLA</a>
+                  <a href="" role="listitem">UC Merced</a>
+                  <a href="" role="listitem">UC Riverside</a>
+                  <a href="" role="listitem">UC San Diego</a>
+                  <a href="" role="listitem">UC San Francisco</a>
+                  <a href="" role="listitem">UC Santa Barbara</a>
+                  <a href="" role="listitem">UC Santa Cruz</a>
+                  <a href="" role="listitem">UC Office of the President</a>
+                  <a href="" role="listitem">UC Press</a>
+                </div>
+              </div>
+            </details>
+          </div>
+          <img className="c-subheader__banner" src="images/logo_ucr.svg" alt="UC Riverside"/>
           <div className="c-subheader__sidebar">
             <button className="o-button__3">Deposit</button>
           </div>
