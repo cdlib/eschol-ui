227c1d5eaba4b78ecba171b17ca98856d89f4202
JoelUCOP <joel.hagedorn@ucop.edu>
2017-07-17 16:30:58 -0700

    create unitlist comp html, styles, and sample text

--- app/jsx/components/UnitListComp.jsx
+++ app/jsx/components/UnitListComp.jsx
@@ -6,7 +6,18 @@ class UnitListComp extends React.Component {
   render() {
     return (
       <div className="c-unitlist">
-        [unit list component to go here]
+        <h3>Journal</h3>
+        <ul>
+          <li>
+            <a href="">Language Ideologies and Hegemonic Factors Imposed upon Judeo-Spanish Speaking Communitie</a>
+          </li>
+          <li>
+            <a href="">From the New Heights: The City and Migrating Latinas in Real Women Have Curves and María Full of Grace</a>
+          </li>
+          <li>
+            <a href="">Coupled Cardiac Electrophysiology and Contraction using Finite Element</a>
+          </li>
+        </ul>
       </div>
     )
   }
