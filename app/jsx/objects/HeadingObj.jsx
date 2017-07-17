b5614d5eedc396e946ac9f0bb2422f3950443c2b
JoelUCOP <joel.hagedorn@ucop.edu>
2017-07-17 15:36:50 -0700

    create heading object '2a' style and example

--- app/jsx/objects/HeadingObj.jsx
+++ app/jsx/objects/HeadingObj.jsx
@@ -18,6 +18,9 @@ class HeadingObj extends React.Component {
         <h2>Heading Style 2</h2>
         <h1 className="o-heading2">From the New Heights: The City and Migrating Latinas in Real Women Have Curves and María Full of Grace</h1>
 
+        <h2>Heading Style 2a</h2>
+        <h5 className="o-heading2a">From the New Heights: The City and Migrating Latinas in Real Women Have Curves and María Full of Grace</h5>
+
         <h2>Heading Style 3</h2>
         <h3 className="o-heading3">From the New Heights: The City and Migrating Latinas in Real Women Have Curves and María Full of Grace</h3>
 
