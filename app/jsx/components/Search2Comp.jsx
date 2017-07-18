1c1fc0b8a6019d944ce6bbe4f6807a823de512de
JoelUCOP <joel.hagedorn@ucop.edu>
2017-07-18 16:38:49 -0700

    add search2 fieldset/legend for radio buttons

--- app/jsx/components/Search2Comp.jsx
+++ app/jsx/components/Search2Comp.jsx
@@ -14,12 +14,15 @@ class SearchComp2 extends React.Component {
             <label className="c-search2__label" htmlFor="global-search">Search eScholarship</label>
             <input type="search" id="global-search" className="c-search2__field" placeholder="Search" onFocus={this.makeActive} onBlur={this.makeInactive}/>
           </div>
-          <div className={this.state.refineActive ? "c-search2__refine--active" : "c-search2__refine"}>
-            <input type="radio" name="search-refine" id="c-search2__refine-eschol" defaultChecked={true} onFocus={this.makeActive} onBlur={this.makeInactive}/>
-            <label htmlFor="c-search2__refine-eschol">All of eScholarship</label>
-            <input type="radio" name="search-refine" id="c-search2__refine-campus" onFocus={this.makeActive} onBlur={this.makeInactive}/>
-            <label htmlFor="c-search2__refine-campus">This campus</label>
-          </div>
+          <fieldset>
+            <legend>Refine Search</legend>
+            <div className={this.state.refineActive ? "c-search2__refine--active" : "c-search2__refine"}>
+              <input type="radio" name="search-refine" id="c-search2__refine-eschol" defaultChecked={true} onFocus={this.makeActive} onBlur={this.makeInactive}/>
+              <label htmlFor="c-search2__refine-eschol">All of eScholarship</label>
+              <input type="radio" name="search-refine" id="c-search2__refine-campus" onFocus={this.makeActive} onBlur={this.makeInactive}/>
+              <label htmlFor="c-search2__refine-campus">This campus</label>
+            </div>
+          </fieldset>
         </div>
         <button className="c-search2__submit-button" aria-label="search"></button>
         <button className="c-search2__search-close-button" aria-label="close search field" onClick={()=>this.props.onClose()}></button>
