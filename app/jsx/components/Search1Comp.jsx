56fcae410ea1d03aed6d434d62f50a9fe7db4c14
JoelUCOP <joel.hagedorn@ucop.edu>
2017-07-27 16:18:04 -0700

    remove unused aria-labelledby attribute in search1 comp

--- app/jsx/components/Search1Comp.jsx
+++ app/jsx/components/Search1Comp.jsx
@@ -6,7 +6,7 @@ class SearchComp1 extends React.Component {
   render() {
     return (
       <div className="c-search1">
-        <label className="c-search1__label" htmlFor="c-search1__field" aria-labelledby="c-search1__tagline">search</label>
+        <label className="c-search1__label" htmlFor="c-search1__field">search</label>
         <input type="search" id="c-search1__field" className="c-search1__field" placeholder="Search for articles, books, theses, and more"/>
         <button className="c-search1__submit-button" aria-label="submit search"></button>
         <button className="c-search1__search-close-button" aria-label="close search field" onClick = {()=>this.props.onClose()}></button>
