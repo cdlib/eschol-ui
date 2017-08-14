dcac1f536f4fdafa75726e8933dc504ebddb8481
JoelUCOP <joel.hagedorn@ucop.edu>
2017-08-14 16:31:01 -0700

    update journalitemmultimedia layout mediafeature sample html

--- app/jsx/layouts/JournalItemMultimediaLayout.jsx
+++ app/jsx/layouts/JournalItemMultimediaLayout.jsx
@@ -49,12 +49,9 @@ class JournalItemMultimediaLayout extends React.Component {
                 <details className="c-togglecontent" open>
                   <summary>Main Content</summary>
                   <div className="o-mediafeature--video">
+                    <h1>New Horizons Kuiper Belt Fly-Through</h1>
                     <div className="o-mediafeature__item">
                       <video src="images/sample_video.mp4" controls poster="images/sample_video-poster.jpg"></video>
-                    </div>
-                    <div className="o-mediafeature__title-bar">
-                      <h1>New Horizons Kuiper Belt Fly-Through</h1>
-                      <button>Download</button>
                     </div>
                     <div className="o-mediafeature__description">
                       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis optio numquam aut facere odio et accusantium eius quo harum, quis ab molestias illum totam earum nemo voluptatum eligendi nobis. Molestiae reprehenderit et neque nisi nam corporis totam exercitationem, numquam fuga. Sint minima quisquam, velit quod ratione. Vero, libero, accusantium. Rem.
