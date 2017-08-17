22d94fa10d842904829d567c64cf777a3772a350
JoelUCOP <joel.hagedorn@ucop.edu>
2017-08-17 16:01:49 -0700

    simplify mediafile no-grid layout; combine styles

--- app/jsx/objects/MediaFileObj.jsx
+++ app/jsx/objects/MediaFileObj.jsx
@@ -36,7 +36,6 @@ class MediaFileObj extends React.Component {
           <h2 className="o-mediafile__heading">
             Outer Space Music
           </h2>
-          <button className="o-mediafile__view" onClick={this.handleOpenModal}><span>View Media</span></button>
           <div className="o-mediafile__preview" href="images/sample_audio.mp3" onClick={this.handleOpenModal} aria-label="play audio file">
             {/* No preview image for this type. To add, use:
             <img className="o-mediafile__image" src="[image path]" alt="[alt text]" /> */}
@@ -44,6 +43,7 @@ class MediaFileObj extends React.Component {
           <div className="o-mediafile__description">
             Architecto quo praesentium, suscipit, qui est maxime ut repellendus earum odio ab, consequuntur saepe voluptatem commodi dolorum eos adipisci, fugiat tempore ipsa. Reprehenderit corrupti quae ea, veritatis iste, perspiciatis. Ad, reiciendis praesentium!
           </div>
+          <button className="o-mediafile__view" onClick={this.handleOpenModal}><span>View Media</span></button>
           <a href="" className="o-mediafile__download" download>Download</a>
         </div>
 
@@ -53,7 +53,6 @@ class MediaFileObj extends React.Component {
           <h2 className="o-mediafile__heading">
             New Horizons File Archive
           </h2>
-          <button className="o-mediafile__view"><span>View Media</span></button>
           <div className="o-mediafile__preview" href="images/sample_audio.mp3">
             {/* No preview image for this type. To add, use:
             <img className="o-mediafile__image" src="[image path]" alt="[alt text]" /> */}
@@ -61,6 +60,7 @@ class MediaFileObj extends React.Component {
           <div className="o-mediafile__description">
             Eaque, qui rerum fugiat sed. Error aperiam quo atque debitis doloremque. Adipisci, ab placeat! Atque vitae blanditiis, fuga necessitatibus harum libero, repudiandae quasi vero repellendus fugiat asperiores placeat tempore qui vel iusto!
           </div>
+          <button className="o-mediafile__view"><span>View Media</span></button>
           <a href="" className="o-mediafile__download" download>Download</a>
         </div>
 
@@ -70,13 +70,13 @@ class MediaFileObj extends React.Component {
           <h2 className="o-mediafile__heading">
             New Horizons Full Trajectory - Side View
           </h2>
-          <button className="o-mediafile__view"><span>View Media</span></button>
           <div className="o-mediafile__preview" href="images/temp_new-horizons-data.png">
             <img className="o-mediafile__image" src="images/temp_new-horizons-data.png" alt="New horizons data" />
           </div>
           <div className="o-mediafile__description">
             Esse nulla ad veritatis dolorum, vitae maxime tenetur nemo. Quod eius corrupti provident nisi ullam, repellendus molestias, aliquam neque nulla dolorem, magnam commodi ratione enim ex, suscipit labore veniam deserunt nam exercitationem!
           </div>
+          <button className="o-mediafile__view"><span>View Media</span></button>
           <a href="" className="o-mediafile__download" download>Download</a>
         </div>
 
@@ -86,7 +86,6 @@ class MediaFileObj extends React.Component {
           <h2 className="o-mediafile__heading">
             New Horizons Kuiper Belt Fly-through
           </h2>
-          <button className="o-mediafile__view"><span>View Media</span></button>
           <div className="o-mediafile__preview" href="images/sample_video.mp4" aria-label="play video file">
             {/* No preview image for this type. To add, use:
             <img className="o-mediafile__image" src="[image path]" alt="[alt text]" /> */}
@@ -94,6 +93,7 @@ class MediaFileObj extends React.Component {
           <div className="o-mediafile__description">
             Veritatis veniam quidem voluptatem laborum necessitatibus iure facilis laudantium possimus sequi libero aliquid, adipisci explicabo, itaque odit ut delectus nemo quas, quia neque. Error cum porro pariatur vel ducimus aliquid quo adipisci.
           </div>
+          <button className="o-mediafile__view"><span>View Media</span></button>
           <a href="" className="o-mediafile__download" download>Download</a>
         </div>
 
