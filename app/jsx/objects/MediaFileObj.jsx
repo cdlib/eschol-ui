cb8b8161fcd7df24a422c5d287e0ea52c7e739cd
JoelUCOP <joel.hagedorn@ucop.edu>
2017-08-17 14:00:00 -0700

    refactor mediafile headings, element source order; add 'view media' button

--- app/jsx/objects/MediaFileObj.jsx
+++ app/jsx/objects/MediaFileObj.jsx
@@ -34,70 +34,72 @@ class MediaFileObj extends React.Component {
 
         <div className="o-mediafile--audio">
           <h2 className="o-mediafile__heading">
-            <button>Outer Space Music</button>
+            Outer Space Music
           </h2>
-          <div className="o-mediafile__preview" href="images/sample_audio.mp3" aria-label="play audio file">
+          <button className="o-mediafile__view" onClick={this.handleOpenModal}><span>View Media</span></button>
+          <div className="o-mediafile__preview" href="images/sample_audio.mp3" onClick={this.handleOpenModal} aria-label="play audio file">
             {/* No preview image for this type. To add, use:
             <img className="o-mediafile__image" src="[image path]" alt="[alt text]" /> */}
           </div>
-          <a href="" className="o-mediafile__download" download>Download</a>
           <div className="o-mediafile__description">
             Architecto quo praesentium, suscipit, qui est maxime ut repellendus earum odio ab, consequuntur saepe voluptatem commodi dolorum eos adipisci, fugiat tempore ipsa. Reprehenderit corrupti quae ea, veritatis iste, perspiciatis. Ad, reiciendis praesentium!
           </div>
+          <a href="" className="o-mediafile__download" download>Download</a>
         </div>
 
         <h2>Data or PDF File (no preview image)</h2>
 
         <div className="o-mediafile--data">
           <h2 className="o-mediafile__heading">
-            <button>New Horizons File Archive</button>
+            New Horizons File Archive
           </h2>
+          <button className="o-mediafile__view"><span>View Media</span></button>
           <div className="o-mediafile__preview" href="images/sample_audio.mp3">
             {/* No preview image for this type. To add, use:
             <img className="o-mediafile__image" src="[image path]" alt="[alt text]" /> */}
           </div>
-          <a href="" className="o-mediafile__download" download>Download</a>
           <div className="o-mediafile__description">
             Eaque, qui rerum fugiat sed. Error aperiam quo atque debitis doloremque. Adipisci, ab placeat! Atque vitae blanditiis, fuga necessitatibus harum libero, repudiandae quasi vero repellendus fugiat asperiores placeat tempore qui vel iusto!
           </div>
+          <a href="" className="o-mediafile__download" download>Download</a>
         </div>
 
         <h2>Image File (preview image; selected state)</h2>
 
         <div className="o-mediafile--image--selected">
           <h2 className="o-mediafile__heading">
-            <button>New Horizons Full Trajectory - Side View</button>
+            New Horizons Full Trajectory - Side View
           </h2>
+          <button className="o-mediafile__view"><span>View Media</span></button>
           <div className="o-mediafile__preview" href="images/temp_new-horizons-data.png">
             <img className="o-mediafile__image" src="images/temp_new-horizons-data.png" alt="New horizons data" />
           </div>
-          <a href="" className="o-mediafile__download" download>Download</a>
           <div className="o-mediafile__description">
             Esse nulla ad veritatis dolorum, vitae maxime tenetur nemo. Quod eius corrupti provident nisi ullam, repellendus molestias, aliquam neque nulla dolorem, magnam commodi ratione enim ex, suscipit labore veniam deserunt nam exercitationem!
           </div>
+          <a href="" className="o-mediafile__download" download>Download</a>
         </div>
 
         <h2>Video File (no preview image)</h2>
 
         <div className="o-mediafile--video">
           <h2 className="o-mediafile__heading">
-            <button>New Horizons Kuiper Belt Fly-through</button>
+            New Horizons Kuiper Belt Fly-through
           </h2>
+          <button className="o-mediafile__view"><span>View Media</span></button>
           <div className="o-mediafile__preview" href="images/sample_video.mp4" aria-label="play video file">
             {/* No preview image for this type. To add, use:
             <img className="o-mediafile__image" src="[image path]" alt="[alt text]" /> */}
           </div>
-          <a href="" className="o-mediafile__download" download>Download</a>
           <div className="o-mediafile__description">
             Veritatis veniam quidem voluptatem laborum necessitatibus iure facilis laudantium possimus sequi libero aliquid, adipisci explicabo, itaque odit ut delectus nemo quas, quia neque. Error cum porro pariatur vel ducimus aliquid quo adipisci.
           </div>
+          <a href="" className="o-mediafile__download" download>Download</a>
         </div>
 
         {/* Modal */}
 
         <div className="c-modal">
-          {/* onClick element below can be a button or link */}
-          <button onClick={this.handleOpenModal}>Trigger Modal</button>
           <ReactModal 
              isOpen={this.state.showModal}
              contentLabel="onRequestClose Example"
