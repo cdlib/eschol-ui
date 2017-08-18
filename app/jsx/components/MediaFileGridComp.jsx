bb0acfd4dc5e5a142fbe7afcdd694fea8d3a3b08
JoelUCOP <joel.hagedorn@ucop.edu>
2017-08-18 15:34:19 -0700

    import mediamodal comp into mediafilegrid

--- app/jsx/components/MediaFileGridComp.jsx
+++ app/jsx/components/MediaFileGridComp.jsx
@@ -1,20 +1,29 @@
 // ##### Media File Grid Component ##### //
 
 import React from 'react'
+import MediaModalComp from '../components/MediaModalComp.jsx'
 import MediaFileAudioObj from '../objects/MediaFileAudioObj.jsx'
 import MediaFileDataObj from '../objects/MediaFileDataObj.jsx'
 import MediaFileImageObj from '../objects/MediaFileImageObj.jsx'
 import MediaFileVideoObj from '../objects/MediaFileVideoObj.jsx'
 
 class MediaFileGridComp extends React.Component {
+  
   render() {
     return (
-      <div className="c-mediafilegrid">
-        <MediaFileAudioObj />
-        <MediaFileDataObj />
-        <MediaFileImageObj />
-        <MediaFileVideoObj />
+      <div>
+        
+        <div className="c-mediafilegrid">
+          <MediaFileAudioObj />
+          <MediaFileDataObj />
+          <MediaFileImageObj />
+          <MediaFileVideoObj />
+        </div>
+
+        <MediaModalComp />
+
       </div>
+      
     )
   }
 }
