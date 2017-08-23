1170dbfbdf8147630a6d0cce3ff4a09d2e8b4f1a
JoelCDL <joel.hagedorn@ucop.edu>
2017-08-23 15:53:04 -0700

    add wizard jsx toggle logic and test buttons

--- app/jsx/components/WizardComp.jsx
+++ app/jsx/components/WizardComp.jsx
@@ -9,15 +9,31 @@ import WizardTypeComp from './WizardTypeComp.jsx'
 import WizardUnitComp from './WizardUnitComp.jsx'
 
 class WizardComp extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {wizardStep: 1}
+  }
+
   render() {
     return (
       <div className="c-wizard">
-        <WizardRoleComp />
-        <WizardCampusComp />
-        <WizardTypeComp />
-        <WizardUnitComp />
-        <WizardSeriesComp />
-        <WizardLinkComp />
+
+        {/* Temporary buttons to test toggle */}
+        <button onClick = {()=> this.setState({wizardStep: 1})}>Role</button>
+        <button onClick = {()=> this.setState({wizardStep: 2})}>Campus</button>
+        <button onClick = {()=> this.setState({wizardStep: 3})}>Type</button>
+        <button onClick = {()=> this.setState({wizardStep: 4})}>Unit</button>
+        <button onClick = {()=> this.setState({wizardStep: 5})}>Series</button>
+        <button onClick = {()=> this.setState({wizardStep: 6})}>Link</button>
+        <br/><br/>
+
+        {this.state.wizardStep === 1 ? <WizardRoleComp /> : null}
+        {this.state.wizardStep === 2 ? <WizardCampusComp /> : null}
+        {this.state.wizardStep === 3 ? <WizardTypeComp /> : null}
+        {this.state.wizardStep === 4 ? <WizardUnitComp /> : null}
+        {this.state.wizardStep === 5 ? <WizardSeriesComp /> : null}
+        {this.state.wizardStep === 6 ? <WizardLinkComp /> : null}
       </div>
     )
   }
