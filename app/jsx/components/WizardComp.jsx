e113296a81e86c9ced00c58e53f54b2a9f1b7a45
JoelCDL <joel.hagedorn@ucop.edu>
2017-08-30 16:46:40 -0700

    remove wizard comp test code

--- app/jsx/components/WizardComp.jsx
+++ app/jsx/components/WizardComp.jsx
@@ -12,72 +12,45 @@ class WizardComp extends React.Component {
 
   constructor(props){
     super(props)
-    this.state = {wizardStep: 1, wizardDir: "fwd"}
+    this.state = {wizardStep: 1, wizardDir: 'fwd'}
   }
 
-  goForward = (step) =>{this.setState({wizardStep: step, wizardDir: "fwd", prevStep: this.state.wizardStep})}
+  goForward = (step) =>{this.setState({wizardStep: step, wizardDir: 'fwd', prevStep: this.state.wizardStep})}
 
   goBackward = ()=>{
     if (this.state.prevStep)
-      this.setState({wizardStep: this.state.prevStep, wizardDir: "bkw", prevStep: null})
+      this.setState({wizardStep: this.state.prevStep, wizardDir: 'bkw', prevStep: null})
     else
-      this.setState({wizardStep: this.state.wizardStep - 1, wizardDir: "bkw", prevStep: null})
+      this.setState({wizardStep: this.state.wizardStep - 1, wizardDir: 'bkw', prevStep: null})
   }
 
   render() {
     return (
-      <div>
+      <div className="c-wizard">
 
-      {/* Temporary buttons to test toggle */}
-        <button onClick = {()=> this.setState({wizardStep: 1})}>Role</button>
-        <button onClick = {()=> this.setState({wizardStep: 2})}>Campus</button>
-        <button onClick = {()=> this.setState({wizardStep: 3})}>Type</button>
-        <button onClick = {()=> this.setState({wizardStep: 4})}>Unit</button>
-        <button onClick = {()=> this.setState({wizardStep: 5})}>Series</button>
-        <button onClick = {()=> this.setState({wizardStep: 6})}>Link</button>
-        <br/><br/>
-
-        <div className="c-wizard">
-
-          <div className={this.state.wizardStep === 1 ? `c-wizard__current-${this.state.wizardDir}` : `c-wizard__standby-${this.state.wizardDir}`} aria-hidden={this.state.wizardStep === 1 ? null : true}>
-            <WizardRoleComp goForward = {this.goForward} />
-          </div>
-
-          <div className={this.state.wizardStep === 2 ? `c-wizard__current-${this.state.wizardDir}` : `c-wizard__standby-${this.state.wizardDir}`} aria-hidden={this.state.wizardStep === 2 ? null : true}>
-            <WizardCampusComp goForward = {this.goForward} goBackward = {this.goBackward} />
-          </div>
-
-          <div className={this.state.wizardStep === 3 ? `c-wizard__current-${this.state.wizardDir}` : `c-wizard__standby-${this.state.wizardDir}`} aria-hidden={this.state.wizardStep === 3 ? null : true}>
-            <WizardTypeComp goForward = {this.goForward} goBackward = {this.goBackward} />
-          </div>
-
-          <div className={this.state.wizardStep === 4 ? `c-wizard__current-${this.state.wizardDir}` : `c-wizard__standby-${this.state.wizardDir}`} aria-hidden={this.state.wizardStep === 4 ? null : true}>
-            <WizardUnitComp goForward = {this.goForward} goBackward = {this.goBackward} />
-          </div>
-
-          <div className={this.state.wizardStep === 5 ? `c-wizard__current-${this.state.wizardDir}` : `c-wizard__standby-${this.state.wizardDir}`} aria-hidden={this.state.wizardStep === 5 ? null : true}>
-            <WizardSeriesComp goForward = {this.goForward} goBackward = {this.goBackward} />
-          </div>
-
-          <div className={this.state.wizardStep === 6 ? `c-wizard__current-${this.state.wizardDir}` : `c-wizard__standby-${this.state.wizardDir}`} aria-hidden={this.state.wizardStep === 6 ? null : true}>
-            <WizardLinkComp goBackward = {this.goBackward} />
-          </div>
-
+        <div className={this.state.wizardStep === 1 ? `c-wizard__current-${this.state.wizardDir}` : `c-wizard__standby-${this.state.wizardDir}`} aria-hidden={this.state.wizardStep === 1 ? null : true}>
+          <WizardRoleComp goForward = {this.goForward} />
         </div>
 
+        <div className={this.state.wizardStep === 2 ? `c-wizard__current-${this.state.wizardDir}` : `c-wizard__standby-${this.state.wizardDir}`} aria-hidden={this.state.wizardStep === 2 ? null : true}>
+          <WizardCampusComp goForward = {this.goForward} goBackward = {this.goBackward} />
+        </div>
 
-          {/*}
-          <div className={this.state.stepActive ? "c-wizard--active" : "c-wizard"}>
+        <div className={this.state.wizardStep === 3 ? `c-wizard__current-${this.state.wizardDir}` : `c-wizard__standby-${this.state.wizardDir}`} aria-hidden={this.state.wizardStep === 3 ? null : true}>
+          <WizardTypeComp goForward = {this.goForward} goBackward = {this.goBackward} />
+        </div>
 
-          {this.state.wizardStep === 1 ? <WizardRoleComp /> : null}
-          {this.state.wizardStep === 2 ? <WizardCampusComp /> : null}
-          {this.state.wizardStep === 3 ? <WizardTypeComp /> : null}
-          {this.state.wizardStep === 4 ? <WizardUnitComp /> : null}
-          {this.state.wizardStep === 5 ? <WizardSeriesComp /> : null}
-          {this.state.wizardStep === 6 ? <WizardLinkComp /> : null}
+        <div className={this.state.wizardStep === 4 ? `c-wizard__current-${this.state.wizardDir}` : `c-wizard__standby-${this.state.wizardDir}`} aria-hidden={this.state.wizardStep === 4 ? null : true}>
+          <WizardUnitComp goForward = {this.goForward} goBackward = {this.goBackward} />
+        </div>
 
-          </div>
-          {*/}
+        <div className={this.state.wizardStep === 5 ? `c-wizard__current-${this.state.wizardDir}` : `c-wizard__standby-${this.state.wizardDir}`} aria-hidden={this.state.wizardStep === 5 ? null : true}>
+          <WizardSeriesComp goForward = {this.goForward} goBackward = {this.goBackward} />
+        </div>
+
+        <div className={this.state.wizardStep === 6 ? `c-wizard__current-${this.state.wizardDir}` : `c-wizard__standby-${this.state.wizardDir}`} aria-hidden={this.state.wizardStep === 6 ? null : true}>
+          <WizardLinkComp goBackward = {this.goBackward} />
+        </div>
 
       </div>
     )
