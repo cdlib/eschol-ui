8e5041ddda34e29c4c5285041d18f80938f60585
JoelCDL <joel.hagedorn@ucop.edu>
2017-08-30 17:38:34 -0700

    begin adding wizard tab focus logic

--- app/jsx/components/WizardComp.jsx
+++ app/jsx/components/WizardComp.jsx
@@ -12,7 +12,7 @@ class WizardComp extends React.Component {
 
   constructor(props){
     super(props)
-    this.state = {wizardStep: 1, wizardDir: 'fwd'}
+    this.state = {wizardStep: 1, wizardDir: 'fwd', setFocus: false}
   }
 
   goForward = (step) =>{this.setState({wizardStep: step, wizardDir: 'fwd', prevStep: this.state.wizardStep})}
@@ -24,32 +24,39 @@ class WizardComp extends React.Component {
       this.setState({wizardStep: this.state.wizardStep - 1, wizardDir: 'bkw', prevStep: null})
   }
 
+  tabFocus = ()=> {
+    // focus method:
+    // document.querySelector('.c-wizard__step h1').focus()
+    document.querySelector(".c-wizard__step").style.backgroundColor = 'red';
+    this.setState({setFocus: true})
+  }
+
   render() {
     return (
       <div className="c-wizard">
 
         <div className={this.state.wizardStep === 1 ? `c-wizard__current-${this.state.wizardDir}` : `c-wizard__standby-${this.state.wizardDir}`} aria-hidden={this.state.wizardStep === 1 ? null : true}>
-          <WizardRoleComp goForward = {this.goForward} />
+          <WizardRoleComp goForward = {this.goForward} tabFocus />
         </div>
 
         <div className={this.state.wizardStep === 2 ? `c-wizard__current-${this.state.wizardDir}` : `c-wizard__standby-${this.state.wizardDir}`} aria-hidden={this.state.wizardStep === 2 ? null : true}>
-          <WizardCampusComp goForward = {this.goForward} goBackward = {this.goBackward} />
+          <WizardCampusComp goForward = {this.goForward} goBackward = {this.goBackward} tabFocus />
         </div>
 
         <div className={this.state.wizardStep === 3 ? `c-wizard__current-${this.state.wizardDir}` : `c-wizard__standby-${this.state.wizardDir}`} aria-hidden={this.state.wizardStep === 3 ? null : true}>
-          <WizardTypeComp goForward = {this.goForward} goBackward = {this.goBackward} />
+          <WizardTypeComp goForward = {this.goForward} goBackward = {this.goBackward} tabFocus />
         </div>
 
         <div className={this.state.wizardStep === 4 ? `c-wizard__current-${this.state.wizardDir}` : `c-wizard__standby-${this.state.wizardDir}`} aria-hidden={this.state.wizardStep === 4 ? null : true}>
-          <WizardUnitComp goForward = {this.goForward} goBackward = {this.goBackward} />
+          <WizardUnitComp goForward = {this.goForward} goBackward = {this.goBackward} tabFocus />
         </div>
 
         <div className={this.state.wizardStep === 5 ? `c-wizard__current-${this.state.wizardDir}` : `c-wizard__standby-${this.state.wizardDir}`} aria-hidden={this.state.wizardStep === 5 ? null : true}>
-          <WizardSeriesComp goForward = {this.goForward} goBackward = {this.goBackward} />
+          <WizardSeriesComp goForward = {this.goForward} goBackward = {this.goBackward} tabFocus />
         </div>
 
         <div className={this.state.wizardStep === 6 ? `c-wizard__current-${this.state.wizardDir}` : `c-wizard__standby-${this.state.wizardDir}`} aria-hidden={this.state.wizardStep === 6 ? null : true}>
-          <WizardLinkComp goBackward = {this.goBackward} />
+          <WizardLinkComp goBackward = {this.goBackward} tabFocus />
         </div>
 
       </div>
