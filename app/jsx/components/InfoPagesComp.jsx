933597deeea2e683fb511adedec4c64b65496d50
JoelUCOP <joel.hagedorn@ucop.edu>
2017-09-05 16:41:59 -0700

    add 3 more infopages item samples, update item flexbox styles

--- app/jsx/components/InfoPagesComp.jsx
+++ app/jsx/components/InfoPagesComp.jsx
@@ -36,6 +36,30 @@ class InfoPagesComp extends React.Component {
             <div className="c-infopages__text">eScholarship provides open access, scholarly publishing services to the University of California and delivers a dynamic research platform to scholars worldwide.
             </div>
           </div>
+          <div className="c-infopages__item">
+            <h2>
+              <a href="">The Center for Environmental Design Research</a>
+              <a href="" className="c-infopages__title">In the Press</a>
+            </h2>
+            <div className="c-infopages__text">The University of California awards contract to Symplectic for the implementation of a publication harvesting system to support UC’s Open Access Policy" March, 2014
+            </div>
+          </div>
+          <div className="c-infopages__item">
+            <h2>
+              <a href="">eScholarship</a>
+              <a href="" className="c-infopages__title">Lorem Ipsum Dolor Sit Amet</a>
+            </h2>
+            <div className="c-infopages__text">Debitis incidunt aliquam nemo iure alias, amet rerum velit quidem. Quia, provident voluptatum earum mollitia aliquam at nulla quam, sunt! Laboriosam est perspiciatis molestias excepturi ad repellendus.
+            </div>
+          </div>
+          <div className="c-infopages__item">
+            <h2>
+              <a href="">UC Berkeley</a>
+              <a href="" className="c-infopages__title">Consectetur Adipisicing Elit Delectus Veritatis Mollitia</a>
+            </h2>
+            <div className="c-infopages__text">eScholarship provides open access, scholarly publishing services to the University of California and delivers a dynamic research platform to scholars worldwide.
+            </div>
+          </div>
         </div>
         <button className="c-infopages__show-all">Show all informational page results</button>
       </div>
