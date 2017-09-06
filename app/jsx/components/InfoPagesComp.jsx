ee7a4c91d02abee11533e2dc0320cdb85fb2e1f1
JoelCDL <joel.hagedorn@ucop.edu>
2017-09-06 15:37:06 -0700

    create infopages show more/less <div> regions

--- app/jsx/components/InfoPagesComp.jsx
+++ app/jsx/components/InfoPagesComp.jsx
@@ -13,108 +13,112 @@ class InfoPagesComp extends React.Component {
   render() {
     return (
       <div className="c-infopages">
-        <div className="c-infopages__items">
-          <div className="c-infopages__item">
-            <h2>
-              <a href="">The Center for Environmental Design Research</a>
-              <a href="" className="c-infopages__title">In the Press</a>
-            </h2>
-            <div className="c-infopages__text">The University of California awards contract to Symplectic for the implementation of a publication harvesting system to support UC’s Open Access Policy" March, 2014
+        <div className="c-infopages__show-less">
+          <div className="c-infopages__items">
+            <div className="c-infopages__item">
+              <h2>
+                <a href="">The Center for Environmental Design Research</a>
+                <a href="" className="c-infopages__title">In the Press</a>
+              </h2>
+              <div className="c-infopages__text">The University of California awards contract to Symplectic for the implementation of a publication harvesting system to support UC’s Open Access Policy" March, 2014
+              </div>
+            </div>
+            <div className="c-infopages__item">
+              <h2>
+                <a href="">eScholarship</a>
+                <a href="" className="c-infopages__title">Lorem Ipsum Dolor Sit Amet</a>
+              </h2>
+              <div className="c-infopages__text">Debitis incidunt aliquam nemo iure alias, amet rerum velit quidem. Quia, provident voluptatum earum mollitia aliquam at nulla quam, sunt! Laboriosam est perspiciatis molestias excepturi ad repellendus.
+              </div>
+            </div>
+            <div className="c-infopages__item">
+              <h2>
+                <a href="">UC Berkeley</a>
+                <a href="" className="c-infopages__title">Consectetur Adipisicing Elit Delectus Veritatis Mollitia</a>
+              </h2>
+              <div className="c-infopages__text">eScholarship provides open access, scholarly publishing services to the University of California and delivers a dynamic research platform to scholars worldwide.
+              </div>
             </div>
           </div>
-          <div className="c-infopages__item">
-            <h2>
-              <a href="">eScholarship</a>
-              <a href="" className="c-infopages__title">Lorem Ipsum Dolor Sit Amet</a>
-            </h2>
-            <div className="c-infopages__text">Debitis incidunt aliquam nemo iure alias, amet rerum velit quidem. Quia, provident voluptatum earum mollitia aliquam at nulla quam, sunt! Laboriosam est perspiciatis molestias excepturi ad repellendus.
-            </div>
-          </div>
-          <div className="c-infopages__item">
-            <h2>
-              <a href="">UC Berkeley</a>
-              <a href="" className="c-infopages__title">Consectetur Adipisicing Elit Delectus Veritatis Mollitia</a>
-            </h2>
-            <div className="c-infopages__text">eScholarship provides open access, scholarly publishing services to the University of California and delivers a dynamic research platform to scholars worldwide.
-            </div>
-          </div>
+          <button className="c-infopages__show" onClick={()=> this.setState({showMore: true})} hidden={this.state.showMore}>Show more results</button>
         </div>
-        <div className="c-infopages__items" hidden={!this.state.showMore}>
-          <div className="c-infopages__item">
-            <h2>
-              <a href="">The Center for Environmental Design Research</a>
-              <a href="" className="c-infopages__title">In the Press</a>
-            </h2>
-            <div className="c-infopages__text">The University of California awards contract to Symplectic for the implementation of a publication harvesting system to support UC’s Open Access Policy" March, 2014
+        <div className="c-infopages__show-more" hidden={!this.state.showMore}>
+          <div className="c-infopages__items">
+            <div className="c-infopages__item">
+              <h2>
+                <a href="">The Center for Environmental Design Research</a>
+                <a href="" className="c-infopages__title">In the Press</a>
+              </h2>
+              <div className="c-infopages__text">The University of California awards contract to Symplectic for the implementation of a publication harvesting system to support UC’s Open Access Policy" March, 2014
+              </div>
+            </div>
+            <div className="c-infopages__item">
+              <h2>
+                <a href="">eScholarship</a>
+                <a href="" className="c-infopages__title">Lorem Ipsum Dolor Sit Amet</a>
+              </h2>
+              <div className="c-infopages__text">Debitis incidunt aliquam nemo iure alias, amet rerum velit quidem. Quia, provident voluptatum earum mollitia aliquam at nulla quam, sunt! Laboriosam est perspiciatis molestias excepturi ad repellendus.
+              </div>
+            </div>
+            <div className="c-infopages__item">
+              <h2>
+                <a href="">UC Berkeley</a>
+                <a href="" className="c-infopages__title">Consectetur Adipisicing Elit Delectus Veritatis Mollitia</a>
+              </h2>
+              <div className="c-infopages__text">eScholarship provides open access, scholarly publishing services to the University of California and delivers a dynamic research platform to scholars worldwide.
+              </div>
+            </div>
+            <div className="c-infopages__item">
+              <h2>
+                <a href="">The Center for Environmental Design Research</a>
+                <a href="" className="c-infopages__title">In the Press</a>
+              </h2>
+              <div className="c-infopages__text">The University of California awards contract to Symplectic for the implementation of a publication harvesting system to support UC’s Open Access Policy" March, 2014
+              </div>
+            </div>
+            <div className="c-infopages__item">
+              <h2>
+                <a href="">eScholarship</a>
+                <a href="" className="c-infopages__title">Lorem Ipsum Dolor Sit Amet</a>
+              </h2>
+              <div className="c-infopages__text">Debitis incidunt aliquam nemo iure alias, amet rerum velit quidem. Quia, provident voluptatum earum mollitia aliquam at nulla quam, sunt! Laboriosam est perspiciatis molestias excepturi ad repellendus.
+              </div>
+            </div>
+            <div className="c-infopages__item">
+              <h2>
+                <a href="">UC Berkeley</a>
+                <a href="" className="c-infopages__title">Consectetur Adipisicing Elit Delectus Veritatis Mollitia</a>
+              </h2>
+              <div className="c-infopages__text">eScholarship provides open access, scholarly publishing services to the University of California and delivers a dynamic research platform to scholars worldwide.
+              </div>
+            </div>
+            <div className="c-infopages__item">
+              <h2>
+                <a href="">The Center for Environmental Design Research</a>
+                <a href="" className="c-infopages__title">In the Press</a>
+              </h2>
+              <div className="c-infopages__text">The University of California awards contract to Symplectic for the implementation of a publication harvesting system to support UC’s Open Access Policy" March, 2014
+              </div>
+            </div>
+            <div className="c-infopages__item">
+              <h2>
+                <a href="">eScholarship</a>
+                <a href="" className="c-infopages__title">Lorem Ipsum Dolor Sit Amet</a>
+              </h2>
+              <div className="c-infopages__text">Debitis incidunt aliquam nemo iure alias, amet rerum velit quidem. Quia, provident voluptatum earum mollitia aliquam at nulla quam, sunt! Laboriosam est perspiciatis molestias excepturi ad repellendus.
+              </div>
+            </div>
+            <div className="c-infopages__item">
+              <h2>
+                <a href="">UC Berkeley</a>
+                <a href="" className="c-infopages__title">Consectetur Adipisicing Elit Delectus Veritatis Mollitia</a>
+              </h2>
+              <div className="c-infopages__text">eScholarship provides open access, scholarly publishing services to the University of California and delivers a dynamic research platform to scholars worldwide.
+              </div>
             </div>
           </div>
-          <div className="c-infopages__item">
-            <h2>
-              <a href="">eScholarship</a>
-              <a href="" className="c-infopages__title">Lorem Ipsum Dolor Sit Amet</a>
-            </h2>
-            <div className="c-infopages__text">Debitis incidunt aliquam nemo iure alias, amet rerum velit quidem. Quia, provident voluptatum earum mollitia aliquam at nulla quam, sunt! Laboriosam est perspiciatis molestias excepturi ad repellendus.
-            </div>
-          </div>
-          <div className="c-infopages__item">
-            <h2>
-              <a href="">UC Berkeley</a>
-              <a href="" className="c-infopages__title">Consectetur Adipisicing Elit Delectus Veritatis Mollitia</a>
-            </h2>
-            <div className="c-infopages__text">eScholarship provides open access, scholarly publishing services to the University of California and delivers a dynamic research platform to scholars worldwide.
-            </div>
-          </div>
-          <div className="c-infopages__item">
-            <h2>
-              <a href="">The Center for Environmental Design Research</a>
-              <a href="" className="c-infopages__title">In the Press</a>
-            </h2>
-            <div className="c-infopages__text">The University of California awards contract to Symplectic for the implementation of a publication harvesting system to support UC’s Open Access Policy" March, 2014
-            </div>
-          </div>
-          <div className="c-infopages__item">
-            <h2>
-              <a href="">eScholarship</a>
-              <a href="" className="c-infopages__title">Lorem Ipsum Dolor Sit Amet</a>
-            </h2>
-            <div className="c-infopages__text">Debitis incidunt aliquam nemo iure alias, amet rerum velit quidem. Quia, provident voluptatum earum mollitia aliquam at nulla quam, sunt! Laboriosam est perspiciatis molestias excepturi ad repellendus.
-            </div>
-          </div>
-          <div className="c-infopages__item">
-            <h2>
-              <a href="">UC Berkeley</a>
-              <a href="" className="c-infopages__title">Consectetur Adipisicing Elit Delectus Veritatis Mollitia</a>
-            </h2>
-            <div className="c-infopages__text">eScholarship provides open access, scholarly publishing services to the University of California and delivers a dynamic research platform to scholars worldwide.
-            </div>
-          </div>
-          <div className="c-infopages__item">
-            <h2>
-              <a href="">The Center for Environmental Design Research</a>
-              <a href="" className="c-infopages__title">In the Press</a>
-            </h2>
-            <div className="c-infopages__text">The University of California awards contract to Symplectic for the implementation of a publication harvesting system to support UC’s Open Access Policy" March, 2014
-            </div>
-          </div>
-          <div className="c-infopages__item">
-            <h2>
-              <a href="">eScholarship</a>
-              <a href="" className="c-infopages__title">Lorem Ipsum Dolor Sit Amet</a>
-            </h2>
-            <div className="c-infopages__text">Debitis incidunt aliquam nemo iure alias, amet rerum velit quidem. Quia, provident voluptatum earum mollitia aliquam at nulla quam, sunt! Laboriosam est perspiciatis molestias excepturi ad repellendus.
-            </div>
-          </div>
-          <div className="c-infopages__item">
-            <h2>
-              <a href="">UC Berkeley</a>
-              <a href="" className="c-infopages__title">Consectetur Adipisicing Elit Delectus Veritatis Mollitia</a>
-            </h2>
-            <div className="c-infopages__text">eScholarship provides open access, scholarly publishing services to the University of California and delivers a dynamic research platform to scholars worldwide.
-            </div>
-          </div>
+          <button className="c-infopages__show" onClick={()=> this.setState({showMore: false})}>Show less results</button>
         </div>
-        <button className="c-infopages__show" onClick={()=> this.setState({showMore: true})} hidden={this.state.showMore}>Show more informational page results</button>
-        <button className="c-infopages__show" onClick={()=> this.setState({showMore: false})} hidden={!this.state.showMore}>Show less informational page results</button>
       </div>
     )
   }
