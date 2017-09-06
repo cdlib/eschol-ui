768b9b10b7d3b51518c0778da0cb687cabb14ba0
JoelCDL <joel.hagedorn@ucop.edu>
2017-09-06 12:53:19 -0700

    add infopages show more/less toggle logic

--- app/jsx/components/InfoPagesComp.jsx
+++ app/jsx/components/InfoPagesComp.jsx
@@ -5,6 +5,8 @@ import $ from 'jquery'
 import dotdotdot from 'jquery.dotdotdot'
 
 class InfoPagesComp extends React.Component {
+  state={showMore: false}
+
   componentDidMount() {
     $('.c-infopages__text').dotdotdot({watch:"window"});
   }
@@ -12,6 +14,56 @@ class InfoPagesComp extends React.Component {
     return (
       <div className="c-infopages">
         <div className="c-infopages__items">
+          <div className="c-infopages__item">
+            <h2>
+              <a href="">The Center for Environmental Design Research</a>
+              <a href="" className="c-infopages__title">In the Press</a>
+            </h2>
+            <div className="c-infopages__text">The University of California awards contract to Symplectic for the implementation of a publication harvesting system to support UC’s Open Access Policy" March, 2014
+            </div>
+          </div>
+          <div className="c-infopages__item">
+            <h2>
+              <a href="">eScholarship</a>
+              <a href="" className="c-infopages__title">Lorem Ipsum Dolor Sit Amet</a>
+            </h2>
+            <div className="c-infopages__text">Debitis incidunt aliquam nemo iure alias, amet rerum velit quidem. Quia, provident voluptatum earum mollitia aliquam at nulla quam, sunt! Laboriosam est perspiciatis molestias excepturi ad repellendus.
+            </div>
+          </div>
+          <div className="c-infopages__item">
+            <h2>
+              <a href="">UC Berkeley</a>
+              <a href="" className="c-infopages__title">Consectetur Adipisicing Elit Delectus Veritatis Mollitia</a>
+            </h2>
+            <div className="c-infopages__text">eScholarship provides open access, scholarly publishing services to the University of California and delivers a dynamic research platform to scholars worldwide.
+            </div>
+          </div>
+        </div>
+        <div className="c-infopages__items" hidden={!this.state.showMore}>
+          <div className="c-infopages__item">
+            <h2>
+              <a href="">The Center for Environmental Design Research</a>
+              <a href="" className="c-infopages__title">In the Press</a>
+            </h2>
+            <div className="c-infopages__text">The University of California awards contract to Symplectic for the implementation of a publication harvesting system to support UC’s Open Access Policy" March, 2014
+            </div>
+          </div>
+          <div className="c-infopages__item">
+            <h2>
+              <a href="">eScholarship</a>
+              <a href="" className="c-infopages__title">Lorem Ipsum Dolor Sit Amet</a>
+            </h2>
+            <div className="c-infopages__text">Debitis incidunt aliquam nemo iure alias, amet rerum velit quidem. Quia, provident voluptatum earum mollitia aliquam at nulla quam, sunt! Laboriosam est perspiciatis molestias excepturi ad repellendus.
+            </div>
+          </div>
+          <div className="c-infopages__item">
+            <h2>
+              <a href="">UC Berkeley</a>
+              <a href="" className="c-infopages__title">Consectetur Adipisicing Elit Delectus Veritatis Mollitia</a>
+            </h2>
+            <div className="c-infopages__text">eScholarship provides open access, scholarly publishing services to the University of California and delivers a dynamic research platform to scholars worldwide.
+            </div>
+          </div>
           <div className="c-infopages__item">
             <h2>
               <a href="">The Center for Environmental Design Research</a>
@@ -61,7 +113,8 @@ class InfoPagesComp extends React.Component {
             </div>
           </div>
         </div>
-        <button className="c-infopages__show-all">Show all informational page results</button>
+        <button className="c-infopages__show" onClick={()=> this.setState({showMore: true})} hidden={this.state.showMore}>Show more informational page results</button>
+        <button className="c-infopages__show" onClick={()=> this.setState({showMore: false})} hidden={!this.state.showMore}>Show less informational page results</button>
       </div>
     )
   }
