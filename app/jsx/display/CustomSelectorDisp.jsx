9c574acf8ee94627b59b3f8d406580bcc649303a
JoelUCOP <joel.hagedorn@ucop.edu>
2017-09-18 14:47:25 -0700

    separate both custom selector objects into new files

--- app/jsx/display/CustomSelectorDisp.jsx
+++ app/jsx/display/CustomSelectorDisp.jsx
@@ -2,12 +2,29 @@
 
 import React from 'react'
 import CustomSelectorObj from '../objects/CustomSelectorObj.jsx'
+import CustomSelectorCampusObj from '../objects/CustomSelectorCampusObj.jsx'
 
 class CustomSelectorDisp extends React.Component {
   render() {
     return (
       <div>
+
+        <p>Custom Selector (Generic):</p>
+        <ul>
+          <li>Left-aligned heading</li>
+          <li>Heading without link</li>
+          <li>No item group sub-heading</li>
+        </ul>
         <CustomSelectorObj />
+
+        <p>Custom Selector (Campus):</p>
+        <ul>
+          <li>Right-aligned heading</li>
+          <li>Heading with link</li>
+          <li>Item group sub-heading</li>
+        </ul>
+        <CustomSelectorCampusObj />
+
       </div>
     )
   }
