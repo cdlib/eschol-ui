5b66d7b622146d2da5bea5dfd28133cfe21299b3
JoelUCOP <joel.hagedorn@ucop.edu>
2017-09-28 16:13:38 -0700

    add authorlist comp samples to relateditems comp

--- app/jsx/components/RelatedItemsComp.jsx
+++ app/jsx/components/RelatedItemsComp.jsx
@@ -10,31 +10,71 @@ class RelatedItemsComp extends React.Component {
           <h3>
             <a className="o-textlink__secondary" href="">Entre la ficción y el periodismo: Cambio social y la crónica mexicana contemporánea</a>
           </h3>
-          <span>Nadeau, Evelyn</span>
+          <div className="c-authorlist">
+            <ul className="c-authorlist__list">
+              <li>Nadeau, Evelyn</li>
+              <li>Shaffer, Christopher D</li>
+              <li>Reed, Laura K</li>
+              <li>Smith, Sheryl T</li>
+              <li>Barshop, William</li>
+            </ul>
+          </div>
         </li>
         <li>
           <h3>
             <a className="o-textlink__secondary" href="">Journalism in Catalonia During Francoism</a>
-            </h3>
-          <span>Reguant, Monserrat</span>
+          </h3>
+          <div className="c-authorlist">
+            <ul className="c-authorlist__list">
+              <li>Reguant, Monserrat</li>
+              <li>Shaffer, Christopher D</li>
+              <li>Reed, Laura K</li>
+              <li>Smith, Sheryl T</li>
+              <li>Barshop, William</li>
+            </ul>
+          </div>
         </li>
         <li>
           <h3>
             <a className="o-textlink__secondary" href="">En torno a un cuento olvidado de Clarín: "El oso mayor"</a>
           </h3>
-          <span>Gil, Angeles Ezama</span>
+          <div className="c-authorlist">
+            <ul className="c-authorlist__list">
+              <li>Gil, Angeles Ezama</li>
+              <li>Shaffer, Christopher D</li>
+              <li>Reed, Laura K</li>
+              <li>Smith, Sheryl T</li>
+              <li>Barshop, William</li>
+            </ul>
+          </div>
         </li>
         <li>
           <h3>
             <a className="o-textlink__secondary" href="">Interview with Guillermo Cabrera Infante</a>
           </h3>
-          <span>Graham-Jones, Jean; Deosthale, Duleeh3</span>
+          <div className="c-authorlist">
+            <ul className="c-authorlist__list">
+              <li>Graham-Jones</li>
+              <li>Shaffer, Christopher D</li>
+              <li>Reed, Laura K</li>
+              <li>Smith, Sheryl T</li>
+              <li>Barshop, William</li>
+            </ul>
+          </div>
         </li>
         <li>
           <h3>
             <a className="o-textlink__secondary" href="">Lazlo Moussong. Castillos en la letra. Xalah3a, México: Universidad Veracruzana, 1986.</a>
           </h3>
-          <span>Radchik, Laura</span>
+          <div className="c-authorlist">
+            <ul className="c-authorlist__list">
+              <li>Radchik, Laura</li>
+              <li>Shaffer, Christopher D</li>
+              <li>Reed, Laura K</li>
+              <li>Smith, Sheryl T</li>
+              <li>Barshop, William</li>
+            </ul>
+          </div>
         </li>
       </ul>
     )
