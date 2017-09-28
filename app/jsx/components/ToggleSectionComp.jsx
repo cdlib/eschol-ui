b7a801222a4f2247cb8bf64b55f009c0c33c192f
JoelUCOP <joel.hagedorn@ucop.edu>
2017-09-28 16:56:32 -0700

    add toglesection heading, header margin

--- app/jsx/components/ToggleSectionComp.jsx
+++ app/jsx/components/ToggleSectionComp.jsx
@@ -6,7 +6,9 @@ class ToggleSectionComp extends React.Component {
   render() {
     return (
       <section className="c-togglesection">
-        <header>Toggle Section Heading</header>
+        <header>
+          <h2>Toggle Section Heading</h2>
+        </header>
         Toggle Section Content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus temporibus expedita laudantium fugiat, ipsam, repellat labore, adipisci itaque est id optio corrupti. Quas laudantium nobis magnam dignissimos, autem incidunt optio?
       </section>
     )
