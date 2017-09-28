9bac39ee93bacff6e100317f340efb014d63abfa
JoelUCOP <joel.hagedorn@ucop.edu>
2017-09-28 14:58:24 -0700

    create initial hero comp html & styles

--- app/jsx/components/HeroComp.jsx
+++ app/jsx/components/HeroComp.jsx
@@ -6,7 +6,11 @@ class HeroComp extends React.Component {
   render() {
     return (
       <div className="c-hero">
-        [hero component to go here]
+        <h1>Open Access Publications from the University of California</h1>
+        <div className="c-hero__campuses">
+          <span>UC Davis</span>
+          <a href="">Explore all of our campuses</a>
+        </div>
       </div>
     )
   }
