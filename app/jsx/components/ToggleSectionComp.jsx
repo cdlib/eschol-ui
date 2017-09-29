287489ea5219957bddbeef0d58d9741c46d3efaa
JoelUCOP <joel.hagedorn@ucop.edu>
2017-09-29 14:34:33 -0700

    update togglesection toggle logic

--- app/jsx/components/ToggleSectionComp.jsx
+++ app/jsx/components/ToggleSectionComp.jsx
@@ -3,15 +3,15 @@
 import React from 'react'
 
 class ToggleSectionComp extends React.Component {
-  state={showMore: false}
+  state={showContent: true}
 
   render() {
     return (
       <section className="c-togglesection">
-        <header tabIndex="0" onClick={()=> this.setState({showMore: true})}>
+        <header tabIndex="0" onClick={()=> this.setState({showContent: !this.state.showContent})}>
           <h2>Toggle Section Heading</h2>
         </header>
-        <div className="c-togglesection__content" hidden={!this.state.showMore}>
+        <div className="c-togglesection__content" hidden={!this.state.showContent}>
           Toggle Section Content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus temporibus expedita laudantium fugiat, ipsam, repellat labore, adipisci itaque est id optio corrupti. Quas laudantium nobis magnam dignissimos, autem incidunt optio?
         </div>
       </section>
