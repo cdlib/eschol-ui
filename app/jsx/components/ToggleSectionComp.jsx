9e225c90820f30927c085f255c90bd2ceef39e69
JoelUCOP <joel.hagedorn@ucop.edu>
2017-09-29 13:15:24 -0700

    add initial toggleselection toggle logic

--- app/jsx/components/ToggleSectionComp.jsx
+++ app/jsx/components/ToggleSectionComp.jsx
@@ -3,13 +3,15 @@
 import React from 'react'
 
 class ToggleSectionComp extends React.Component {
+  state={showMore: false}
+
   render() {
     return (
       <section className="c-togglesection">
-        <header>
+        <header tabIndex="0" onClick={()=> this.setState({showMore: true})}>
           <h2>Toggle Section Heading</h2>
         </header>
-        <div className="c-togglesection__content">
+        <div className="c-togglesection__content" hidden={!this.state.showMore}>
           Toggle Section Content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus temporibus expedita laudantium fugiat, ipsam, repellat labore, adipisci itaque est id optio corrupti. Quas laudantium nobis magnam dignissimos, autem incidunt optio?
         </div>
       </section>
