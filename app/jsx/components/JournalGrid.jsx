66b607276a0d6592be709f77cd0a5260376ad981
JoelUCOP <joel.hagedorn@ucop.edu>
2017-10-02 15:25:45 -0700

    create initial journalgrid styles with sample images

--- app/jsx/components/JournalGrid.jsx
+++ app/jsx/components/JournalGrid.jsx
@@ -6,7 +6,14 @@ class JournalGridComp extends React.Component {
   render() {
     return (
       <div className="c-journalgrid">
-        [journal grid component to go here]
+        <img src="http://escholarship.org/issueCovers/ucla_spanport_mester/43_01_cover.png" alt="Mester journal"/>
+        <img src="http://escholarship.org/issueCovers/gis_globalsocieties/05_00_cover.png" alt="Berkeley Review of Education journal"/>
+        <img src="http://escholarship.org/issueCovers/ucb_crp_bpj/28_01_cover.png" alt="Berkeley Planning Journal"/>
+        <img src="http://escholarship.org/issueCovers/anrcs_californiaagriculture/71_03_cover.png" alt="Californial Agriculture journal"/>
+        <img src="http://escholarship.org/issueCovers/uclalaw_cllr/34_01_cover.png" alt="Chicana/o-Latina/o Law Review journal"/>
+        <div className="c-journalgrid__all">
+          <a href="">View all <strong>68</strong> current journals</a>
+        </div>
       </div>
     )
   }
