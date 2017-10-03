a221f18b0300df543fe0cd3dfc33318ed5b955ee
JoelUCOP <joel.hagedorn@ucop.edu>
2017-10-03 15:07:24 -0700

    update homesection1 structure; add grid and heading styles

--- app/jsx/components/HomeSection1Comp.jsx
+++ app/jsx/components/HomeSection1Comp.jsx
@@ -7,11 +7,13 @@ class HomeSection1Comp extends React.Component {
     return (
       <div className="c-homesection1">
         <h2 className="c-homesection1__heading">Why Open Access?</h2>
-        <h3 className="o-heading4a">Good for Authors</h3>
-        <p>Open access research is read and cited more than access-restricted scholarship, increasing the academic impact of and public engagement with your ideas.</p>
-        <h3 className="o-heading4a">Good for Readers</h3>
-        <p>The University of California’s Open Access policies extend the University’s public mission to share broadly -- throughout California, the nation, and the world -- the research and knowledge produced at our campuses.</p>
-        <h3>Metrics of Use</h3>
+        <div className="c-homesection1__description">
+          <h3>Good for Authors</h3>
+          <p>Open access research is read and cited more than access-restricted scholarship, increasing the academic impact of and public engagement with your ideas.</p>
+          <h3>Good for Readers</h3>
+          <p>The University of California’s Open Access policies extend the University’s public mission to share broadly -- throughout California, the nation, and the world -- the research and knowledge produced at our campuses.</p>
+        </div>
+        <h3 className="c-homesection1__stat-heading">Metrics of Use</h3>
         <div className="o-stat">
           <div className="o-stat--item">
             <a href="">24,844</a> Items
@@ -23,7 +25,7 @@ class HomeSection1Comp extends React.Component {
             <a href="">6,532</a> Items since UC <br/> OA Policy passed
           </div>
         </div>
-        <a href="" className="o-textlink__external-link">More about UC Open Access</a>
+        <a href="" className="c-homesection1__more">More about UC Open Access</a>
       </div>
     )
   }
