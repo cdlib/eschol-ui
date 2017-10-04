29e578c23273c7e4b6b374c8c0569db183261f76
JoelCDL <joel.hagedorn@ucop.edu>
2017-10-04 15:25:32 -0700

    udpate homesection3 content; add grid styles

--- app/jsx/components/HomeSection3Comp.jsx
+++ app/jsx/components/HomeSection3Comp.jsx
@@ -7,12 +7,17 @@ class HomeSection3Comp extends React.Component {
   render() {
     return (
       <div className="c-homesection3">
-        <p className="o-heading4a">eScholarship is also an open access publishing platform</p>
-        <p>eScholarship Publishing is an open access publishing platform subsidized by the University of California, managed by the California Digital Library, and offered free of charge to UC-affiliated departments, research units, publishing programs and individual scholars.</p>
-        <p>We offer publishing and production tools, including a full editorial and peer review system, as well as professional support and consulting services.</p>
-        <a href="" className="o-textlink__external-link">How to start a journal</a>
-        <JournalGridComp />
-        <a href="" className="o-textlink__external-link">Browse all eScholarship journals</a>
+        <strong className="c-homesection3__heading">eScholarship is also an open access publishing platform</strong>
+        <div className="c-homesection3__description">
+          <h3>eScholarship Publishing</h3>
+          <p>eScholarship Publishing is an open access publishing platform subsidized by the University of California, managed by the California Digital Library, and offered free of charge to UC-affiliated departments, research units, publishing programs and individual scholars.</p>
+          <p>We offer publishing and production tools, including a full editorial and peer review system, as well as professional support and consulting services.</p>
+        </div>
+        <a href="" className="c-homesection3__start">How to start a journal</a>
+        <div className="c-homesection3__journalgrid">
+          <JournalGridComp />
+        </div>
+        <a href="" className="c-homesection3__browse-all">Browse all eScholarship journals</a>
       </div>
     )
   }
