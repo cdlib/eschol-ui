fa9ca3cc0e201559bf77d13159d69e990d9b323f
JoelUCOP <joel.hagedorn@ucop.edu>
2017-10-10 11:26:47 -0700

    create initial teaser comp html

--- app/jsx/components/TeaserComp.jsx
+++ app/jsx/components/TeaserComp.jsx
@@ -6,7 +6,22 @@ class TeaserComp extends React.Component {
   render() {
     return (
       <div className="c-teaser">
-        [teaser component to go here]
+        <section>
+          <header>
+            <h2>eScholarship Repository</h2>
+            <p>Illum officiis eos animi blanditiis dolores aliquam quia eum expedita beatae iste alias.
+            </p>
+            <a href="">Learn more</a>
+          </header>
+        </section>
+        <section>
+          <header>
+            <h2>eScholarship Publishing</h2>
+            <p>Quod dolorem, nostrum ad quas quam doloribus labore quia a modi officiis unde natus.
+            </p>
+            <a href="">Learn more</a>
+          </header>
+        </section>
       </div>
     )
   }
