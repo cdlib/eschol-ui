e1911c0dddcd271b97299d5ce2bae50e6898306d
JoelUCOP <joel.hagedorn@ucop.edu>
2017-10-13 10:56:13 -0700

    update hero campus classname; combine campus link styles

--- app/jsx/components/HeroComp.jsx
+++ app/jsx/components/HeroComp.jsx
@@ -7,7 +7,7 @@ class HeroComp extends React.Component {
     return (
       <div className="c-hero" style={{backgroundImage: "url('images/hero-ucd.jpg')"}}>
         <h1>Open Access Publications from the University of California</h1>
-        <div className="c-hero__campuses">
+        <div className="c-hero__campus">
           <span>UC Davis</span>
           <a href="">Explore all of our campuses</a>
         </div>
