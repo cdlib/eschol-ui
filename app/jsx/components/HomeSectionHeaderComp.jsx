0fe7f8a61ddda1b452be1bf4dc5a8e567a36b81e
JoelUCOP <joel.hagedorn@ucop.edu>
2017-10-13 15:30:51 -0700

    setup homesectionheader component files

---
+++ app/jsx/components/HomeSectionHeaderComp.jsx
@@ -0,0 +1,15 @@
+// ##### Home Section Header Component ##### //
+
+import React from 'react'
+
+class HomeSectionHeaderComp extends React.Component {
+  render() {
+    return (
+      <section className="c-homesectionheader">
+        <header>[home section header goes here]</header>
+      </section>
+    )
+  }
+}
+
+module.exports = HomeSectionHeaderComp;
---
+++ app/jsx/display/HomeSectionHeaderDisp.jsx
@@ -0,0 +1,16 @@
+// ##### Home Section Header Display ##### //
+
+import React from 'react'
+import HomeSectionHeaderComp from '../components/HomeSectionHeaderComp.jsx'
+
+class HomeSectionHeaderDisp extends React.Component {
+  render() {
+    return (
+      <div>
+        <HomeSectionHeaderComp />
+      </div>
+    )
+  }
+}
+
+module.exports = HomeSectionHeaderDisp;
