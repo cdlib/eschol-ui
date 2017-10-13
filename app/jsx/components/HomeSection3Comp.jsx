d296ad7c737d5c4a064b05fa38402e628a2276f0
JoelUCOP <joel.hagedorn@ucop.edu>
2017-10-13 11:50:23 -0700

    swap homesection3 link order

--- app/jsx/components/HomeSection3Comp.jsx
+++ app/jsx/components/HomeSection3Comp.jsx
@@ -13,11 +13,11 @@ class HomeSection3Comp extends React.Component {
           <p>eScholarship Publishing is an open access publishing platform subsidized by the University of California, managed by the California Digital Library, and offered free of charge to UC-affiliated departments, research units, publishing programs and individual scholars.</p>
           <p>We offer publishing and production tools, including a full editorial and peer review system, as well as professional support and consulting services.</p>
         </div>
-        <a href="" className="c-homesection3__start">How to start a journal</a>
         <div className="c-homesection3__journalgrid">
           <JournalGridComp />
         </div>
         <a href="" className="c-homesection3__browse-all">Browse all eScholarship journals</a>
+        <a href="" className="c-homesection3__start">How to start a journal</a>
       </div>
     )
   }
