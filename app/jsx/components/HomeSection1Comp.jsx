5dca05e447fd35c3b7c63cda6478e3f6ba99d8de
JoelUCOP <joel.hagedorn@ucop.edu>
2017-10-17 11:52:57 -0700

    replace section1 minuses in text with em dashes

--- app/jsx/components/HomeSection1Comp.jsx
+++ app/jsx/components/HomeSection1Comp.jsx
@@ -10,7 +10,7 @@ class HomeSection1Comp extends React.Component {
           <h3>Good for Authors</h3>
           <p>Open access research is read and cited more than access-restricted scholarship, increasing the academic impact of and public engagement with your ideas.</p>
           <h3>Good for Readers</h3>
-          <p>The University of California’s Open Access policies extend the University’s public mission to share broadly -- throughout California, the nation, and the world -- the research and knowledge produced at our campuses.</p>
+          <p>The University of California’s Open Access policies extend the University’s public mission to share broadly &mdash; throughout California, the nation, and the world &mdash; the research and knowledge produced at our campuses.</p>
         </div>
         <h3 className="c-homesection__1-metrics-heading">Metrics of Use</h3>
         <div className="o-stat">
