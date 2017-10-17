056b0e500d561130402f77a64509de10dc213588
JoelUCOP <joel.hagedorn@ucop.edu>
2017-10-17 16:42:18 -0700

    create initial campushero html & styles

--- app/jsx/components/CampusHeroComp.jsx
+++ app/jsx/components/CampusHeroComp.jsx
@@ -6,7 +6,11 @@ class CampusHeroComp extends React.Component {
   render() {
     return (
       <div className="c-campushero">
-        [campus hero component to go here]
+        <h1>Share your research with a global audience</h1>
+        <div className="c-campushero__text">
+          The eScholarship suite of open access publishing services gives UC scholars direct control over the creation and dissemination of the full range of their research.
+        </div>
+        <a href="" className="c-campushero__link">Get started</a>
       </div>
     )
   }
