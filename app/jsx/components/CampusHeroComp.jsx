5ea7fd918ba91c62a855b85b23ac91dc89f03f3c
JoelCDL <joel.hagedorn@ucop.edu>
2017-10-18 12:09:22 -0700

    add campushero image and initial screeen3 styles

--- app/jsx/components/CampusHeroComp.jsx
+++ app/jsx/components/CampusHeroComp.jsx
@@ -5,8 +5,10 @@ import React from 'react'
 class CampusHeroComp extends React.Component {
   render() {
     return (
-      <div className="c-campushero">
-        <h1>Share your research with a global audience</h1>
+      <div className="c-campushero" style={{backgroundImage: "url('images/hero-ucb.jpg')"}}>
+        <div className="c-campushero__lede">
+          Share your research with a global audience
+        </div>
         <div className="c-campushero__text">
           The eScholarship suite of open access publishing services gives UC scholars direct control over the creation and dissemination of the full range of their research.
         </div>
