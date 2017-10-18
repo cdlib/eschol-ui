966bbc8b3dfd06ff3d8a7a095cd03410c6df4e7e
JoelCDL <joel.hagedorn@ucop.edu>
2017-10-18 14:57:34 -0700

    add campushero screen3 grid, gradients, margin/padding

--- app/jsx/components/CampusHeroComp.jsx
+++ app/jsx/components/CampusHeroComp.jsx
@@ -7,12 +7,14 @@ class CampusHeroComp extends React.Component {
     return (
       <div className="c-campushero" style={{backgroundImage: "url('images/hero-ucb.jpg')"}}>
         <div className="c-campushero__lede">
-          Share your research with a global audience
+          Share your research <span>with a global audience</span>
         </div>
         <div className="c-campushero__text">
           The eScholarship suite of open access publishing services gives UC scholars direct control over the creation and dissemination of the full range of their research.
         </div>
-        <a href="" className="c-campushero__link">Get started</a>
+        <div className="c-campushero__link">
+          <a href="">Get started</a>
+        </div>
       </div>
     )
   }
