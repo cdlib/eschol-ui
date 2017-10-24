ee4059e9ac94f498edd7514c86678e1b4e0408b2
JoelUCOP <joel.hagedorn@ucop.edu>
2017-10-24 15:36:02 -0700

    remove medialist 'contains' text

--- app/jsx/components/MediaListComp.jsx
+++ app/jsx/components/MediaListComp.jsx
@@ -6,12 +6,12 @@ class MediaListComp extends React.Component {
   render() {
     return (
       <ul className="c-medialist">
-        <li className="c-medialist__image">Contains 2 supplemental images</li>
-        <li className="c-medialist__pdf">Contains 2 supplemental PDFs</li>
-        <li className="c-medialist__audio">Contains 5 supplemental audio files</li>
-        <li className="c-medialist__video">Contains 5 supplemental videos</li>
-        <li className="c-medialist__zip">Contains 4 supplemental <abbr title="zips">ZIPs</abbr></li>
-        <li className="c-medialist__other">Contains 3 supplemental files</li>
+        <li className="c-medialist__image">2 supplemental images</li>
+        <li className="c-medialist__pdf">2 supplemental PDFs</li>
+        <li className="c-medialist__audio">5 supplemental audio files</li>
+        <li className="c-medialist__video">5 supplemental videos</li>
+        <li className="c-medialist__zip">4 supplemental <abbr title="zips">ZIPs</abbr></li>
+        <li className="c-medialist__other">3 supplemental files</li>
       </ul>
     )
   }
