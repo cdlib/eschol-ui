0fba9146c94b735311f90d9674912c569adc6c53
JoelCDL <joel.hagedorn@ucop.edu>
2017-10-25 11:55:16 -0700

    add renamed unitseries files

---
+++ app/jsx/display/UnitSeriesDisp.jsx
@@ -0,0 +1,16 @@
+// ##### Unit Series Display ##### //
+
+import React from 'react'
+import UnitSeriesComp from '../components/UnitSeriesComp.jsx'
+
+class UnitSeriesDisp extends React.Component {
+  render() {
+    return (
+      <div>
+        <UnitSeriesComp />
+      </div>
+    )
+  }
+}
+
+module.exports = UnitSeriesDisp;
