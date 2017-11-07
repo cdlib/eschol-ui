0564e2372bb0e0b2714d65114aeac8f3724a571b
JoelUCOP <joel.hagedorn@ucop.edu>
2017-11-07 11:24:30 -0800

    change pagination links to buttons; update styles, code comments

--- app/jsx/components/PaginationComp.jsx
+++ app/jsx/components/PaginationComp.jsx
@@ -6,13 +6,17 @@ class PaginationComp extends React.Component {
   render() {
     return (
       <div className="c-pagination">
-        <a className="c-pagination__prevnext" href="">Previous</a>
-        <a className="c-pagination__item" href="">1</a>
-        <a className="c-pagination__item--current" aria-label="2 current" href="">2</a>
-        <a className="c-pagination__item" href="">3</a>
+        {/* If the result set is "1", then the "Previous" button should be disabled */}
+        <button disabled className="c-pagination__prevnext">Previous</button>
+        {/* Note the <span>, not <button> for the current result set, with the aria-label containing 'current' text */}
+        <span className="c-pagination__item--current" aria-label="2 current">1</span>
+        <button className="c-pagination__item">2</button>
+
+        <button className="c-pagination__item">3</button>
         <span className="c-pagination__ellipses">&hellip;</span>
-        <a className="c-pagination__item" href="">342</a>
-        <a className="c-pagination__prevnext" href="">Next</a>
+        <button className="c-pagination__item">342</button>
+        {/* If the result set is the last in the recordset, then the "Next" button should be disabled */}
+        <button className="c-pagination__prevnext">Next</button>
       </div>
     )
   }
