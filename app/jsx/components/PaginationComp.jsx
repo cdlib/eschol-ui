7f957e41c0c43aa1969b01cd82bd4e93516409dd
JoelUCOP <joel.hagedorn@ucop.edu>
2017-11-07 10:05:31 -0800

    add pagination aria-label for 'current'set

--- app/jsx/components/PaginationComp.jsx
+++ app/jsx/components/PaginationComp.jsx
@@ -7,8 +7,8 @@ class PaginationComp extends React.Component {
     return (
       <div className="c-pagination">
         <a className="c-pagination__prevnext" href="">Previous</a>
-        <a className="c-pagination__item--active" href="">1</a>
-        <a className="c-pagination__item" href="">2</a>
+        <a className="c-pagination__item" href="">1</a>
+        <a className="c-pagination__item--current" aria-label="2 current" href="">2</a>
         <a className="c-pagination__item" href="">3</a>
         <span className="c-pagination__ellipses">&hellip;</span>
         <a className="c-pagination__item" href="">342</a>
