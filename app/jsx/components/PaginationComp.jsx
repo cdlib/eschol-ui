2c7f1ccf0bbdc30d035b83a3651df974f32fb80f
JoelUCOP <joel.hagedorn@ucop.edu>
2017-11-20 14:46:43 -0800

    update pagination aria-label samples, sass notes

--- app/jsx/components/PaginationComp.jsx
+++ app/jsx/components/PaginationComp.jsx
@@ -11,18 +11,13 @@ class PaginationComp extends React.Component {
         <nav className="c-pagination">
           <ul>
             {/*
-            If the result set is first in the recordset, then change the "Previous" <a> tag to a <span>, as below:
-            
-            Note the aria-label text differences in each list item below:
+            Note the aria-label text differences in each list item below, including "last" result set:
             */}
-            <li className="c-pagination__prev" aria-label="go to previous result set">Previous</li>
             <li><a href="" className="c-pagination__item" aria-label="go to result set 1">1</a></li>
             <li><a href="" className="c-pagination__item" aria-label="go to result set 2">2</a></li>
             <li><a href="" className="c-pagination__item--current" aria-label="you are on result set 3">3</a></li>
             <li><a href="" className="c-pagination__item" aria-label="go to result set 4">4</a></li>
             <li><a href="" className="c-pagination__item" aria-label="go to result set 5">5</a></li>
-            {/* If the result set is last in the recordset, then change the "Next" <a> tag to a <span> below: */}
-            <li className="c-pagination__next" aria-label="go to next result set">Next</li>
           </ul>
         </nav>
 
@@ -30,17 +25,13 @@ class PaginationComp extends React.Component {
         <nav className="c-pagination--next">
           <ul>
             {/*
-            If the result set is first in the recordset, then change the "Previous" <a> tag to a <span>, as below:
-            
-            Note the aria-label text differences in each list item below:
+            Note the aria-label text differences in each list item below, including "last" result set:
             */}
-            <li className="c-pagination__prev" aria-label="go to previous result set">Previous</li>
             <li><a href="" className="c-pagination__item" aria-label="go to result set 1">1</a></li>
             <li><a href="" className="c-pagination__item" aria-label="go to result set 2">2</a></li>
             <li><a href="" className="c-pagination__item--current" aria-label="you are on result set 3">3</a></li>
             <li><a href="" className="c-pagination__item" aria-label="go to result set 4">4</a></li>
-            <li><a href="" className="c-pagination__item" aria-label="go to result set 1000">1000</a></li>
-            {/* If the result set is last in the recordset, then change the "Next" <a> tag to a <span> below: */}
+            <li><a href="" className="c-pagination__item" aria-label="go to last result set 1000">1000</a></li>
             <li className="c-pagination__next"><a href="" aria-label="go to next result set">Next</a></li>
           </ul>
         </nav>
@@ -49,17 +40,14 @@ class PaginationComp extends React.Component {
         <nav className="c-pagination--prev--next">
           <ul>
             {/*
-            If the result set is first in the recordset, then change the "Previous" <a> tag to a <span>, as below:
-            
-            Note the aria-label text differences in each list item below:
+            Note the aria-label text differences in each list item below, including "last" result set:
             */}
             <li className="c-pagination__prev"><a href="" aria-label="go to previous result set">Previous</a></li>
             <li><a href="" className="c-pagination__item" aria-label="go to result set 1">1</a></li>
             <li><a href="" className="c-pagination__item" aria-label="go to result set 46">46</a></li>
             <li><a href="" className="c-pagination__item--current" aria-label="you are on result set 47">47</a></li>
             <li><a href="" className="c-pagination__item" aria-label="go to result set 48">48</a></li>
-            <li><a href="" className="c-pagination__item" aria-label="go to result set 1000">1000</a></li>
-            {/* If the result set is last in the recordset, then change the "Next" <a> tag to a <span> below: */}
+            <li><a href="" className="c-pagination__item" aria-label="go to last result set 1000">1000</a></li>
             <li className="c-pagination__next"><a href="" aria-label="go to next result set">Next</a></li>
           </ul>
         </nav>
@@ -68,18 +56,14 @@ class PaginationComp extends React.Component {
         <nav className="c-pagination--prev">
           <ul>
             {/*
-            If the result set is first in the recordset, then change the "Previous" <a> tag to a <span>, as below:
-            
-            Note the aria-label text differences in each list item below:
+            Note the aria-label text differences in each list item below, including "last" result set:
             */}
             <li className="c-pagination__prev"><a href="" aria-label="go to previous result set">Previous</a></li>
             <li><a href="" className="c-pagination__item" aria-label="go to result set 1">1</a></li>
             <li><a href="" className="c-pagination__item" aria-label="go to result set 997">997</a></li>
             <li><a href="" className="c-pagination__item--current" aria-label="you are on result set 998">998</a></li>
             <li><a href="" className="c-pagination__item" aria-label="go to result set 999">999</a></li>
-            <li><a href="" className="c-pagination__item" aria-label="go to result set 1000">1000</a></li>
-            {/* If the result set is last in the recordset, then change the "Next" <a> tag to a <span> below: */}
-            <li className="c-pagination__next" aria-label="go to next result set">Next</li>
+            <li><a href="" className="c-pagination__item" aria-label="go to last result set 1000">1000</a></li>
           </ul>
         </nav>
       </div>
