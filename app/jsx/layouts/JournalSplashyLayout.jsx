2d8fa882fec2a79feade59f87832b24991cb33b1
JoelUCOP <joel.hagedorn@ucop.edu>
2017-11-28 11:59:39 -0800

    remove issue comp thumbnail util styles

--- app/jsx/layouts/JournalSplashyLayout.jsx
+++ app/jsx/layouts/JournalSplashyLayout.jsx
@@ -7,7 +7,7 @@ import BreadcrumbComp from '../components/BreadcrumbComp.jsx'
 import MarqueeComp from '../components/MarqueeComp.jsx'
 import ItemActionsComp from '../components/ItemActionsComp.jsx'
 import CustomSelectorObj from '../objects/CustomSelectorObj.jsx'
-import MediaListComp from '../components/MediaListComp.jsx'
+import IssueComp from '../components/IssueComp.jsx'
 import PubComp from '../components/PubComp.jsx'
 import JournalInfoComp from '../components/JournalInfoComp.jsx'
 import RelatedItemsComp from '../components/RelatedItemsComp.jsx'
@@ -72,44 +72,7 @@ class JournalSplashyLayout extends React.Component {
             <section className="o-columnbox1">
               <ItemActionsComp />
               <CustomSelectorObj />
-              <div className="c-pubpreview">
-                <a className="c-pubpreview__img" href="">
-                  <img src="http://placehold.it/150x200?text=Image" alt="placeholder" />
-                </a>
-                <div className="c-pub">
-                  <h3 className="c-pub__heading">
-                    <a href="">Language Ideologies and Hegemonic Factors Imposed upon Judeo-Spanish Speaking Communities</a>
-                  </h3>
-                  <div className="c-authorlist">
-                    <ul className="c-authorlist__list">
-                    {/* links can be left out of the list if no links are desired: */}
-                      <li><a href="">Leung, Wilson</a></li>
-                      <li><a href="">Shaffer, Christopher D</a></li>
-                      <li><a href="">Reed, Laura K</a></li>
-                      <li><a href="">Smith, Sheryl T</a></li>
-                      <li><a href="">Barshop, William</a></li>
-                      <li><a href="">Dirkes, William</a></li>
-                      <li><a href="">Dothager, Matthew</a></li>
-                      <li><a href="">Lee, Paul</a></li>
-                      <li><a href="">Wong, Jeannette</a></li>
-                      <li><a href="">Xiong, David</a></li>
-                      <li><a href="">Yuan, Han</a></li>
-                      <li><a href="">Bedard James E J</a></li>
-                      <li><a href="">Machone, Joshua F</a></li>
-                      <li><a href="">Patterson, Seantay D</a></li>
-                      <li><a href="">Price, Amber L</a></li>
-                      <li><a href="">Turner, Bryce A</a></li>
-                      <li><a href="">Robic, Srebrenka</a></li>
-                      <li><a href="" className="c-authorlist__list-more-link">et al.</a></li>
-                    </ul>
-                  </div>
-                  <div className="c-pub__abstract">
-                    <p>Itaque iure laboriosam est porro ipsam veritatis quidem error nostrum officiis molestias aperiam voluptate voluptas odit suscipit velit, repellendus non, provident quia quibusdam labore. Id consectetur, reiciendis soluta sint earum nam nihil autem tempora aperiam adipisci magnam quidem quia velit, eum quasi veniam magni explicabo quae commodi quod vitae libero. In, officiis! Qui, expedita nihil ipsa quibusdam quia, enim voluptas corrupti placeat.
-                    </p>
-                  </div>
-                  <MediaListComp />
-                </div>
-              </div>
+              <IssueComp />
               <h3 className="o-heading1a">Table of Contents</h3>
               <div className="o-dividecontent2x--ruled">
                 <PubComp />
