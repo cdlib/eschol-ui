52003c604f68db285a8f86be89298cf3773cf0a6
JoelUCOP <joel.hagedorn@ucop.edu>
2017-12-04 15:12:53 -0800

    replace share comp interactive-element-size util with new interactive-summary-size util

--- app/jsx/components/ShareComp.jsx
+++ app/jsx/components/ShareComp.jsx
@@ -6,7 +6,7 @@ class ShareComp extends React.Component {
   render() {
     return (
       <details className="c-share">
-        <summary><span>Share</span></summary>
+        <summary>Share</summary>
         <div className="c-share__list">
           <a className="c-share__email" href="">Email</a>
           <a className="c-share__facebook" href="">Facebook</a>
