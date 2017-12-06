9fa9c5dfe5a57ec4d81998d0eea085e311e4ce1e
JoelCDL <joel.hagedorn@ucop.edu>
2017-12-06 14:23:46 -0800

    fix issue comp caption overflow bug

--- app/jsx/components/IssueComp.jsx
+++ app/jsx/components/IssueComp.jsx
@@ -12,7 +12,7 @@ class IssueComp extends React.Component {
           <ThumbnailComp
             src="https://escholarship.org/images/homecover_fb.png"
             alt="journal cover" />
-          <figcaption><i>Cover Caption:</i> Quasi ipsum beatae dolores veritatis cumque impedit optio ipsam.</figcaption>
+          <figcaption><i>Cover Caption:</i> The cover image is from Multimedia artist Saya Woolfalk’s 2012 project titled The Empathics. This performance piece is set in a fictional hybrid plant/human community that Woolfalk ethnographically analyzes, documents, and displays.</figcaption>
         </figure>
         <div className="c-issue__description">
           <p>Aut vitae, ipsa magnam, voluptates cum deleniti quaerat asperiores. Repudiandae reprehenderit tempora beatae cumque nulla, molestias rem atque dolore modi deserunt veniam quod numquam voluptatibus dolor ut illo. Dolorum natus autem aliquid commodi nesciunt ducimus quis libero enim dolorem reprehenderit amet iusto labore pariatur expedita nisi sed recusandae, ullam! Quos quae accusantium incidunt repudiandae maxime cupiditate! Sequi qui sapiente neque quasi exercitationem.
