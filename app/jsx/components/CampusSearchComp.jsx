0acac6994473fed03dd6538cf8b6a6d70ed3cc63
JoelCDL <joel.hagedorn@ucop.edu>
2017-12-06 16:04:43 -0800

    begin campussearch refactor

--- app/jsx/components/CampusSearchComp.jsx
+++ app/jsx/components/CampusSearchComp.jsx
@@ -6,14 +6,11 @@ class CampusSearchComp extends React.Component {
   render() {
     return (
       <div className="c-campussearch">
-        <label htmlFor="c-campussearch__search" className="c-campussearch__label">Discover UC Berkeley scholarship</label>
+        <label htmlFor="c-campussearch__search" className="c-campussearch__label">Discover UC Santa Barbara eScholarship</label>
         <div className="c-campussearch__search">
           <input type="search" className="c-campussearch__input" id="c-campussearch__search"/>
           <button className="c-campussearch__button" aria-label="Search"></button>
         </div>
-        <small className="c-campussearch__subtext">
-          Looking for research data? <a href="">Visit UC Berkeley Dash</a>.
-        </small>
       </div>
     )
   }
