75764a44048a484264eb8e6c1e2c159a399a2501
JoelUCOP <joel.hagedorn@ucop.edu>
2017-12-08 15:24:40 -0800

    simplify campussearch comp layout

--- app/jsx/components/CampusSearchComp.jsx
+++ app/jsx/components/CampusSearchComp.jsx
@@ -6,7 +6,7 @@ class CampusSearchComp extends React.Component {
   render() {
     return (
       <div className="c-campussearch">
-        <label htmlFor="c-campussearch__search" className="c-campussearch__label">Discover UC Santa Barbara eScholarship</label>
+        <label htmlFor="c-campussearch__search" className="c-campussearch__label">Discover Lawrence Berkeley National Laboratory scholarship</label>
         <div className="c-campussearch__search">
           <input type="search" className="c-campussearch__input" id="c-campussearch__search"/>
           <button className="c-campussearch__button" aria-label="Search"></button>
