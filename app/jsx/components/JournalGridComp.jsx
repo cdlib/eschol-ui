a2a4a2a2b8a8e0481f97e0a9cef7a8de80dc9a62
JoelUCOP <joel.hagedorn@ucop.edu>
2017-12-11 15:08:19 -0800

    update journalgrid img src paths

--- app/jsx/components/JournalGridComp.jsx
+++ app/jsx/components/JournalGridComp.jsx
@@ -6,11 +6,11 @@ class JournalGridComp extends React.Component {
   render() {
     return (
       <div className="c-journalgrid">
-        <img src="http://escholarship.org/issueCovers/ucla_spanport_mester/43_01_cover.png" alt="Mester journal"/>
-        <img src="http://escholarship.org/issueCovers/gis_globalsocieties/05_00_cover.png" alt="Berkeley Review of Education journal"/>
-        <img src="http://escholarship.org/issueCovers/ucb_crp_bpj/28_01_cover.png" alt="Berkeley Planning Journal"/>
-        <img src="http://escholarship.org/issueCovers/anrcs_californiaagriculture/71_03_cover.png" alt="Californial Agriculture journal"/>
-        <img src="http://escholarship.org/issueCovers/uclalaw_cllr/34_01_cover.png" alt="Chicana/o-Latina/o Law Review journal"/>
+        <img src="https://escholarship.org/images/homecover_mester.png" alt="Mester journal"/>
+        <img src="https://escholarship.org/images/homecover_interactions.png" alt="Interactions journal"/>
+        <img src="https://escholarship.org/images/homecover_bpj.jpg" alt="Berkeley Planning Journal"/>
+        <img src="https://escholarship.org/images/homecover_fb.png" alt="Frontiers journal"/>
+        <img src="https://escholarship.org/images/homecover_jcmrs.png" alt="Critical journal"/>
         <div className="c-journalgrid__all">
           <a href="">View all <strong>68</strong> current journals</a>
         </div>
