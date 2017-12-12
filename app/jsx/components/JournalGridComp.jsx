9f4d54eff048fcd144715b73df25a0ff1efc86e1
JoelUCOP <joel.hagedorn@ucop.edu>
2017-12-12 15:52:31 -0800

    replace journalgrid static images with lazyload images

--- app/jsx/components/JournalGridComp.jsx
+++ app/jsx/components/JournalGridComp.jsx
@@ -1,25 +1,36 @@
 // ##### Journal Grid Component ##### //
 
 import React from 'react'
+import LazyLoadComp from '../components/LazyLoadComp.jsx'
 
 class JournalGridComp extends React.Component {
   render() {
     return (
       <div className="c-journalgrid">
         <a href="">
-          <img src="https://escholarship.org/images/homecover_mester.png" alt="Mester journal"/>
+          <LazyLoadComp
+            src="https://escholarship.org/images/homecover_mester.png"
+            alt="Mester journal" />
         </a>
         <a href="">
-          <img src="https://escholarship.org/images/homecover_interactions.png" alt="Interactions journal"/>
+          <LazyLoadComp
+            src="https://escholarship.org/images/homecover_interactions.png"
+            alt="Interactions journal" />
         </a>
         <a href="">
-          <img src="https://escholarship.org/images/homecover_bpj.jpg" alt="Berkeley Planning Journal"/>
+          <LazyLoadComp
+            src="https://escholarship.org/images/homecover_bpj.jpg"
+            alt="Berkeley Planning Journal" />
         </a>
         <a href="">
-          <img src="https://escholarship.org/images/homecover_fb.png" alt="Frontiers journal"/>
+          <LazyLoadComp
+            src="https://escholarship.org/images/homecover_fb.png"
+            alt="Frontiers journal" />
         </a>
         <a href="">
-          <img src="https://escholarship.org/images/homecover_jcmrs.png" alt="Critical journal"/>
+          <LazyLoadComp
+            src="https://escholarship.org/images/homecover_jcmrs.png"
+            alt="Critical journal" />
         </a>
         <div className="c-journalgrid__all">
           <a href="">View all <strong>68</strong> current journals</a>
