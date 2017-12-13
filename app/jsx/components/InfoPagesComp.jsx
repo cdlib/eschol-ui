d3739e67ee689fb72cb74490d3bb7820b904b0d6
JoelUCOP <joel.hagedorn@ucop.edu>
2017-12-12 16:29:12 -0800

    update infopages pagination component

--- app/jsx/components/InfoPagesComp.jsx
+++ app/jsx/components/InfoPagesComp.jsx
@@ -3,7 +3,6 @@
 import React from 'react'
 import $ from 'jquery'
 import dotdotdot from 'jquery.dotdotdot'
-import PaginationComp from '../components/PaginationComp.jsx'
 
 class InfoPagesComp extends React.Component {
   state={showMore: false}
@@ -116,7 +115,19 @@ class InfoPagesComp extends React.Component {
               </h3>
             </div>
           </div>
-          <PaginationComp />
+          <nav className="c-pagination--next">
+            <ul>
+              {/*
+              Note the aria-label text differences in each list item below, including "last" result set:
+              */}
+              <li><a href="" className="c-pagination__item" aria-label="go to result set 1">1</a></li>
+              <li><a href="" className="c-pagination__item" aria-label="go to result set 2">2</a></li>
+              <li><a href="" className="c-pagination__item--current" aria-label="you are on result set 3">3</a></li>
+              <li><a href="" className="c-pagination__item" aria-label="go to result set 4">4</a></li>
+              <li><a href="" className="c-pagination__item" aria-label="go to last result set 1000">1000</a></li>
+              <li className="c-pagination__next"><a href="" aria-label="go to next result set">Next</a></li>
+            </ul>
+          </nav>
           <button className="c-infopages__toggle" onClick={()=> this.setState({showMore: false})}>Show fewer results</button>
         </div>
       </div>
