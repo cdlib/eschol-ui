2f2b8ab8f7608fc8392bf61f3708149152fd503c
JoelUCOP <joel.hagedorn@ucop.edu>
2017-12-21 11:18:18 -0800

    add unitcarousel authorlist sample items

--- app/jsx/components/UnitCarouselComp.jsx
+++ app/jsx/components/UnitCarouselComp.jsx
@@ -22,70 +22,153 @@ class UnitCarouselComp extends React.Component {
             <div className="c-unitcarousel__item">
               <h3><a href="">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</a>
               </h3>
-              <div className="c-unitcarousel__item-authorlist">authorlist comp goes here
+              <div className="c-unitcarousel__item-authorlist">
+                <div className="c-authorlist">
+                  <ul className="c-authorlist__list">
+                    <li>Barshop, William</li>
+                    <li>Bedard James E J</li>
+                    <li>Dothager, Matthew</li>
+                  </ul>
+                </div>
               </div>
               <div className="c-unitcarousel__item-type--article">Article</div>
             </div>
             <div className="c-unitcarousel__item">
               <h3><a href="">Quia veritatis est, eum facilis expedita consequatur consequuntur suscipit obcaecati!</a>
               </h3>
-              <div className="c-unitcarousel__item-authorlist">authorlist comp goes here
+              <div className="c-unitcarousel__item-authorlist">
+                <div className="c-authorlist">
+                  <ul className="c-authorlist__list">
+                    <li>Barshop, William</li>
+                    <li>Bedard James E J</li>
+                    <li>Dothager, Matthew</li>
+                    <li>Lee, Paul</li>
+                    <li>Leung, Wilson</li>
+                    <li>Machone, Joshua F</li>
+                  </ul>
+                </div>
               </div>
               <div className="c-unitcarousel__item-type--book">Book</div>
             </div>
             <div className="c-unitcarousel__item">
               <h3><a href="">Pariatur cupiditate rerum quidem, omnis et inventore repellendus velit autem.</a>
               </h3>
-              <div className="c-unitcarousel__item-authorlist">authorlist comp goes here
+              <div className="c-unitcarousel__item-authorlist">
+                <div className="c-authorlist">
+                  <ul className="c-authorlist__list">
+                    <li>Barshop, William</li>
+                    <li>Bedard James E J</li>
+                    <li>Dothager, Matthew</li>
+                    <li>Lee, Paul</li>
+                  </ul>
+                </div>
               </div>
               <div className="c-unitcarousel__item-type--thesis">Thesis</div>
             </div>
             <div className="c-unitcarousel__item">
               <h3><a href="">Quidem blanditiis voluptatum pariatur earum doloremque similique ab cumque sit.</a>
               </h3>
-              <div className="c-unitcarousel__item-authorlist">authorlist comp goes here
+              <div className="c-unitcarousel__item-authorlist">
+                <div className="c-authorlist">
+                  <ul className="c-authorlist__list">
+                    <li>Barshop, William</li>
+                    <li>Bedard James E J</li>
+                    <li>Dothager, Matthew</li>
+                    <li>Lee, Paul</li>
+                    <li>Leung, Wilson</li>
+                    <li>Machone, Joshua F</li>
+                  </ul>
+                </div>
               </div>
               <div className="c-unitcarousel__item-type--multimedia">Multimedia</div>
             </div>
             <div className="c-unitcarousel__item">
               <h3><a href="">Ratione ipsa vero aperiam, error quis voluptatibus rerum laboriosam, doloremque.</a>
               </h3>
-              <div className="c-unitcarousel__item-authorlist">authorlist comp goes here
+              <div className="c-unitcarousel__item-authorlist">
+                <div className="c-authorlist">
+                  <ul className="c-authorlist__list">
+                    <li>Barshop, William</li>
+                    <li>Bedard James E J</li>
+                  </ul>
+                </div>
               </div>
               <div className="c-unitcarousel__item-type--article">Article</div>
             </div>
             <div className="c-unitcarousel__item">
               <h3><a href="">Sunt eos praesentium, voluptates veritatis autem hic eligendi officia odit!</a>
               </h3>
-              <div className="c-unitcarousel__item-authorlist">authorlist comp goes here
+              <div className="c-unitcarousel__item-authorlist">
+                <div className="c-authorlist">
+                  <ul className="c-authorlist__list">
+                    <li>Barshop, William</li>
+                    <li>Bedard James E J</li>
+                    <li>Dothager, Matthew</li>
+                    <li>Lee, Paul</li>
+                    <li>Leung, Wilson</li>
+                    <li>Machone, Joshua F</li>
+                  </ul>
+                </div>
               </div>
               <div className="c-unitcarousel__item-type--book">Book</div>
             </div>
             <div className="c-unitcarousel__item">
               <h3><a href="">Facere, quaerat, consequuntur.</a>
               </h3>
-              <div className="c-unitcarousel__item-authorlist">authorlist comp goes here
+              <div className="c-unitcarousel__item-authorlist">
+                <div className="c-authorlist">
+                  <ul className="c-authorlist__list">
+                    <li>Barshop, William</li>
+                    <li>Bedard James E J</li>
+                    <li>Dothager, Matthew</li>
+                    <li>Lee, Paul</li>
+                  </ul>
+                </div>
               </div>
               <div className="c-unitcarousel__item-type--thesis">Thesis</div>
             </div>
             <div className="c-unitcarousel__item">
               <h3><a href="">Esse, quis fugit similique officiis earum molestias vitae dolores odit.</a>
               </h3>
-              <div className="c-unitcarousel__item-authorlist">authorlist comp goes here
+              <div className="c-unitcarousel__item-authorlist">
+                <div className="c-authorlist">
+                  <ul className="c-authorlist__list">
+                    <li>Barshop, William</li>
+                    <li>Bedard James E J</li>
+                    <li>Dothager, Matthew</li>
+                    <li>Lee, Paul</li>
+                    <li>Leung, Wilson</li>
+                    <li>Machone, Joshua F</li>
+                  </ul>
+                </div>
               </div>
               <div className="c-unitcarousel__item-type--multimedia">Multimedia</div>
             </div>
             <div className="c-unitcarousel__item">
               <h3><a href="">Rerum ad itaque rem culpa maxime, animi facilis incidunt quidem!</a>
               </h3>
-              <div className="c-unitcarousel__item-authorlist">authorlist comp goes here
+              <div className="c-unitcarousel__item-authorlist">
+                <div className="c-authorlist">
+                  <ul className="c-authorlist__list">
+                    <li>Barshop, William</li>
+                    <li>Bedard James E J</li>
+                  </ul>
+                </div>
               </div>
               <div className="c-unitcarousel__item-type--article">Article</div>
             </div>
             <div className="c-unitcarousel__item">
               <h3><a href="">Consectetur nostrum voluptatibus soluta, magnam velit nobis tenetur tempore ipsum!</a>
               </h3>
-              <div className="c-unitcarousel__item-authorlist">authorlist comp goes here
+              <div className="c-authorlist">
+                <ul className="c-authorlist__list">
+                  <li>Barshop, William</li>
+                  <li>Bedard James E J</li>
+                  <li>Dothager, Matthew</li>
+                  <li>Lee, Paul</li>
+                  <li>Leung, Wilson</li>
+                  <li>Machone, Joshua F</li>
+                </ul>
               </div>
               <div className="c-unitcarousel__item-type--book">Book</div>
             </div>
