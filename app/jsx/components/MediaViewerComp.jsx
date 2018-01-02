2149732f027fe56842e2f25cfc36e1b01affec6e
JoelUCOP <joel.hagedorn@ucop.edu>
2018-01-02 14:30:06 -0800

    move journalitemmultimedia layout elements to mediaviewer comp

--- app/jsx/components/MediaViewerComp.jsx
+++ app/jsx/components/MediaViewerComp.jsx
@@ -1,12 +1,47 @@
 // ##### Media Viewer Component ##### //
 
 import React from 'react'
+import MediaFileGridComp from '../components/MediaFileGridComp.jsx'
 
 class MediaViewerComp extends React.Component {
   render() {
     return (
       <div className="c-mediaviewer">
-        [media viewer component to go here]
+        <div className="o-mediafeature--video">
+          <h3>New Horizons Kuiper Belt Fly-Through</h3>
+          <div className="o-mediafeature__item">
+            <video src="images/sample_video.mp4" controls poster="images/sample_video-poster.jpg"></video>
+          </div>
+          <div className="o-mediafeature__description">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis optio numquam aut facere odio et accusantium eius quo harum, quis ab molestias illum totam earum nemo voluptatum eligendi nobis. Molestiae reprehenderit et neque nisi nam corporis totam exercitationem, numquam fuga. Sint minima quisquam, velit quod ratione. Vero, libero, accusantium. Rem.
+          </div>
+        </div>
+        <div className="c-itemactions">
+          <div className="o-input__droplist2">
+            <label htmlFor="o-input__droplist-label2">Refine media type by:</label>
+            <select name="" id="o-input__droplist-label2">
+              <option value="">All</option>
+              <option value="">Documents</option>
+              <option value="">Images</option>
+              <option value="">Videos</option>
+              <option value="">Others</option>
+            </select>
+          </div>
+          <div className="o-download">
+            <a href="" className="o-download__button" download>Download All Files</a>
+            <details className="o-download__formats">
+              <summary aria-label="formats"></summary>
+              <ul className="o-download__single-menu">
+                <li><a href="">Image</a></li>
+                <li><a href="">Audio</a></li>
+                <li><a href="">Video</a></li>
+                <li><a href="">Zip</a></li>
+                <li><a href="">File</a></li>
+              </ul>
+            </details>
+          </div>
+        </div>
+        <MediaFileGridComp />
       </div>
     )
   }
--- app/jsx/layouts/JournalItemMultimediaLayout.jsx
+++ app/jsx/layouts/JournalItemMultimediaLayout.jsx
@@ -7,7 +7,7 @@ import NavBarComp from '../components/NavBarComp.jsx'
 import ShareComp from '../components/ShareComp.jsx'
 import AuthorListComp from '../components/AuthorListComp.jsx'
 import PubInfoComp from '../components/PubInfoComp.jsx'
-import MediaFileGridComp from '../components/MediaFileGridComp.jsx'
+import MediaViewerComp from '../components/MediaViewerComp.jsx'
 import JumpComp from '../components/JumpComp.jsx'
 import RelatedItemsComp from '../components/RelatedItemsComp.jsx'
 import FooterComp from '../components/FooterComp.jsx'
@@ -86,41 +86,7 @@ class JournalItemMultimediaLayout extends React.Component {
                 </details>
                 <details className="c-togglecontent" open>
                   <summary>Main Content</summary>
-                  <div className="o-mediafeature--video">
-                    <h3>New Horizons Kuiper Belt Fly-Through</h3>
-                    <div className="o-mediafeature__item">
-                      <video src="images/sample_video.mp4" controls poster="images/sample_video-poster.jpg"></video>
-                    </div>
-                    <div className="o-mediafeature__description">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis optio numquam aut facere odio et accusantium eius quo harum, quis ab molestias illum totam earum nemo voluptatum eligendi nobis. Molestiae reprehenderit et neque nisi nam corporis totam exercitationem, numquam fuga. Sint minima quisquam, velit quod ratione. Vero, libero, accusantium. Rem.
-                    </div>
-                  </div>
-                  <div className="c-itemactions">
-                    <div className="o-input__droplist2">
-                      <label htmlFor="o-input__droplist-label2">Refine media type by:</label>
-                      <select name="" id="o-input__droplist-label2">
-                        <option value="">All</option>
-                        <option value="">Documents</option>
-                        <option value="">Images</option>
-                        <option value="">Videos</option>
-                        <option value="">Others</option>
-                      </select>
-                    </div>
-                    <div className="o-download">
-                      <a href="" className="o-download__button" download>Download All Files</a>
-                      <details className="o-download__formats">
-                        <summary aria-label="formats"></summary>
-                        <ul className="o-download__single-menu">
-                          <li><a href="">Image</a></li>
-                          <li><a href="">Audio</a></li>
-                          <li><a href="">Video</a></li>
-                          <li><a href="">Zip</a></li>
-                          <li><a href="">File</a></li>
-                        </ul>
-                      </details>
-                    </div>
-                  </div>
-                  <MediaFileGridComp />
+                  <MediaViewerComp />
                 </details>
               </div>
             </div>
