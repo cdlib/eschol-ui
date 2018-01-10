0fb6a39b453a7e370d43e0e5588f0f3f969679f9
JoelCDL <joel.hagedorn@ucop.edu>
2018-01-10 11:57:52 -0800

    add more datatable sample data

--- app/jsx/components/DataTableComp.jsx
+++ app/jsx/components/DataTableComp.jsx
@@ -12,27 +12,185 @@ class DataTableComp extends React.Component {
             <th scope="col">Quantity</th>
             <th scope="col">Apples</th>
             <th scope="col">Oranges</th>
-            <th scope="col">Pears</th>
+            <th scope="col">Bananas</th>
+            <th scope="col">Grapes</th>
+            <th scope="col">Strawberries</th>
+            <th scope="col">Cherries</th>
+            <th scope="col">Blackberries</th>
+            <th scope="col">Blueberries</th>
+            <th scope="col">Rasberries</th>
+            <th scope="col">Cranberries</th>
+            <th scope="col">Avocados (a fruit!)</th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <th scope="row">One</th>
-            <td>Odio</td>
             <td>Lorem</td>
-            <td>Optio</td>
+            <td>Animi</td>
+            <td>Ab</td>
+            <td>Nulla</td>
+            <td>Esse</td>
+            <td>Perspiciatis</td>
+            <td>Labore</td>
+            <td>Eligendi</td>
+            <td>Quas</td>
+            <td>Mollitia</td>
+            <td>Doloribus</td>
           </tr>
           <tr>
             <th scope="row">Two</th>
-            <td>Iusto</td>
-            <td>Architecto</td>
             <td>Lorem</td>
+            <td>Consequuntur</td>
+            <td>In</td>
+            <td>Laborum</td>
+            <td>Fugit</td>
+            <td>Reiciendis</td>
+            <td>Natus</td>
+            <td>Repudiandae</td>
+            <td>Fuga</td>
+            <td>Laudantium</td>
+            <td>Modi</td>
           </tr>
           <tr>
             <th scope="row">Three</th>
             <td>Lorem</td>
-            <td>Assumenda</td>
+            <td>Aspernatur</td>
+            <td>Quod</td>
             <td>Quos</td>
+            <td>Ipsum</td>
+            <td>Id</td>
+            <td>Repudiandae</td>
+            <td>Nisi</td>
+            <td>Culpa</td>
+            <td>Illum</td>
+            <td>Asperiores</td>
+          </tr>
+          <tr>
+            <th scope="row">Four</th>
+            <td>Lorem</td>
+            <td>Ab</td>
+            <td>Beatae</td>
+            <td>Ut</td>
+            <td>Aut</td>
+            <td>Ducimus</td>
+            <td>Eius</td>
+            <td>Velit</td>
+            <td>Velit</td>
+            <td>Inventore</td>
+            <td>Nihil</td>
+          </tr>
+          <tr>
+            <th scope="row">Five</th>
+            <td>Lorem</td>
+            <td>Ipsa</td>
+            <td>Consectetur</td>
+            <td>Distinctio</td>
+            <td>Distinctio</td>
+            <td>Amet</td>
+            <td>Minima</td>
+            <td>Fugiat</td>
+            <td>Consequatur</td>
+            <td>Incidunt</td>
+            <td>Reiciendis</td>
+          </tr>
+          <tr>
+            <th scope="row">Six</th>
+            <td>Lorem</td>
+            <td>Velit</td>
+            <td>Rerum</td>
+            <td>Hic</td>
+            <td>Itaque</td>
+            <td>Commodi</td>
+            <td>Dolorem</td>
+            <td>Fuga</td>
+            <td>Architecto</td>
+            <td>Beatae</td>
+            <td>Ut</td>
+          </tr>
+          <tr>
+            <th scope="row">Seven</th>
+            <td>Lorem</td>
+            <td>Voluptatibus</td>
+            <td>Quaerat</td>
+            <td>Officia</td>
+            <td>Magni</td>
+            <td>Similique</td>
+            <td>Voluptates</td>
+            <td>Cum</td>
+            <td>Pariatur</td>
+            <td>Obcaecati</td>
+            <td>Officia</td>
+          </tr>
+          <tr>
+            <th scope="row">Eight</th>
+            <td>Lorem</td>
+            <td>A</td>
+            <td>Repellat</td>
+            <td>Ab</td>
+            <td>Reprehenderit</td>
+            <td>Ipsum</td>
+            <td>Dolore</td>
+            <td>Facere</td>
+            <td>Ipsa</td>
+            <td>Unde</td>
+            <td>Eveniet</td>
+          </tr>
+          <tr>
+            <th scope="row">Nine</th>
+            <td>Lorem</td>
+            <td>Et</td>
+            <td>Optio</td>
+            <td>Porro</td>
+            <td>Eveniet</td>
+            <td>Blanditiis</td>
+            <td>Magni</td>
+            <td>Numquam</td>
+            <td>Illum</td>
+            <td>Veniam</td>
+            <td>Quia</td>
+          </tr>
+          <tr>
+            <th scope="row">Ten</th>
+            <td>Lorem</td>
+            <td>Accusantium</td>
+            <td>Delectus</td>
+            <td>Ex</td>
+            <td>Repudiandae</td>
+            <td>Voluptatem</td>
+            <td>Accusamus</td>
+            <td>Repudiandae</td>
+            <td>Rerum</td>
+            <td>Hic</td>
+            <td>Voluptate</td>
+          </tr>
+          <tr>
+            <th scope="row">Eleven</th>
+            <td>Lorem</td>
+            <td>Ut</td>
+            <td>Sit</td>
+            <td>Nihil</td>
+            <td>Totam</td>
+            <td>Porro</td>
+            <td>Autem</td>
+            <td>Aspernatur</td>
+            <td>Repellat</td>
+            <td>Rem</td>
+            <td>Nulla</td>
+          </tr>
+          <tr>
+            <th scope="row">Twelve</th>
+            <td>Lorem</td>
+            <td>Iusto</td>
+            <td>Cupiditate</td>
+            <td>Voluptatibus</td>
+            <td>Excepturi</td>
+            <td>Mollitia</td>
+            <td>Quasi</td>
+            <td>Dolorem?</td>
+            <td>Voluptatum</td>
+            <td>Laudantium</td>
+            <td>Tempore</td>
           </tr>
         </tbody>
       </table>
