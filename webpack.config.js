957b0b6e99312cf65bc6d16e1399c5a722d9e629
JoelUCOP <joel.hagedorn@ucop.edu>
2018-01-26 16:45:51 -0800

    add initial webpack config and babel plugins

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './app/jsx/app.jsx',
+  output: {
+    filename: 'webpack-test.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env', 'babel-preset-es2015', 'babel-preset-react', 'babel-preset-stage-2']
+          }
+        }
+      }
+    ]
+  }
+};
