eed017d8801f300a590594ed740f586965ae97a3
JoelUCOP <joel.hagedorn@ucop.edu>
2018-01-29 15:28:44 -0800

    add package-lock.json to .gitignore; add webpack config files intended for commit e2e6e72

---
+++ webpack.common.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './app/jsx/app.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env', 'babel-preset-react', 'babel-preset-stage-2']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    // plugins
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist/js')
+  },
+};
