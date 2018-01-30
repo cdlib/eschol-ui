4cdad2680e9cf0751410991dd7297030d4353f44
JoelUCOP <joel.hagedorn@ucop.edu>
2018-01-29 16:40:19 -0800

    begin adding webpack scss loaders/plugins

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,28 @@
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    filename: "webpack-test-styles.css"
+});
 
 module.exports = merge(common, {
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: extractSass.extract({
+        use: [{
+          loader: "css-loader"
+        },
+        {
+          loader: "sass-loader"
+        }],
+      })
+    }]
+  },
   plugins: [
+    extractSass,
     new UglifyJSPlugin()
   ]
 });
