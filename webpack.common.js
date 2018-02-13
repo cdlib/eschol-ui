59a2a6d9e7ec20554993d1b93e83273e341182bf
JoelUCOP <joel.hagedorn@ucop.edu>
2018-02-13 13:01:00 -0800

    remove webpack img-loader, file-loader for images

--- webpack.common.js
+++ webpack.common.js
@@ -34,21 +34,6 @@ module.exports = {
         })
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: '../images/'
-            }
-          },
-          {
-            loader: 'img-loader'
-          }
-        ],
-      },
-      {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
           {
