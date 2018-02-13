5c8ede99e8788cc74c29739541020c5c37554cdc
JoelUCOP <joel.hagedorn@ucop.edu>
2018-02-13 10:23:11 -0800

    create remote path var for static media samples; add to socialicons comp

--- app/jsx/components/SocialIconsComp.jsx
+++ app/jsx/components/SocialIconsComp.jsx
@@ -1,22 +1,20 @@
 // ##### Social Icons Component ##### //
 
 import React from 'react'
-import '../../images/logo_facebook-circle-black.svg'
-import '../../images/logo_twitter-circle-black.svg'
-import '../../images/logo_rss-circle-black.svg'
+import MEDIA_PATH from '../../js/MediaPath.js'
 
 class SocialIconsComp extends React.Component {
   render() {
     return (
       <div className="c-socialicons">
         <a href="">
-          <img src="images/logo_facebook-circle-black.svg" alt="Facebook"/>
+          <img src={MEDIA_PATH + 'logo_facebook-circle-black.svg'} alt="Facebook"/>
         </a>
         <a href="">
-          <img src="images/logo_twitter-circle-black.svg" alt="Twitter"/>
+          <img src={MEDIA_PATH + 'logo_twitter-circle-black.svg'} alt="Twitter"/>
         </a>
         <a href="">
-          <img src="images/logo_rss-circle-black.svg" alt="RSS"/>
+          <img src={MEDIA_PATH + 'logo_rss-circle-black.svg'} alt="RSS"/>
         </a>
       </div>
     )
