d4ae4848ff4165d2bf03d19deebe0e137e710807
JoelUCOP <joel.hagedorn@ucop.edu>
2018-02-13 13:08:30 -0800

    remove commented out webpack devServer and HMR plugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,8 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-// const webpack = require('webpack'); // for webpack.HotModuleReplacementPlugin()
 
 module.exports = merge(common, {
-  // devServer: {
-  //   publicPath: '/dist/js',
-  //   hot: true
-  // },
   module: {
     rules: [{
       test: /\.scss$/,
@@ -28,7 +23,6 @@ module.exports = merge(common, {
     }]
   },
   plugins: [
-    // new webpack.HotModuleReplacementPlugin()
     new BrowserSyncPlugin({ 
       host: 'localhost',
       port: 3000,
