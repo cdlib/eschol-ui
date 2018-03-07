587b105b41acee48ebd5ab74c277d96b9814a763
JoelUCOP <joel.hagedorn@ucop.edu>
2018-03-07 14:57:28 -0800

    replace authorlist static first/last names with faker

--- app/jsx/components/AuthorListComp.jsx
+++ app/jsx/components/AuthorListComp.jsx
@@ -3,32 +3,33 @@
 import React from 'react'
 import $ from 'jquery'
 import dotdotdot from 'jquery.dotdotdot'
+import faker from 'faker/locale/en'
 
 class AuthorListComp extends React.Component {
   componentDidMount() {
     $('.c-authorlist__list').dotdotdot({watch:'window', after:'.c-authorlist__list-more-link', ellipsis:' ', wrap:'children'});
-    setTimeout(()=> $('.c-authorlist__list').trigger("update"), 0)
+    setTimeout(()=> $('.c-authorlist__list').trigger('update'), 0)
   }
   render() {
     return (
       <div className="c-authorlist">
         <ul className="c-authorlist__list">
-          <li><a href="">Barshop, William</a></li>
-          <li><a href="">Bedard James E J</a></li>
-          <li><a href="">Dothager, Matthew</a></li>
-          <li><a href="">Lee, Paul</a></li>
-          <li><a href="">Leung, Wilson</a></li>
-          <li><a href="">Machone, Joshua F</a></li>
-          <li><a href="">Patterson, Seantay D</a></li>
-          <li><a href="">Price, Amber L</a></li>
-          <li><a href="">Reed, Laura K</a></li>
-          <li><a href="">Robic, Srebrenka</a></li>
-          <li><a href="">Shaffer, Christopher D</a></li>
-          <li><a href="">Smith, Sheryl T</a></li>
-          <li><a href="">Turner, Bryce A</a></li>
-          <li><a href="">Wong, Jeannette</a></li>
-          <li><a href="">Xiong, David</a></li>
-          <li><a href="">Yuan, Han</a></li>
+          <li><a href="">{faker.fake("{{name.lastName}}, {{name.firstName}}")}</a></li>
+          <li><a href="">{faker.fake("{{name.lastName}}, {{name.firstName}}")}</a></li>
+          <li><a href="">{faker.fake("{{name.lastName}}, {{name.firstName}}")}</a></li>
+          <li><a href="">{faker.fake("{{name.lastName}}, {{name.firstName}}")}</a></li>
+          <li><a href="">{faker.fake("{{name.lastName}}, {{name.firstName}}")}</a></li>
+          <li><a href="">{faker.fake("{{name.lastName}}, {{name.firstName}}")}</a></li>
+          <li><a href="">{faker.fake("{{name.lastName}}, {{name.firstName}}")}</a></li>
+          <li><a href="">{faker.fake("{{name.lastName}}, {{name.firstName}}")}</a></li>
+          <li><a href="">{faker.fake("{{name.lastName}}, {{name.firstName}}")}</a></li>
+          <li><a href="">{faker.fake("{{name.lastName}}, {{name.firstName}}")}</a></li>
+          <li><a href="">{faker.fake("{{name.lastName}}, {{name.firstName}}")}</a></li>
+          <li><a href="">{faker.fake("{{name.lastName}}, {{name.firstName}}")}</a></li>
+          <li><a href="">{faker.fake("{{name.lastName}}, {{name.firstName}}")}</a></li>
+          <li><a href="">{faker.fake("{{name.lastName}}, {{name.firstName}}")}</a></li>
+          <li><a href="">{faker.fake("{{name.lastName}}, {{name.firstName}}")}</a></li>
+          <li><a href="">{faker.fake("{{name.lastName}}, {{name.firstName}}")}</a></li>
           <li><a href="" className="c-authorlist__list-more-link">et al.</a></li>
         </ul>
       </div>
