de78916e430ea245663665be0d309f73d4962a63
JoelUCOP <joel.hagedorn@ucop.edu>
2018-03-08 10:51:18 -0800

    replace download disp static lorem with faker lorem

--- app/jsx/display/DownloadDisp.jsx
+++ app/jsx/display/DownloadDisp.jsx
@@ -2,18 +2,14 @@
 
 import React from 'react'
 import DownloadObj from '../objects/DownloadObj.jsx'
+import faker from 'faker/locale/en'
 
 class DownloadDisp extends React.Component {
   render() {
     return (
       <div>
          <DownloadObj />
-         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime aspernatur, modi deserunt placeat rerum laboriosam voluptates iusto ipsum, architecto, quidem expedita laborum alias vero? Delectus voluptatibus sit magni incidunt, praesentium dolorem perspiciatis earum illo natus odit iure consequatur aliquid, dolore quaerat pariatur error nesciunt voluptates, laudantium. Rerum, dolores deserunt. Eaque iusto, architecto illo perspiciatis ratione, cum animi fugit dolorum atque totam magnam vitae est sed, labore tenetur culpa molestias distinctio at. Cum facilis quam necessitatibus impedit, sint libero velit magnam a repudiandae aut minima, fuga, aspernatur commodi omnis expedita? Facilis quae sed aperiam, beatae est voluptatem. Optio atque fugit laboriosam.
-         </p>
-         <p>Nemo voluptate consequatur vitae hic perspiciatis ea delectus molestias, provident doloribus quibusdam, porro obcaecati corporis perferendis distinctio culpa quidem eum blanditiis ullam nulla dicta harum! Ipsam impedit esse, iure? Architecto nulla saepe reiciendis! Ipsum doloremque, blanditiis magni quidem libero deleniti numquam rem saepe eius, iste necessitatibus molestiae corporis, esse velit perferendis incidunt consectetur totam sequi recusandae vel explicabo, dolorem sapiente! Facere, fugit, labore. Vel maxime quam numquam iusto non beatae veniam deserunt temporibus sunt ducimus sed, doloremque officia voluptatem incidunt aut possimus, quae autem magni provident fugit perspiciatis in laborum? At quaerat quos quo reiciendis officiis neque expedita, ab dolorum.
-         </p>
-         <p>Facilis minus quidem illo, accusantium ipsa neque aperiam voluptatem cupiditate. Qui cupiditate totam vitae libero illo eos necessitatibus nihil labore! Reprehenderit voluptates illum ex nemo laudantium assumenda, dolores fuga! Saepe, earum numquam, esse eaque voluptatum suscipit cumque nulla, nam magnam voluptatem assumenda cum officiis debitis expedita omnis distinctio fugit quibusdam neque sed, minus voluptate architecto incidunt id. Minus cumque similique, eum doloremque illo assumenda deserunt dignissimos molestias. Consectetur suscipit similique odio praesentium illum dolore deleniti, neque doloremque unde sunt qui consequatur, sed debitis, rerum quisquam veniam fugit quam natus officia, ut ea aperiam voluptate velit quibusdam! Deleniti ut reprehenderit, laboriosam.
-         </p>
+         <p>{faker.fake("{{lorem.paragraphs}}")}</p>
       </div>
     )
   }
